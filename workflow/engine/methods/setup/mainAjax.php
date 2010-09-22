a1d00cd516181a157ce75eca4fedeec0050d5dc7
Erik Amaru Ortiz <erik@colosa.com>
2010-09-22 16:31:44 +0000

    -------------------------------------------------------------------------------- [2010-08-23 15:58:20] Rev:196  |  [0;35;33m1 files Modified[0m erik: more improvements un setup ADMIN tab -------------------------------------------------------------------------------- 1. M /trunk/workflow/engine/methods/setup/mainAjax.php

--- workflow/engine/methods/setup/mainAjax.php
+++ workflow/engine/methods/setup/mainAjax.php
@@ -1,63 +1,66 @@
 <?php
 
-  $request = isset($_POST['request'])? $_POST['request']: (isset($_GET['request'])? $_GET['request']: null);
-  
-  switch($request){
-    case 'loadMenu':
-      if( ! isset($_GET['menu']) ) {
-        exit(0);
-      }
+ob_start();
 
-      global $G_TMP_MENU;
-      $oMenu = new Menu();
-      $oMenu->load('setup');
-      $items = Array();
-      
-      foreach( $oMenu->Options as $i=>$option) {
-        if( $oMenu->Types[$i] == $_GET['menu'] ){
-          $items[] = Array(
-            'id'    => $oMenu->Id[$i],
-            'url'  => ($oMenu->Options[$i]!='')? $oMenu->Options[$i]: '#',
-            //'onclick' => ($oMenu->JS[$i]!='')? $oMenu->JS[$i]: '',
-            'text' => $oMenu->Labels[$i],
-            //'icon'  => ($oMenu->Icons[$i]!='')? $oMenu->Icons[$i]: 'icon-pmlogo.png',
-            //'target'=> ($oMenu->Types[$i]=='admToolsContent')? 'admToolsContent': ''
-            'loaded' => true,
-            'leaf'   => true,
-            'cls'    => 'pm-tree-node',
-            'iconCls'=> 'ICON_'.$oMenu->Id[$i]
-          );
-        } else if( ! in_array($oMenu->Types[$i], Array('tool', 'maintenance')) && $_GET['menu'] == 'setting' ){
-          $items[] = Array(
-            'id'    => $oMenu->Id[$i],
-            'url'  => ($oMenu->Options[$i]!='')? $oMenu->Options[$i]: '#',
-            //'onclick' => ($oMenu->JS[$i]!='')? $oMenu->JS[$i]: '',
-            'text' => $oMenu->Labels[$i],
-            //'icon'  => ($oMenu->Icons[$i]!='')? $oMenu->Icons[$i]: 'icon-pmlogo.png',
-            //'target'=> ($oMenu->Types[$i]=='admToolsContent')? 'admToolsContent': ''
-            'loaded' => true,
-            'leaf'   => true,
-            'cls'    => 'pm-tree-node',
-            'iconCls'=> 'ICON_'.$oMenu->Id[$i]
-          );
-        }
-      }
-      
-      if( isset($_SESSION['DEV_FLAG']) && $_SESSION['DEV_FLAG'] && $_GET['menu'] == 'tool' ){
+$request = isset($_POST['request'])? $_POST['request']: (isset($_GET['request'])? $_GET['request']: null);
+
+switch($request){
+  case 'loadMenu':
+    if( ! isset($_GET['menu']) ) {
+      exit(0);
+    }
+
+    global $G_TMP_MENU;
+    $oMenu = new Menu();
+    $oMenu->load('setup');
+    $items = Array();
+
+    foreach( $oMenu->Options as $i=>$option) {
+      if( $oMenu->Types[$i] == $_GET['menu'] ){
         $items[] = Array(
-          'id'    => 'translations',
-          'url'  => '../tools/translations',
-          'text' => 'Translations',
+          'id'    => $oMenu->Id[$i],
+          'url'  => ($oMenu->Options[$i]!='')? $oMenu->Options[$i]: '#',
+          //'onclick' => ($oMenu->JS[$i]!='')? $oMenu->JS[$i]: '',
+          'text' => $oMenu->Labels[$i],
+          //'icon'  => ($oMenu->Icons[$i]!='')? $oMenu->Icons[$i]: 'icon-pmlogo.png',
+          //'target'=> ($oMenu->Types[$i]=='admToolsContent')? 'admToolsContent': ''
           'loaded' => true,
           'leaf'   => true,
           'cls'    => 'pm-tree-node',
-          'iconCls'=> 'ICON_'
+          'iconCls'=> 'ICON_'.$oMenu->Id[$i]
+        );
+      } else if( ! in_array($oMenu->Types[$i], Array('tool', 'maintenance')) && $_GET['menu'] == 'setting' ){
+        $items[] = Array(
+          'id'    => $oMenu->Id[$i],
+          'url'  => ($oMenu->Options[$i]!='')? $oMenu->Options[$i]: '#',
+          //'onclick' => ($oMenu->JS[$i]!='')? $oMenu->JS[$i]: '',
+          'text' => $oMenu->Labels[$i],
+          //'icon'  => ($oMenu->Icons[$i]!='')? $oMenu->Icons[$i]: 'icon-pmlogo.png',
+          //'target'=> ($oMenu->Types[$i]=='admToolsContent')? 'admToolsContent': ''
+          'loaded' => true,
+          'leaf'   => true,
+          'cls'    => 'pm-tree-node',
+          'iconCls'=> 'ICON_'.$oMenu->Id[$i]
         );
       }
-      
-      echo G::json_encode($items);
-    break;
+    }
 
-  }
+    if( isset($_SESSION['DEV_FLAG']) && $_SESSION['DEV_FLAG'] && $_GET['menu'] == 'tool' ){
+      $items[] = Array(
+        'id'    => 'translations',
+        'url'  => '../tools/translations',
+        'text' => 'Translations',
+        'loaded' => true,
+        'leaf'   => true,
+        'cls'    => 'pm-tree-node',
+        'iconCls'=> 'ICON_'
+      );
+    }
+    $x = ob_get_contents();
+    ob_end_clean();
+    echo G::json_encode($items);
+  break;
+
+}
 
   
