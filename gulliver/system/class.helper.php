a4c5ae0bcc8327914302dca7f172c8844cda5109
Erik Amaru Ortiz <erik@colosa.com>
2010-09-22 17:06:38 +0000

    [2010-08-26 14:20:42] Rev:226  |  1 files Modified erik: added the header file -------------------------------------------------------------------------------- 1. M /trunk/gulliver/system/class.helper.php

--- gulliver/system/class.helper.php
+++ gulliver/system/class.helper.php
@@ -1,4 +1,35 @@
-<?
+<?php
+/**
+ * class.form.php
+ *
+ * ProcessMaker Open Source Edition
+ * Copyright (C) 2004 - 2008 Colosa Inc.23
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * For more information, contact Colosa Inc, 2566 Le Jeune Rd.,
+ * Coral Gables, FL, 33134, USA, or email info@colosa.com.
+ *
+ */
+ 
+/**
+ * Class Helper
+ * @author Erik Amaru Ortiz. <erik@colosa.com>
+ * @package gulliver.system
+ * @access public
+ * @dependencies none
+ */
 
 class Helper{
   public $content;
