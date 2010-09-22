685b12fa0b5829bc9a93ebe3466e766c6eebda23
Erik Amaru Ortiz <erik@colosa.com>
2010-09-22 19:54:51 +0000

    [2010-08-31 15:27:09] Rev:247  |  1 files Modified erik: improvements for language export (fixes) -------------------------------------------------------------------------------- 1. M /trunk/gulliver/system/class.i18n_po.php

--- gulliver/system/class.i18n_po.php
+++ gulliver/system/class.i18n_po.php
@@ -1,5 +1,38 @@
 <?php
-
+/**
+ * class.dbtable.php
+ *
+ * ProcessMaker Open Source Edition
+ * Copyright (C) 2004 - 2008 Colosa Inc.23
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * For more information, contact Colosa Inc, 2566 Le Jeune Rd.,
+ * Coral Gables, FL, 33134, USA, or email info@colosa.com.
+ *
+ */
+ 
+ /**
+ * i18n_PO 
+ * This class build bigger PO files without size limit and this not use much memory that the allowed
+ * 
+ * @package gulliver.system
+ * @author Erik Amaru Ortiz <erik@colosa.com>
+ * @date Aug 31th, 2010
+ * @copyright (C) 2002 by Colosa Development Team.
+ */
+ 
 class i18n_PO
 {
   private $_file   = NULL;
@@ -116,14 +149,16 @@ class i18n_PO
     }
   }
   
-  function headerStroke(){
+  function headerStroke()
+  {
     if( $this->_editingHeader ) {
       $this->_editingHeader = FALSE;
       $this->_writeLine('');;
     }
   }
   
-  function __destruct() {
+  function __destruct() 
+  {
     if ( $this->_fp )
       fclose($this->_fp);
   }
