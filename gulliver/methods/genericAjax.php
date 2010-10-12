8db9804ca065a703d2ef85e599134d2d9c89cafe
Erik Amaru Ortiz <erik@colosa.com>
2010-10-12 16:03:58 +0000

    [2010-09-30 15:27:05] Rev:4231  |  1 files Modified erik: BUG:  0005419: å ä ö is not working as a search text
    Is solved for those characteres type
    
    
    
    --------------------------------------------------------------------------------
    1. M /trunk/gulliver/methods/genericAjax.php

--- gulliver/methods/genericAjax.php
+++ gulliver/methods/genericAjax.php
@@ -17,110 +17,108 @@ if( isset($request) ){
 			}
 			
 		break;
+    /** widgets **/
+    case 'suggest':
 
-        /** widgets **/
+      try{
+        $sData = base64_decode(str_rot13($_GET['hash']));
+        list($SQL, $DB_UID) = explode('@', $sData);
 
-        case 'suggest':
+        $aRows = Array();
+        try {
+            $con = Propel::getConnection($DB_UID);
+            $con->begin();
+            $rs = $con->executeQuery($SQL);
+            $con->commit();
 
-          try{
-            $sData = base64_decode(str_rot13($_GET['hash']));
-            list($SQL, $DB_UID) = explode('@', $sData);
-      
-            $aRows = Array();
-            try {
-                $con = Propel::getConnection($DB_UID);
-                $con->begin();
-                $rs = $con->executeQuery($SQL);
-                $con->commit();
-
-                while ( $rs->next() ) {
-                    array_push($aRows, $rs->getRow());
-                }
-            } catch (SQLException $sqle) {
-                $con->rollback();
+            while ( $rs->next() ) {
+                array_push($aRows, $rs->getRow());
             }
+        } catch (SQLException $sqle) {
+            $con->rollback();
+        }
 
-            $input = strtolower( $_GET['input'] );
-            $len = strlen($input);
-            $limit = isset($_GET['limit']) ? (int) $_GET['limit'] : 0;
+        $input = strtolower( $_GET['input'] );
+        $len = strlen($input);
+        $limit = isset($_GET['limit']) ? (int) $_GET['limit'] : 0;
 
-            $aResults = array();
-            $count = 0;
-
-            
-            if ($len){
-                for ($i=0;$i<count($aRows);$i++){
-                    $aRow = $aRows[$i];
-                    $nCols = sizeof($aRow);
-                    
-                    $aRow = array_values($aRow);
-                    switch( $nCols ){
-                        case 1:
-                            $id = $aRow[0];
-                            $value = $aRow[0];
-                            $info = '';
-                            break;
-
-                        case 2:
-                            
-                            $id = $aRow[0];
-                            $value = $aRow[1];
-                            $info = '';
-                            break;
-                        
-                        case $nCols >= 3:
-                            //print_r($aRow);
-                            $id = $aRow[0];
-                            $value = $aRow[1];
-                            $info = $aRow[2];
-                            break;
-                    }
+        $aResults = array();
+        $count = 0;
 
 
-                    // had to use utf_decode, here
-                    // not necessary if the results are coming from mysql
-                    //
-                    if (strtolower(substr(utf8_decode($value),0,$len)) == $input){
-                        $count++;
-                        $aResults[] = array( "id"=>$id ,"value"=>htmlspecialchars($value), "info"=>htmlspecialchars($info) );
-                    }
+        if ($len){
+            for ($i=0;$i<count($aRows);$i++){
+                $aRow = $aRows[$i];
+                $nCols = sizeof($aRow);
 
-                    if ($limit && $count==$limit)
+                $aRow = array_values($aRow);
+                switch( $nCols ){
+                    case 1:
+                        $id = $aRow[0];
+                        $value = $aRow[0];
+                        $info = '';
+                        break;
+
+                    case 2:
+
+                        $id = $aRow[0];
+                        $value = $aRow[1];
+                        $info = '';
+                        break;
+
+                    case $nCols >= 3:
+                        //print_r($aRow);
+                        $id = $aRow[0];
+                        $value = $aRow[1];
+                        $info = $aRow[2];
                         break;
                 }
-            }
 
-            header ("Expires: Mon, 26 Jul 1997 05:00:00 GMT"); // Date in the past
-            header ("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT"); // always modified
-            header ("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
-            header ("Pragma: no-cache"); // HTTP/1.0
 
-            if (isset($_REQUEST['json'])) {
-                header("Content-Type: application/json");
-                echo "{\"status\":0,  \"results\": [";
-                $arr = array();
-                for ($i=0;$i<count($aResults);$i++) {
-                    $arr[] = "{\"id\": \"".$aResults[$i]['id']."\", \"value\": \"".$aResults[$i]['value']."\", \"info\": \"".$aResults[$i]['info']."\"}";
+                // had to use utf_decode, here
+                // not necessary if the results are coming from mysql
+                //
+                if (strtolower(substr($value,0,$len)) == $input){
+                    $count++;
+                    $aResults[] = array( "id"=>$id ,"value"=>htmlspecialchars($value), "info"=>htmlspecialchars($info) );
                 }
-                echo implode(", ", $arr);
-                echo "]}";
-            } else {
-                header("Content-Type: text/xml");
 
-                echo "<?xml version=\"1.0\" encoding=\"utf-8\" ?><results>";
-                for ($i=0;$i<count($aResults);$i++)
-                {
-                    echo "<rs id=\"".$aResults[$i]['id']."\" info=\"".$aResults[$i]['info']."\">".$aResults[$i]['value']."</rs>";
-                }
-                echo "</results>";
+                if ($limit && $count==$limit)
+                    break;
             }
+        }
 
-          } catch(Exception $e){
-            $err = $e->getMessage();
-            $err = eregi_replace("[\n|\r|\n\r]", ' ', $err);
-            echo '{"status":1, "message":"'.$err.'"}';
-          }
-        break;
+        header ("Expires: Mon, 26 Jul 1997 05:00:00 GMT"); // Date in the past
+        header ("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT"); // always modified
+        header ("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+        header ("Pragma: no-cache"); // HTTP/1.0
+
+        if (isset($_REQUEST['json'])) {
+            header("Content-Type: application/json");
+            echo "{\"status\":0,  \"results\": [";
+            $arr = array();
+            for ($i=0;$i<count($aResults);$i++) {
+                $arr[] = "{\"id\": \"".$aResults[$i]['id']."\", \"value\": \"".$aResults[$i]['value']."\", \"info\": \"".$aResults[$i]['info']."\"}";
+            }
+            echo implode(", ", $arr);
+            echo "]}";
+        } else {
+            header("Content-Type: text/xml");
+
+            echo "<?xml version=\"1.0\" encoding=\"utf-8\" ?><results>";
+            for ($i=0;$i<count($aResults);$i++)
+            {
+                echo "<rs id=\"".$aResults[$i]['id']."\" info=\"".$aResults[$i]['info']."\">".$aResults[$i]['value']."</rs>";
+            }
+            echo "</results>";
+        }
+
+      } catch(Exception $e){
+        $err = $e->getMessage();
+        $err = eregi_replace("[\n|\r|\n\r]", ' ', $err);
+        echo '{"status":1, "message":"'.$err.'"}';
+      }
+    break;
 
 
       case 'storeInTmp':
