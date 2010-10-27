186ba14bd74f7e4a318dd87abc658e844f3a19ff
Alvaro Campos <alvaro@colosa.com>
2010-10-27 19:47:34 +0000

    solved problem with browser IE browser for documents

--- workflow/engine/templates/appFolder/appFolderTree.php
+++ workflow/engine/templates/appFolder/appFolderTree.php
@@ -15,7 +15,7 @@ $html = '
 	 <div class="boxTopBlue"><div class="a"></div><div class="b"></div><div class="c"></div></div>
 	 <div class="boxContentBlue">
 
-	  <table width="100%" style="margin:0px;" cellspacing="0" cellpadding="0">
+	  <table style="margin:0px;" cellspacing="0" cellpadding="0">
 	  <tr>
 		  <td class="userGroupTitle"><div class="userGroupLink">'.G::LoadTranslation("ID_PM_FOLDER").': <a href="javascript:openPMFolder(\''.$rootFolder.'\',\''.$rootFolder.'\')">'.$folderPath['PATH'].'</a></div></td>
 	  </tr>
@@ -24,18 +24,61 @@ $html = '
 	<div class="boxBottomBlue"><div class="a"></div><div class="b"></div><div class="c"></div></div>
 
 	';
-	$html.='<div class="treeBase" style="width:360px;height: expression( this.scrollHeight > 319 ? \'320px\' : \'auto\' ); /* sets max-height for IE */  max-height: 320px; /* sets max-height value for all standards-compliant browsers */  overflow:auto;">
+	$html.='<div class="treeBase" style="width:360px;height: expression( this.scrollHeight > 319 ? \'320px\' : \'auto\' ); /* sets max-height for IE */  max-height: 320px; /* sets max-height value for all standards-compliant browsers */  overflow:hidden;">
 	<div class="boxTop">
 		<div class="a"></div>
 		<div class="b"></div>
 		<div class="c"></div>
 	</div>
 
-  <div class="content" > ';
-  $html.="<div id='child_$rootFolder'  ></div>
+ <!-- <div class="content" style="width:360px;height: expression( this.scrollHeight > 319 ? \'320px\' : \'auto\' ); /* sets max-height for IE */  max-height: 320px; /* sets max-height value for all standards-compliant browsers */  overflow:hidden;"> -->';
+  
+  //add alvaro
+  function looking_for_browser($user_agent) {
+      $browsers = array(
+           'Opera' => 'Opera',
+           'Mozilla Firefox'=> '(Firebird)|(Firefox)',
+           'Galeon' => 'Galeon',
+           'Mozilla'=>'Gecko',
+           'MyIE'=>'MyIE',
+           'Lynx' => 'Lynx',
+           'Netscape' => '(Mozilla/4\.75)|(Netscape6)|(Mozilla/4\.08)|(Mozilla/4\.5)|(Mozilla/4\.6)|(Mozilla/4\.79)',
+           'Konqueror'=>'Konqueror',
+           'Internet Explorer 8' => '(MSIE 8\.[0-9]+)',
+           'Internet Explorer 7' => '(MSIE 7\.[0-9]+)',
+           'Internet Explorer 6' => '(MSIE 6\.[0-9]+)',
+           'Internet Explorer 5' => '(MSIE 5\.[0-9]+)',
+           'Internet Explorer 4' => '(MSIE 4\.[0-9]+)',
+ );
+ foreach($browsers as $browser=>$pattern){
+        if (eregi($pattern, $user_agent))
+        return $browser;
+     }
+ return 'Unknown';
+ }
+
+if((looking_for_browser($_SERVER['HTTP_USER_AGENT'])=='Internet Explorer 8')||(looking_for_browser($_SERVER['HTTP_USER_AGENT'])=='Internet Explorer 7')||(looking_for_browser($_SERVER['HTTP_USER_AGENT'])=='Internet Explorer 6')){
+    $html.="
+        <div class='content' style='width:360px;height: expression( this.scrollHeight > 319 ? \'320px\' : \'auto\' ); /* sets max-height for IE */  max-height: 320px; /* sets max-height value for all standards-compliant browsers */  overflow:hidden;'>
+        <div id='child_$rootFolder'></div>
   </div></div>
   ";
+
   $html.='
+  <div class="boxBottom" style="width:360px;overflow:hidden;">
+		<div class="a"></div>
+		<div class="b"></div>
+		<div class="c"></div>
+	</div>
+	</div>';
+
+}else{
+    $html.="
+        <div class='content'>
+        <div id='child_$rootFolder'></div>
+  </div></div>
+  ";
+    $html.='
   <div class="boxBottom">
 		<div class="a"></div>
 		<div class="b"></div>
@@ -43,6 +86,12 @@ $html = '
 	</div>
 	</div>';
 
+}
+
+
+  //end add
+
+  
 
 	$html.='<div class="treeBase" style="width:360px;height:200px; border		: 0px solid #006699; overflow	: hidden;">
 	<div class="boxTop">
