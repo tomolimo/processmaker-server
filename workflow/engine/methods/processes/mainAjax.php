347a89d1fb679153f89108c3d00cca693bb6b646
Erik Amaru Ortiz <erik@colosa.com>
2010-10-28 21:51:46 +0000

    setting the default values for process category dropdown into processes list

--- workflow/engine/methods/processes/mainAjax.php
+++ workflow/engine/methods/processes/mainAjax.php
@@ -4,10 +4,16 @@ $request = $_POST['request'];
 
 switch($request){
   case 'categoriesList':
-    $r = Array(Array('CAT_ID'=>1, 'CAT_NAME'=>'uno'), Array('CAT_ID'=>2, 'CAT_NAME'=>'dos'), Array('CAT_ID'=>3, 'CAT_NAME'=>'tres'));
     require_once "classes/model/ProcessCategory.php";
+    
     $processCategory = new ProcessCategory;
-    $response->rows = $processCategory->getAll('array');
+    $defaultOption = Array();
+    $defaultOption[] = Array('CATEGORY_UID'=>'<reset>', 'CATEGORY_NAME'=>G::LoadTranslation('ID_ALL'));
+    $defaultOption[] = Array('CATEGORY_UID'=>'', 'CATEGORY_NAME'=>G::LoadTranslation('ID_PROCESS_NO_CATEGORY'));
+    
+    $response->rows = array_merge($defaultOption, $processCategory->getAll('array'));
+    
     echo G::json_encode($response); 
+    
   break;
 }
