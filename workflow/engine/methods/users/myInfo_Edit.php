4a157813cbaed96af9628369b55f1e2004357918
Erik Amaru Ortiz <erik@colosa.com>
2010-11-24 15:34:09 +0000

    updating the users permision

--- workflow/engine/methods/users/myInfo_Edit.php
+++ workflow/engine/methods/users/myInfo_Edit.php
@@ -23,31 +23,31 @@
  *
  */
 try {
-	global $RBAC;
-	$RBAC->requirePermissions('PM_USERS'); 
+  global $RBAC;
+  $RBAC->requirePermissions('PM_LOGIN'); 
 
   // deprecated the class XmlForm_Field_Image is currently part of the class.xmlform.php package
   // the use of the external xmlfield_Image is highly discouraged
   if (!class_exists('XmlForm_Field_Image')){
     G::LoadClass('xmlfield_Image');
   }
-	require_once 'classes/model/Users.php';
-	require_once 'classes/model/Department.php';
+  require_once 'classes/model/Users.php';
+  require_once 'classes/model/Department.php';
 
-	unset($_SESSION['CURRENT_USER']);
-	$oUser                    = new Users();
-	$aFields                  = $oUser->load($_SESSION['USER_LOGGED']);
-	$aFields['USR_PASSWORD']  = '********';
-	$aFields['MESSAGE0']      = G::LoadTranslation('ID_USER_REGISTERED') . '!';
-	$aFields['MESSAGE1']      = G::LoadTranslation('ID_MSG_ERROR_USR_USERNAME');
-	$aFields['MESSAGE2']      = G::LoadTranslation('ID_MSG_ERROR_DUE_DATE');
-	$aFields['MESSAGE3']      = G::LoadTranslation('ID_NEW_PASS_SAME_OLD_PASS');
-	$aFields['MESSAGE4']      = G::LoadTranslation('ID_MSG_ERROR_USR_FIRSTNAME');
-	$aFields['MESSAGE5']      = G::LoadTranslation('ID_MSG_ERROR_USR_LASTNAME');
-	$aFields['NO_RESUME']     = G::LoadTranslation('ID_NO_RESUME');
-	$aFields['START_DATE']    = date('Y-m-d');
-	$aFields['END_DATE']      = date('Y-m-d', mktime(0, 0, 0, date('m'), date('d'), date('Y') + 5));
-	$aFields['RANDOM']        = rand();
+  unset($_SESSION['CURRENT_USER']);
+  $oUser                    = new Users();
+  $aFields                  = $oUser->load($_SESSION['USER_LOGGED']);
+  $aFields['USR_PASSWORD']  = '********';
+  $aFields['MESSAGE0']      = G::LoadTranslation('ID_USER_REGISTERED') . '!';
+  $aFields['MESSAGE1']      = G::LoadTranslation('ID_MSG_ERROR_USR_USERNAME');
+  $aFields['MESSAGE2']      = G::LoadTranslation('ID_MSG_ERROR_DUE_DATE');
+  $aFields['MESSAGE3']      = G::LoadTranslation('ID_NEW_PASS_SAME_OLD_PASS');
+  $aFields['MESSAGE4']      = G::LoadTranslation('ID_MSG_ERROR_USR_FIRSTNAME');
+  $aFields['MESSAGE5']      = G::LoadTranslation('ID_MSG_ERROR_USR_LASTNAME');
+  $aFields['NO_RESUME']     = G::LoadTranslation('ID_NO_RESUME');
+  $aFields['START_DATE']    = date('Y-m-d');
+  $aFields['END_DATE']      = date('Y-m-d', mktime(0, 0, 0, date('m'), date('d'), date('Y') + 5));
+  $aFields['RANDOM']        = rand();
 
   //getting the user and department
   $oDepInfo = new Department();
@@ -78,76 +78,71 @@ try {
   }
   
   $G_MAIN_MENU              = 'processmaker';
-	$G_ID_MENU_SELECTED       = 'MY_ACCOUNT';
-	$G_PUBLISH                = new Publisher;
+  $G_ID_MENU_SELECTED       = 'MY_ACCOUNT';
+  $G_PUBLISH                = new Publisher;
 
 
-	//$RBAC->systemObj->loadByCode('PROCESSMAKER');//('PROCESSMAKER', $_SESSION['USER_LOGGED']);
-	
-	#verifying if it has any preferences on the configurations table
-	G::loadClass('configuration');
-	$oConf = new Configurations; 
-	$oConf->loadConfig($x, 'USER_PREFERENCES','','',$_SESSION['USER_LOGGED'],'');
-	
-	//echo $RBAC->aUserInfo['PROCESSMAKER']['ROLE']['ROL_CODE'];
-	//G::pr($RBAC->userObj->load($_SESSION['USER_LOGGED']));
-	if( sizeof($oConf->Fields) > 0){ #this user has a configuration record
-		$aFields['PREF_DEFAULT_LANG'] = $oConf->aConfig['DEFAULT_LANG'];
-		$aFields['PREF_DEFAULT_MENUSELECTED'] = $oConf->aConfig['DEFAULT_MENU'];
-	} else {
-		switch($RBAC->aUserInfo['PROCESSMAKER']['ROLE']['ROL_CODE']){
-			case 'PROCESSMAKER_ADMIN':
-				$aFields['PREF_DEFAULT_MENUSELECTED'] = 'PM_USERS';
-				break;
-
-			case 'PROCESSMAKER_OPERATOR':
-				$aFields['PREF_DEFAULT_MENUSELECTED'] = 'PM_CASES';
-				break;
-			
-		}
-		$aFields['PREF_DEFAULT_LANG'] = SYS_LANG;
-	}
-	//G::pr($RBAC->aUserInfo);
-	$rows[] = Array('id'=>'char', 'name'=>'char');
-
-
-	foreach($RBAC->aUserInfo['PROCESSMAKER']['PERMISSIONS'] as $permission){
-		
-		switch($permission['PER_CODE']){
-			case 'PM_USERS':	
-				 $rows[] = Array('id'=>'PM_USERS', 'name'=>strtoupper(G::LoadTranslation('ID_USERS'))); 
-			break;
-			case 'PM_CASES':	
-				 $rows[] = Array('id'=>'PM_CASES', 'name'=>strtoupper(G::LoadTranslation('ID_CASES'))); 
-			break;
-			case 'PM_FACTORY':	
-				 $rows[] = Array('id'=>'PM_FACTORY', 'name'=>strtoupper(G::LoadTranslation('ID_APPLICATIONS'))); 
-			break;
-			case 'PM_DASHBOARD':	
-				 $rows[] = Array('id'=>'PM_DASHBOARD', 'name'=>strtoupper(G::LoadTranslation('ID_DASHBOARD'))); 
-			break;
-			case 'PM_SETUP':	
-				 $rows[] = Array('id'=>'PM_SETUP', 'name'=>strtoupper(G::LoadTranslation('ID_SETUP'))); 
-			break;
-		}
-	}
-	//G::pr($rows); die;
-	global $_DBArray;
-	$_DBArray['menutab']   = $rows;
-	$_SESSION['_DBArray'] = $_DBArray;
-	G::LoadClass('ArrayPeer');
-	$oCriteria = new Criteria('dbarray');
-	$oCriteria->setDBArrayTable('menutab');
- 
-	if ($RBAC->userCanAccess('PM_EDITPERSONALINFO') == 1) { //he has permitions for edit his profile
-		$G_PUBLISH->AddContent('xmlform', 'xmlform', 'users/myInfo.xml', '', $aFields, 'myInfo_Save');
-	}	else { //he has not permitions for edit his profile, so just view mode will be displayed
-		$G_PUBLISH->AddContent('xmlform', 'xmlform', 'users/myInfo2.xml', '', $aFields, '');
-	}
+  //$RBAC->systemObj->loadByCode('PROCESSMAKER');//('PROCESSMAKER', $_SESSION['USER_LOGGED']);
   
-	G::RenderPage('publish');
+  #verifying if it has any preferences on the configurations table
+  G::loadClass('configuration');
+  $oConf = new Configurations; 
+  $oConf->loadConfig($x, 'USER_PREFERENCES','','',$_SESSION['USER_LOGGED'],'');
+  
+  //echo $RBAC->aUserInfo['PROCESSMAKER']['ROLE']['ROL_CODE'];
+  //G::pr($RBAC->userObj->load($_SESSION['USER_LOGGED']));
+  if( sizeof($oConf->Fields) > 0){ #this user has a configuration record
+    $aFields['PREF_DEFAULT_LANG'] = $oConf->aConfig['DEFAULT_LANG'];
+    $aFields['PREF_DEFAULT_MENUSELECTED'] = $oConf->aConfig['DEFAULT_MENU'];
+  } else {
+    switch($RBAC->aUserInfo['PROCESSMAKER']['ROLE']['ROL_CODE']){
+      case 'PROCESSMAKER_ADMIN':
+        $aFields['PREF_DEFAULT_MENUSELECTED'] = 'PM_USERS';
+        break;
+
+      case 'PROCESSMAKER_OPERATOR':
+        $aFields['PREF_DEFAULT_MENUSELECTED'] = 'PM_CASES';
+        break;
+      
+    }
+    $aFields['PREF_DEFAULT_LANG'] = SYS_LANG;
+  }
+  //G::pr($RBAC->aUserInfo);
+  $rows[] = Array('id'=>'char', 'name'=>'char');
+
+
+  foreach($RBAC->aUserInfo['PROCESSMAKER']['PERMISSIONS'] as $permission){
+    
+    switch($permission['PER_CODE']){
+      case 'PM_USERS':  
+      case 'PM_SETUP':  
+         $rows[] = Array('id'=>'PM_SETUP', 'name'=>strtoupper(G::LoadTranslation('ID_SETUP'))); 
+      break;
+      case 'PM_CASES':  
+         $rows[] = Array('id'=>'PM_CASES', 'name'=>strtoupper(G::LoadTranslation('ID_CASES'))); 
+      break;
+      case 'PM_FACTORY':  
+         $rows[] = Array('id'=>'PM_FACTORY', 'name'=>strtoupper(G::LoadTranslation('ID_APPLICATIONS'))); 
+      break;
+    }
+  }
+  //G::pr($rows); die;
+  global $_DBArray;
+  $_DBArray['menutab']   = $rows;
+  $_SESSION['_DBArray'] = $_DBArray;
+  G::LoadClass('ArrayPeer');
+  $oCriteria = new Criteria('dbarray');
+  $oCriteria->setDBArrayTable('menutab');
+ 
+  if ($RBAC->userCanAccess('PM_EDITPERSONALINFO') == 1) { //he has permitions for edit his profile
+    $G_PUBLISH->AddContent('xmlform', 'xmlform', 'users/myInfo.xml', '', $aFields, 'myInfo_Save');
+  }  else { //he has not permitions for edit his profile, so just view mode will be displayed
+    $G_PUBLISH->AddContent('xmlform', 'xmlform', 'users/myInfo2.xml', '', $aFields, '');
+  }
+  
+  G::RenderPage('publish');
 }
 catch (Exception $oException) {
-	die($oException->getMessage());
+  die($oException->getMessage());
 }
 ?>
