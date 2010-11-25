3099e9cc57ff0d89eb40093ecab78fbf3202ef39
Alexandre Rosenfeld <alexandre@colosa.com>
2010-11-25 13:22:31 +0000

    Clean drafts: added date, remove from table CONTENT and files

--- workflow/engine/bin/commands/cmdDrafts.php
+++ workflow/engine/bin/commands/cmdDrafts.php
@@ -1,61 +1,88 @@
 <?php
 
-pake_task('drafts');
+pake_task('drafts-clean');
 
-function delete_app_from_table($con, $table_name, $app_uid) {
+function delete_app_from_table($con, $tableName, $appUid, $col="APP_UID") {
   $stmt = $con->createStatement();
-  $sql = "DELETE FROM " . $table_name . " WHERE APP_UID='" . $app_uid . "'";
+  $sql = "DELETE FROM " . $tableName . " WHERE " . $col . "='" . $appUid . "'";
   $rs = $stmt->executeQuery($sql, ResultSet::FETCHMODE_NUM);
 }
 
-function run_drafts($task, $args)
+function run_drafts_clean($task, $args)
 {
-  if (count($args) < 1)
-    throw new Exception("Command not specified");
-  if (strcmp($args[0], 'clean') == 0) {
-    echo "Cleaning drafts\n";
+  echo "Cleaning drafts\n";
     
-    if (count($args) < 2)
-      throw new Exception ("Please specify a workspace name");
-    $workspace = $args[1];
+  if (count($args) < 1)
+    throw new Exception ("Please specify a workspace name");
+  $workspace = $args[0];
 
-    if (!file_exists(PATH_DB . $workspace . '/db.php')) {
-      throw new Exception('Could not find workspace ' . $workspace);
+  $allDrafts = false;
+  if (count($args) < 2) {
+    echo "Type how much days of drafts to delete (ENTER for all): ";
+    $days = rtrim( fgets( STDIN ), "\n" );
+    if ($days == "") {
+      $allDrafts = true;
     }
-    /* Load the configuration from the workspace */
-    require_once( PATH_DB . $workspace . '/db.php' );
-
-    require_once( PATH_THIRDPARTY . 'propel/Propel.php');
-
-    PROPEL::Init ( PATH_METHODS.'dbConnections/rootDbConnections.php' );
-    $con = Propel::getConnection("root");
-
-    $stmt = $con->createStatement();
-
-    /* Search for all the draft cases */
-    $sql = "SELECT APP_UID FROM APPLICATION WHERE APP_STATUS='DRAFT'";
-    $app_list = $stmt->executeQuery($sql, ResultSet::FETCHMODE_ASSOC);
-
-    /* Tables to remove the cases from */
-    $tables = array(
-        "APPLICATION",
-        "APP_DELEGATION",
-        "APP_CACHE_VIEW",
-        "APP_THREAD",
-        "APP_DOCUMENT",
-        "APP_EVENT",
-        "APP_HISTORY",
-        "APP_MESSAGE"
-    );
-
-    echo "Found " . $app_list->getRecordCount() . " cases to remove";
-    foreach ($app_list as $row) {
-      echo ".";
-      foreach ($tables as $table) {
-        delete_app_from_table($con, $table, $row['APP_UID']);
-      }
+  } else {
+    $days = $args[1];
+    if (strcmp($days, "all") == 0) {
+      $allDrafts = true;
     }
-    echo "\n";
   }
+
+  if ($allDrafts)
+    echo "Removing all drafts\n";
+
+  if (!$allDrafts && !is_numeric($days)) {
+    throw new Exception("Days value is not valid: " . $days);
+  }
+
+  if (!file_exists(PATH_DB . $workspace . '/db.php')) {
+    throw new Exception('Could not find workspace ' . $workspace);
+  }
+  /* Load the configuration from the workspace */
+  require_once( PATH_DB . $workspace . '/db.php' );
+
+  require_once( PATH_THIRDPARTY . 'propel/Propel.php');
+
+  PROPEL::Init ( PATH_METHODS.'dbConnections/rootDbConnections.php' );
+  $con = Propel::getConnection("root");
+
+  $stmt = $con->createStatement();
+
+  if (!$allDrafts)
+    $dateSql = "AND DATE_SUB(CURDATE(),INTERVAL " . $days . " DAY) <= APP_CREATE_DATE";
+  else
+    $dateSql = "";
+  /* Search for all the draft cases */
+  $sql = "SELECT APP_UID FROM APPLICATION WHERE APP_STATUS='DRAFT'" . $dateSql;
+  $appRows = $stmt->executeQuery($sql, ResultSet::FETCHMODE_ASSOC);
+
+  /* Tables to remove the cases from */
+  $tables = array(
+      "APPLICATION",
+      "APP_DELEGATION",
+      "APP_CACHE_VIEW",
+      "APP_THREAD",
+      "APP_DOCUMENT",
+      "APP_EVENT",
+      "APP_HISTORY",
+      "APP_MESSAGE"
+  );
+
+  echo "Found " . $appRows->getRecordCount() . " cases to remove";
+  foreach ($appRows as $row) {
+    echo ".";
+    $appUid = $row['APP_UID'];
+    foreach ($tables as $table) {
+      delete_app_from_table($con, $table, $appUid);
+    }
+    delete_app_from_table($con, "CONTENT", $appUid, "CON_ID");
+    if (file_exists(PATH_DB . $workspace . '/files/'. $appUid)) {
+      echo "\nRemoving files from " . $appUid . "\n";
+      G::rm_dir(PATH_DB . $workspace . '/files/'. $appUid);
+    }
+  }
+  echo "\n";
 }
 ?>
