37221c8e7780888d7c5c40be5cf61b5894964c6c
Erik Amaru Ortiz <erik@colosa.com>
2010-11-29 15:23:27 +0000

    FIXED: problem when this js error occur (SETUP) oCasesSubFrame.style.height = height-10; Error: oCasesSubFrame is null

--- workflow/engine/templates/setup/main_Load.php
+++ workflow/engine/templates/setup/main_Load.php
@@ -5,25 +5,21 @@
 </body>
 <script>
   oClientWinSize = getClientWindowSize();
-  h = getStyle(document.getElementById('pm_menu'),'top');
-  h = h.replace("px", "");
-  h = parseInt(h) + 18;
   if ( document.getElementById('pm_submenu') ) 
     document.getElementById('pm_submenu').style.display = 'none';
   document.documentElement.style.overflowY = 'hidden';
+  
   function autoResizeScreen() {
     oCasesFrame    = document.getElementById('adminFrame');
     oClientWinSize = getClientWindowSize();
     height = oClientWinSize.height-105;
     oCasesFrame.style.height = height;
     oCasesSubFrame = oCasesFrame.contentWindow.document.getElementById('setup-frame');
-    oCasesSubFrame.style.height = height-10;
-  }
-  function getStyle(targetElement,styleProp) {
-    if (targetElement) {
-      if (targetElement.currentStyle) return targetElement.currentStyle[styleProp];
-      else if (window.getComputedStyle) return document.defaultView.getComputedStyle(targetElement,null).getPropertyValue(styleProp);
-    }
+
+    if(oCasesSubFrame)
+      oCasesSubFrame.style.height = height-10;
+    else
+      setTimeout('autoResizeScreen()', 2000);
   }
 </script>
 </html>
