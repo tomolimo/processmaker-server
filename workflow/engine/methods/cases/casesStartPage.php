92e61d6d699dc422823752105b90ed73960825b0
Erik Amaru Ortiz <erik@colosa.com>
2010-12-01 14:31:15 +0000

    litle fix for a missing js file 'ux/TabCloseMenu' in cases/casesStartPage.php

--- workflow/engine/methods/cases/casesStartPage.php
+++ workflow/engine/methods/cases/casesStartPage.php
@@ -5,9 +5,6 @@
   }
 
   $oHeadPublisher =& headPublisher::getSingleton();
-  //$oHeadPublisher->setExtSkin( 'xtheme-gray');
-  $oHeadPublisher->usingExtJs('ux/TabCloseMenu');
-  //$oHeadPublisher->usingExtJs('ux/ColumnHeaderGroup');
   $oHeadPublisher->addExtJsScript('cases/casesStartPage', false);    //adding a javascript file .js
   $oHeadPublisher->addContent( 'cases/casesStartPage'); //adding a html file  .html.
 
