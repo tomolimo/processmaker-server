8ebf83691928add3eea925fdea56a8311720aaa4
Fernando Ontiveros <fernando@colosa.com>
2010-12-08 14:02:43 +0000

    Fixed bug 35, google doc list, the repeated calendar names are now validated.

---
+++ workflow/engine/methods/setup/calendarValidate.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * calendarValidate.php
+ *
+ * ProcessMaker Open Source Edition
+ * Copyright (C) 2004 - 2008 Colosa Inc.23
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * For more information, contact Colosa Inc, 2566 Le Jeune Rd.,
+ * Coral Gables, FL, 33134, USA, or email info@colosa.com.
+ *
+ */
+// validating the fields of the Calendar Edit form.
+// checking permissions
+switch ($RBAC->userCanAccess('PM_SETUP_ADVANCE'))
+{
+	case -2:
+	  G::SendTemporalMessage('ID_USER_HAVENT_RIGHTS_SYSTEM', 'error', 'labels');
+	  G::header('location: ../login/login');
+	  die;
+	break;
+	case -1:
+	  G::SendTemporalMessage('ID_USER_HAVENT_RIGHTS_PAGE', 'error', 'labels');
+	  G::header('location: ../login/login');
+	  die;
+	break;
+}
+// validating the fields
+$message = array();
+switch ($_POST['action']){
+  case 'calendarName':
+    require_once ('classes/model/CalendarDefinition.php');
+    $oCalendar  = new CalendarDefinition();
+    $aCalendars = $oCalendar->getCalendarList(false,true);
+//    var_dump($_POST['name']);
+//    var_dump($aCalendars);
+    //$count = 0;
+    $aCalendarDefinitions = end($aCalendars);
+//    var_dump($aCalendarDefinitions);
+    foreach($aCalendarDefinitions as $aDefinitions) {
+      if ($aDefinitions['CALENDAR_NAME']!=$_POST['name']){
+        $validated = true;
+      } else {
+        $validated = false;
+        $message  = G::loadTranslation('ID_CALENDAR_INVALID_NAME');
+        break;
+      }
+    }
+    break;
+  case 'calendarDates':
+        $validated = false;
+        $message = G::loadTranslation('ID_CALENDAR_INVALID_WORK_DATES');
+    break;
+}
+if (!$validated){
+    echo ($message);
+}
+
