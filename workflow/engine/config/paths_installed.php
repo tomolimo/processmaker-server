a30c207c3e1219e945972c66f88da93f64e008a4
Alexandre Rosenfeld <alexandre@colosa.com>
2010-12-17 13:40:35 +0000

    Remove wrong extra files from previous commit.

--- workflow/engine/config/paths_installed.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define( 'PATH_DATA', '/shared/workflow_data/' );
-define( 'PATH_C',    PATH_DATA . 'compiled/' );
-define( 'HASH_INSTALLATION','0tCWxtHN1NaqlJnInZiZlZqYnZRqlGaXmsdnlWKRYpWcw2PJlZWVk5yl1dKslGeXycqclGiUmMhulWSWlJVnYZjCZcqbx5XHZmOWk56UqKHU0qNjYpFnxmnHlcmVk2ZjzMRrmJmXychtZJjEaphqlJTJZ2FikZiW' );
-define( 'SYSTEM_HASH','a3eeeec63fc8342de712a2e6b0d0000f' );
