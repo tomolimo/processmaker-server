74d7795ef48a1dc74a92293bc6bac11c45e14328
Alexandre Rosenfeld <alexandre@colosa.com>
2011-01-11 15:23:41 +0000

    Support CLI on Windows.

---
+++ processmaker.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+for %%F in (%0) do set dirname=%%~dpF
+SET PHP_PATH="%dirname%..\php"
+SET PHP_BIN="%dirname%..\php\php.exe"
+SET PROCESSMAKER_PATH="%dirname%workflow\engine\bin"
+SET PROCESSMAKER_BIN="%dirname%processmaker"
+SET PATH=%PATH%;%PHP_PATH%
+%PHP_BIN% %PROCESSMAKER_BIN% %1 %2 %3 %4 %5 %6 %7 %8 %9
+pause
