a7f967435e7ff066f0779295ea4e6f96711cb7ed
Carlos Pacha <carlos@colosa.com>
2011-01-26 21:22:00 +0000

    it is related 6018 bug, some variables are defined

--- workflow/engine/classes/class.spool.php
+++ workflow/engine/classes/class.spool.php
@@ -250,6 +250,8 @@ class spoolRun {
     $holdcc = array ();
     $holdbcc = array ();
     $text = trim($this->fileData['to']);
+    $textcc ='';
+    $textbcc=''; 
     if( isset($this->fileData['cc']) && trim($this->fileData['cc']) != '' ) {
       $textcc = trim($this->fileData['cc']);
     }
