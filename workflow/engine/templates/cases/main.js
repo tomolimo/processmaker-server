8a4fe643859f8c499d35aa1a8f5ac433a148fed2
Erik Amaru Ortiz <erik@colosa.com>
2011-02-22 23:09:39 +0000

    bug fixed 6081: fixed

--- workflow/engine/templates/cases/main.js
+++ workflow/engine/templates/cases/main.js
@@ -535,48 +535,45 @@ function updateCasesView() {
 }
 
 function updateCasesTree() {
-  try{
-    //treeMenuItems.root.reload();
-    Ext.getCmp('refreshNotifiers').setIcon('/skins/ext/images/default/grid/loading.gif');
-    document.getElementById('ext-gen35').focus();
-    
-    itemsTypes = Array('CASES_INBOX', 'CASES_DRAFT', 'CASES_CANCELLED', 'CASES_SENT', 'CASES_PAUSED', 'CASES_COMPLETED','CASES_SELFSERVICE');
-    if(currentSelectedTreeMenuItem){
-      ReloadTreeMenuItemDetail({item:currentSelectedTreeMenuItem});
-    }
-    Ext.Ajax.request({
-      url: 'casesMenuLoader?action=getAllCounters&r='+Math.random(),
-      success: function(response){
-      	try{
-	        result = eval('('+response.responseText+')');
-	        for(i=0; i<result.length; i++){
-	        	if( document.getElementById('NOTIFIER_'+result[i].item ) ){
-		          oldValue = document.getElementById('NOTIFIER_'+result[i].item).innerHTML;
-		          oldValue = oldValue.replace('<b>', '');
-		          oldValue = oldValue.replace('</b>', '');
-		          
-		          newValue = result[i].count;
+  
+  //treeMenuItems.root.reload();
+  Ext.getCmp('refreshNotifiers').setIcon('/skins/ext/images/default/grid/loading.gif');
+  document.getElementById('ext-gen35').focus();
 
-		          if( oldValue != newValue && oldValue != 0 ){
-		            document.getElementById('NOTIFIER_'+result[i].item).innerHTML = '<b>' + result[i].count + '</b>';
-		            //NOTIFIER_FLAG = true;
-		          } else { 
-		          	//if(NOTIFIER_FLAG === false){
-		              document.getElementById('NOTIFIER_'+result[i].item).innerHTML = result[i].count;
-		            //}
-		          }
-		        } 
-		        else continue;
-	        }
-	        Ext.getCmp('refreshNotifiers').setIcon('/images/refresh.gif');
-	      } catch (e){
-	      	//alert(""+e);
-	      }
-      },
-      failure: function(){},
-      params: {'updateCasesTree': true}
-    });
-  } catch(e){alert(' '+e)}
+  itemsTypes = Array('CASES_INBOX', 'CASES_DRAFT', 'CASES_CANCELLED', 'CASES_SENT', 'CASES_PAUSED', 'CASES_COMPLETED','CASES_SELFSERVICE');
+  if(currentSelectedTreeMenuItem){
+    ReloadTreeMenuItemDetail({item:currentSelectedTreeMenuItem});
+  }
+  Ext.Ajax.request({
+    url: 'casesMenuLoader?action=getAllCounters&r='+Math.random(),
+    success: function(response){
+
+      var result = Ext.util.JSON.decode(response.responseText);
+      for(i=0; i<result.length; i++){
+        if( document.getElementById('NOTIFIER_'+result[i].item ) ){
+          oldValue = document.getElementById('NOTIFIER_'+result[i].item).innerHTML;
+          oldValue = oldValue.replace('<b>', '');
+          oldValue = oldValue.replace('</b>', '');
+
+          newValue = result[i].count;
+
+          if( oldValue != newValue && oldValue != 0 ){
+            document.getElementById('NOTIFIER_'+result[i].item).innerHTML = '<b>' + result[i].count + '</b>';
+            //NOTIFIER_FLAG = true;
+          } else {
+            //if(NOTIFIER_FLAG === false){
+              document.getElementById('NOTIFIER_'+result[i].item).innerHTML = result[i].count;
+            //}
+          }
+        }
+        else continue;
+      }
+      Ext.getCmp('refreshNotifiers').setIcon('/images/refresh.gif');
+    },
+    failure: function(){},
+    params: {'updateCasesTree': true}
+  });
+  
 }
 
 //the timer function will be called after 2 minutes;
