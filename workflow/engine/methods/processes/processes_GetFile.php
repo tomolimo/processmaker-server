7f7a0eeab2a48fe9beacad5893af464e80a1d681
Carlos Pacha <carlos@colosa.com>
2011-03-30 10:38:49 -0400

    BUG 6589:I have been able to reproduce the issue, to solve this issue I have added a piece of code to allow to download this type of file

--- workflow/engine/methods/processes/processes_GetFile.php
+++ workflow/engine/methods/processes/processes_GetFile.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 switch ($_GET['MAIN_DIRECTORY']) {
   case 'mailTemplates':
     $sDirectory = PATH_DATA_MAILTEMPLATES . $_GET['PRO_UID'] . PATH_SEP . ($_GET['CURRENT_DIRECTORY'] != '' ? $_GET['CURRENT_DIRECTORY'] . PATH_SEP : '');
@@ -9,7 +9,10 @@ switch ($_GET['MAIN_DIRECTORY']) {
   default:
     die;
   break;
-}
+} 
+//fixed: added a file extension when is a javascript file by krlos
+$_GET['FILE'] .= ($_GET['sFilextension']!='' && $_GET['sFilextension']=='javascript')?'.js':'';
+
 if (file_exists($sDirectory . $_GET['FILE'])) {
   G::streamFile($sDirectory . $_GET['FILE'], true);
 }
