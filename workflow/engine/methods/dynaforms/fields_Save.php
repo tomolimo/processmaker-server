8b237e3c52c56a1f0313fca6a0ecb07b2bb04f68
Hector Cortez <hector@colosa.com>
2011-06-29 15:29:00 -0400

    BUG 7145 Change delimiter for validate parser at generate xml file.

--- workflow/engine/methods/dynaforms/fields_Save.php
+++ workflow/engine/methods/dynaforms/fields_Save.php
@@ -83,7 +83,7 @@ if (($RBAC_Response=$RBAC->userCanAccess("PM_FACTORY"))!=1) return $RBAC_Respons
   define('DB_XMLDB_TYPE','myxml');
 
   if (isset($_POST['form']['PME_XMLNODE_VALUE'])){
-    $_POST['form']['PME_XMLNODE_VALUE'] = str_replace("'", "\"" , $_POST['form']['PME_XMLNODE_VALUE']);
+    $_POST['form']['PME_XMLNODE_VALUE'] = str_replace("'", "''" , $_POST['form']['PME_XMLNODE_VALUE']);
   }
 
   if (file_exists( PATH_XMLFORM . 'dynaforms/fields/' . $type . '.xml')) {
