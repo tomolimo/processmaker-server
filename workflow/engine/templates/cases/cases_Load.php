b91e4d3ddec7dc8f1400f5b861b029ac780f93d5
Douglas Medrano Chura <douglas@colosa.com>
2011-06-30 09:28:08 -0400

    BUG 6796  Resolved horizontal scroll when the browser rendering
    The problem was solved by modifying the code that calculates the height of a window and too resolved
    the scroll vertical in Internet Explorer to make it look like you see in Firefox (Firefox hides the  scroll main vertical)

--- workflow/engine/templates/cases/cases_Load.php
+++ workflow/engine/templates/cases/cases_Load.php
@@ -1,5 +1,12 @@
 <html>
-  <style>.Footer .content{padding   :0px !important;}</style>
+  <style type="text/css">
+   .Footer .content {
+      padding   :0px !important;
+   }  
+   *html body {
+      overflow-y: hidden;
+   }
+  </style>
   <body onresize="autoResizeScreen()" onload="autoResizeScreen()">
   <iframe name="casesFrame" id="casesFrame" src ="../cases/main_init" width="99%" height="768" frameborder="0">
     <p>Your browser does not support iframes.</p>
@@ -8,21 +15,25 @@
   <script>
     if ( document.getElementById('pm_submenu') )
       document.getElementById('pm_submenu').style.display = 'none';
-    document.documentElement.style.overflowY = 'hidden';
+      document.documentElement.style.overflowY = 'hidden';
 
-    var oClientWinSize = getClientWindowSize();
+      var oClientWinSize = getClientWindowSize();
+      
+
     
     function autoResizeScreen() {
       oCasesFrame    = document.getElementById('casesFrame');
-      height = oClientWinSize.height-105;
+      height = getClientWindowSize().height-90;
       oCasesFrame.style.height = height;
       oCasesSubFrame = oCasesFrame.contentWindow.document.getElementById('casesSubFrame');
+        if(oCasesSubFrame){
+          oCasesSubFrame.style.height = height-5;
+        }
+        else {
+          setTimeout('autoResizeScreen()', 2000);
+        }
 
-      if(oCasesSubFrame)
-        oCasesSubFrame.style.height = height-10;
-      else {
-        setTimeout('autoResizeScreen()', 2000);
-      }
+
     }
   </script>
 </html>
--- workflow/engine/templates/setup/main_Load.php
+++ workflow/engine/templates/setup/main_Load.php
@@ -1,4 +1,9 @@
 <body onresize="autoResizeScreen()" onload="autoResizeScreen()">
+  <style type="text/css">
+   *html body {
+      overflow-y: hidden;
+   }
+  </style>
 <iframe name="adminFrame" id="adminFrame" src ="main_init" width="99%" height="768" frameborder="0">
   <p>Your browser does not support iframes.</p>
 </iframe>
@@ -12,12 +17,12 @@
   function autoResizeScreen() {
     oCasesFrame    = document.getElementById('adminFrame');
     oClientWinSize = getClientWindowSize();
-    height = oClientWinSize.height-105;
+    height = oClientWinSize.height-90;
     oCasesFrame.style.height = height;
     oCasesSubFrame = oCasesFrame.contentWindow.document.getElementById('setup-frame');
 
     if(oCasesSubFrame)
-      oCasesSubFrame.style.height = height-10;
+      oCasesSubFrame.style.height = height-5;
     else
       setTimeout('autoResizeScreen()', 2000);
   }
