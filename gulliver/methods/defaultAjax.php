37f38e9d1647f7a32e0743904fe2cbd79f261d9f
Enrique Ponce De Leon <enrique@colosa.com>
2011-08-11 15:29:58 -0400

    BUG 0000 Undefined values fixing with multiple dependent fields.

--- gulliver/methods/defaultAjax.php
+++ gulliver/methods/defaultAjax.php
@@ -83,7 +83,7 @@
             $value = (array) $value;
             $arrayK = $value[$r2];
             foreach ($arrayK as $key2 => $val) {
-              $fieldBase[$r2][$key2] = $val[$key2];
+              $fieldBase[$r2][$key2] = isset($val[$key2])? $val[$key2] : '';
             }
           }
         }
