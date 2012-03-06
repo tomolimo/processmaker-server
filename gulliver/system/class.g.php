e46761876258df76399c884514ab6288631270fa
Victor Saisa Lopez <victor@colosa.com>
2012-03-06 15:39:05 -0400

    BUG 7646 Solved the problem escaping the character "'"

--- gulliver/system/class.g.php
+++ gulliver/system/class.g.php
@@ -2205,6 +2205,9 @@ $output = $outputHeader.$output;
     }
     
     if ( isset ( $translation[$msgID] ) ){
+      $arraySearch  = array("'");
+      $arrayReplace = array("\\'");
+      
       $translationString = preg_replace("[\n|\r|\n\r]", ' ', $translation[$msgID]);
     
       if( isset($data) && is_array($data) ) {
@@ -2212,6 +2215,8 @@ $output = $outputHeader.$output;
           $translationString = str_replace('{'.$label.'}', $value, $translationString);
         }
       }
+      
+      $translationString = str_replace($arraySearch, $arrayReplace, $translationString);
       
       return $translationString;
     } else {
