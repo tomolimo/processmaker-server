d0849023828f1efbff5c8d72786d88643b0f3c8b
Victor Saisa Lopez <victor@colosa.com>
2012-03-30 09:40:34 -0400

    BUG 0000 "Added field sub-title in Dashboards" SOLVED
    - Added field sub-title in Dashboards
    - Read sub-title value in DASHLET_INSTANCE.DAS_INS_ADDITIONAL_PROPERTIES field

--- workflow/engine/classes/class.pmDashlet.php
+++ workflow/engine/classes/class.pmDashlet.php
@@ -203,6 +203,7 @@ class PMDashlet extends DashletInstance implements DashletInterface {
           
           $row['DAS_XTEMPLATE'] = $this->getXTemplate($row['DAS_CLASS']);
           $row["DAS_TITLE"] = (isset($arrayField["DAS_INS_TITLE"]) && !empty($arrayField["DAS_INS_TITLE"]))? $arrayField["DAS_INS_TITLE"] : $row["DAS_TITLE"];
+          $row["DAS_TITLE"] = $row["DAS_TITLE"] . ((isset($arrayField["DAS_INS_TITLE_SUB_TITLE"]) && !empty($arrayField["DAS_INS_TITLE_SUB_TITLE"]))? str_replace("@@USR_USERNAME", $_SESSION["USR_USERNAME"], $arrayField["DAS_INS_TITLE_SUB_TITLE"]) : null);
           
           $dashletsInstances[$row['DAS_INS_UID']] = $row;
         }
@@ -227,6 +228,7 @@ class PMDashlet extends DashletInstance implements DashletInterface {
           
           $row['DAS_XTEMPLATE'] = $this->getXTemplate($row['DAS_CLASS']);
           $row["DAS_TITLE"] = (isset($arrayField["DAS_INS_TITLE"]) && !empty($arrayField["DAS_INS_TITLE"]))? $arrayField["DAS_INS_TITLE"] : $row["DAS_TITLE"];
+          $row["DAS_TITLE"] = $row["DAS_TITLE"] . ((isset($arrayField["DAS_INS_TITLE_SUB_TITLE"]) && !empty($arrayField["DAS_INS_TITLE_SUB_TITLE"]))? str_replace("@@USR_USERNAME", $_SESSION["USR_USERNAME"], $arrayField["DAS_INS_TITLE_SUB_TITLE"]) : null);
           
           $dashletsInstances[$row['DAS_INS_UID']] = $row;
         }
@@ -253,6 +255,7 @@ class PMDashlet extends DashletInstance implements DashletInterface {
             
             $row['DAS_XTEMPLATE'] = $this->getXTemplate($row['DAS_CLASS']);
             $row["DAS_TITLE"] = (isset($arrayField["DAS_INS_TITLE"]) && !empty($arrayField["DAS_INS_TITLE"]))? $arrayField["DAS_INS_TITLE"] : $row["DAS_TITLE"];
+            $row["DAS_TITLE"] = $row["DAS_TITLE"] . ((isset($arrayField["DAS_INS_TITLE_SUB_TITLE"]) && !empty($arrayField["DAS_INS_TITLE_SUB_TITLE"]))? str_replace("@@USR_USERNAME", $_SESSION["USR_USERNAME"], $arrayField["DAS_INS_TITLE_SUB_TITLE"]) : null);
             
             $dashletsInstances[$row['DAS_INS_UID']] = $row;
           }
@@ -281,6 +284,7 @@ class PMDashlet extends DashletInstance implements DashletInterface {
             
             $row['DAS_XTEMPLATE'] = $this->getXTemplate($row['DAS_CLASS']);
             $row["DAS_TITLE"] = (isset($arrayField["DAS_INS_TITLE"]) && !empty($arrayField["DAS_INS_TITLE"]))? $arrayField["DAS_INS_TITLE"] : $row["DAS_TITLE"];
+            $row["DAS_TITLE"] = $row["DAS_TITLE"] . ((isset($arrayField["DAS_INS_TITLE_SUB_TITLE"]) && !empty($arrayField["DAS_INS_TITLE_SUB_TITLE"]))? str_replace("@@USR_USERNAME", $_SESSION["USR_USERNAME"], $arrayField["DAS_INS_TITLE_SUB_TITLE"]) : null);
             
             $dashletsInstances[$row['DAS_INS_UID']] = $row;
           }
