fcfac3f77c9d129638a68c1d96c8b8e8d4d05f49
Brayan Osmar Pereyra Suxo <brayan@colosa.com>
2012-05-09 17:19:35 -0400

    BUG 0000 fix test/bootstrap.php
    php5.2 and test for first method of Gtest ready

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,6 +4,10 @@
 // Defining the PATH_SEP constant, he we are defining if the the path separator symbol will be '\\' or '/'
 define('PATH_SEP', '/');
 
+if (!defined('__DIR__')) {
+  define ('__DIR__', dirname(__FILE__));
+}
+
 // Defining the Home Directory
 define('PATH_TRUNK', realpath(__DIR__ . '/../') . PATH_SEP);
 define('PATH_HOME',  PATH_TRUNK . 'workflow' . PATH_SEP);
