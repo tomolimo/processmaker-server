273dff47450d3e827ec8cfe7bffc354d6b845072
Herbert Saal Gutierrez <herbert.saal@colosa.com>
2012-06-18 17:58:14 -0400

    BUG 9180 Correct display priority in cases lists
    Code Issue
    ----------
    A number is display in the Priority column in the cases lists instead of the
    priority in text.
    
    Solution
    --------
    Correct display of priority column in cases lists.
    The text that describes the priority is display instead of the priority number.
    The number is converted to the corresponding text with the help of the function
    G::LoadTranslation("ID_PRIORITY_N");

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -12,6 +12,8 @@ if (!defined('__DIR__')) {
 define('PATH_TRUNK', realpath(__DIR__ . '/../') . PATH_SEP);
 define('PATH_HOME',  PATH_TRUNK . 'workflow' . PATH_SEP);
 
+define('SYS_SYS', $GLOBALS['SYS_SYS']);
+
 require  PATH_HOME . 'engine' . PATH_SEP . 'config' . PATH_SEP . 'paths.php';
 
 set_include_path(
@@ -21,3 +23,19 @@ set_include_path(
     PATH_RBAC_CORE . PATH_SEPARATOR .
     get_include_path()
 );
+
+// perpare propel env.
+require_once "propel/Propel.php";
+require_once "creole/Creole.php";
+
+Propel::init( PATH_CORE . "config/databases.php" );
+
+//initialize required classes
+G::LoadClass ('dbtable');
+G::LoadClass ('system');
+
+//read memcached configuration
+$config = System::getSystemConfiguration ('', '', SYS_SYS);
+define ('MEMCACHED_ENABLED', $config ['memcached']);
+define ('MEMCACHED_SERVER', $config ['memcached_server']);
+define ('TIME_ZONE', $config ['time_zone']);
