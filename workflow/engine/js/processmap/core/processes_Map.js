167bab6b6e194969e9ba37e5d21864eb9ba8b95b
Victor Saisa Lopez <victor@colosa.com>
2012-07-03 18:20:20 -0400

    BUG 9319 "Problema con el Case Label" SOLVED
    - By changing the "Case Label", this is not updated in the APP_CACHE_VIEW table
    - Solved problem to changing the "Case Label", now is updated in the APP_CACHE_VIEW table
    - Improvement in file, compliance with the standard PSR2
    
    Note:
    - When you change the "Case Title" of a task, affecting every case,
      if it is the current task of the case (otherwise it would not affect
      the APP_CACHE_VIEW table)
    - If a task is saved empty the "Case Title" the APP_TITLE take the value
      #APP_NUMBER (all cases are in the task, this in APP_CACHE_VIEW table)

--- workflow/engine/js/processmap/core/processes_Map.js
+++ workflow/engine/js/processmap/core/processes_Map.js
@@ -76,7 +76,7 @@ var saveDataTaskTemporal = function(iForm)
         oTaskData.TAS_TYPE_DAY     = getField('TAS_TYPE_DAY').value;
         oTaskData.TAS_CALENDAR     = getField('TAS_CALENDAR').value;
         oTaskData.TAS_TRANSFER_FLY = (getField('TAS_TRANSFER_FLY').checked ? 'TRUE' : 'FALSE');
-        
+
         var fieldEval = new input(getField('TAS_DURATION'));
         if (getField('TAS_DURATION').value.trim() == '') {
           fieldEval.failed();
@@ -178,82 +178,72 @@ var saveDataTaskTemporal = function(iForm)
 var saveTaskData = function(oForm, iForm, iType)
 {
   iLastTab = iForm;
-  
+
   if (!saveDataTaskTemporal(iForm)) {
     return false;
   }
-  
+
   oTaskData.TAS_UID = getField("TAS_UID").value;
-  
+
   /*
   while (oTaskData.TAS_TITLE.charAt(0)==" "){
   oTaskData.TAS_TITLE = oTaskData.TAS_TITLE.substring(1,oTaskData.TAS_TITLE.length) ;
   }
   */
-  
+
   oTaskData.TAS_TITLE = oTaskData.TAS_TITLE.trim();
-  
+
   if (oTaskData.TAS_TITLE == "") {
     alert(G_STRINGS.ID_REQ_TITLE );
-    
+
     return false;
   }
-  
-  //Panel processing //iForm = 6 //Case Labels
-  var pnlProcessing;
-  
-  pnlProcessing = new leimnud.module.panel();
-  
-  pnlProcessing.options = {
-    //title: "",
-    //theme: this.options.theme,
-    limit: true,
-    size: {w: 250, h: 110},
-    position: {x: 50, y: 50, center: true},
-    control: {close: false, resize: false},
-    statusBar: true,
-    fx:{shadow: true, modal: true}
-  };
-  
-  pnlProcessing.make();
-  
-  //pnlProcessing.loader.show();
-  pnlProcessing.addContent("<div style=\"margin-left: 1em; padding: 0.80em 0 1em 4em; background: url(/images/classic/loader_B.gif) no-repeat left top;\">" + _("ID_PROCESSING") + "</div>");
-  
+
   //Set AJAX
   var sParameters = "function=saveTaskData";
-  
+
   var oRPC = new leimnud.module.rpc.xmlhttp({
     url: "../tasks/tasks_Ajax",
     method: "POST",
     args: sParameters + "&oData=" + oTaskData.toJSONString()
   });
-  
+
   oRPC.callback = function (rpc) {
-    //pnlProcessing.loader.hide();
-    pnlProcessing.remove();
-    
+    var res = rpc.xmlhttp.responseText.parseJSON();
+
     if (oTaskData.TAS_TITLE) {
       Pm.data.db.task[getField("INDEX").value].label = Pm.data.db.task[getField("INDEX").value].object.elements.label.innerHTML = oTaskData.TAS_TITLE.replace(re2, "&amp;");
     }
-    
+
     if (oTaskData.TAS_START) {
       oTaskData.TAS_START = ((oTaskData.TAS_START == "TRUE")? true : false);
       Pm.data.render.setTaskINI({task: oTaskData.TAS_UID, value: oTaskData.TAS_START});
     }
-    
+
     try {
-      new leimnud.module.app.info().make({
+      var option = {
         label: changesSavedLabel
-      });
+      }
+
+      switch (res.status) {
+          case "CRONCL":
+              option = {
+                label: changesSavedLabel + "<br /><br />" + _("APP_TITLE_CASE_LABEL_UPDATE"),
+                width: 350,
+                height: 175
+              }
+              break;
+      }
+
+      new leimnud.module.app.info().make(option);
     }
     catch (e) {
       //No show confirmation
     }
-    
+
     Pm.tmp.propertiesPanel.remove();
   }.extend(this);
-  
+
   oRPC.make();
 };
 
--- workflow/engine/methods/tasks/tasks_Ajax.php
+++ workflow/engine/methods/tasks/tasks_Ajax.php
@@ -1,87 +1,81 @@
 <?php
-/**
- * tasks_Ajax.php
- *
- * ProcessMaker Open Source Edition
- * Copyright (C) 2004 - 2008 Colosa Inc.23
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * For more information, contact Colosa Inc, 2566 Le Jeune Rd.,
- * Coral Gables, FL, 33134, USA, or email info@colosa.com.
- *
- */
 try {
-  global $RBAC;
-  switch ($RBAC->userCanAccess('PM_FACTORY')) {
-    case -2:
-	  G::SendTemporalMessage('ID_USER_HAVENT_RIGHTS_SYSTEM', 'error', 'labels');
-      G::header('location: ../login/login');
-      die;
-      break;
-    case -1:
-      G::SendTemporalMessage('ID_USER_HAVENT_RIGHTS_PAGE', 'error', 'labels');
-      G::header('location: ../login/login');
-      die;
-      break;
-  }
+    global $RBAC;
 
-  $oJSON = new Services_JSON();
-  $aData = get_object_vars($oJSON->decode($_POST['oData']));
-  if(isset($_POST['function']))
-    $sAction = $_POST['function'];
-  else
-    $sAction = $_POST['functions'];
+    switch ($RBAC->userCanAccess('PM_FACTORY')) {
+        case -2:
+            G::SendTemporalMessage('ID_USER_HAVENT_RIGHTS_SYSTEM', 'error', 'labels');
+            G::header('location: ../login/login');
+            die;
+            break;
+        case -1:
+            G::SendTemporalMessage('ID_USER_HAVENT_RIGHTS_PAGE', 'error', 'labels');
+            G::header('location: ../login/login');
+            die;
+            break;
+    }
 
-  switch ($sAction) {
-    case 'saveTaskData':
-      require_once 'classes/model/Task.php';
-      $oTask = new Task();
-      
-      /**
-       * routine to replace @amp@ by &
-       * that why the char "&" can't be passed by XmlHttpRequest directly
-       * @autor erik <erik@colosa.com>
-       */
-      foreach($aData as $k=>$v) {
-        $aData[$k] = str_replace('@amp@', '&', $v);
-      }
-        
-      if (isset($aData['SEND_EMAIL'])) {
-        $aData['TAS_SEND_LAST_EMAIL'] = $aData['SEND_EMAIL'] == 'TRUE' ? 'TRUE' : 'FALSE';
-      } else {
-        $aData['TAS_SEND_LAST_EMAIL'] = 'FALSE';
-      }
-      
-      // Additional configuration
-      if (isset($aData['TAS_DEF_MESSAGE_TYPE']) && isset($aData['TAS_DEF_MESSAGE_TEMPLATE'])) {
-      	G::loadClass('configuration');
-		$oConf = new Configurations;
-        $oConf->aConfig = Array(
-          'TAS_DEF_MESSAGE_TYPE' => $aData['TAS_DEF_MESSAGE_TYPE'],
-          'TAS_DEF_MESSAGE_TEMPLATE'=> $aData['TAS_DEF_MESSAGE_TEMPLATE']
-        );
-        $oConf->saveConfig('TAS_EXTRA_PROPERTIES', $aData['TAS_UID'], '', '');
-        unset($aData['TAS_DEF_MESSAGE_TYPE']);
-        unset($aData['TAS_DEF_MESSAGE_TEMPLATE']);
-      }
+    $oJSON = new Services_JSON();
+    $aData = get_object_vars($oJSON->decode($_POST['oData']));
 
-      $oTask->update($aData);
-      break;
-  }
+    if (isset($_POST['function'])) {
+        $sAction = $_POST['function'];
+    } else {
+        $sAction = $_POST['functions'];
+    }
+
+    switch ($sAction) {
+        case "saveTaskData":
+            require_once ("classes/model/Task.php");
+
+            $response = array();
+
+            $oTask = new Task();
+
+            /**
+             * routine to replace @amp@ by &
+             * that why the char "&" can't be passed by XmlHttpRequest directly
+             * @autor erik <erik@colosa.com>
+             */
+
+            foreach ($aData as $k => $v) {
+                $aData[$k] = str_replace('@amp@', '&', $v);
+            }
+
+            if (isset($aData['SEND_EMAIL'])) {
+                $aData['TAS_SEND_LAST_EMAIL'] = $aData['SEND_EMAIL'] == 'TRUE' ? 'TRUE' : 'FALSE';
+            } else {
+                $aData['TAS_SEND_LAST_EMAIL'] = 'FALSE';
+            }
+
+            //Additional configuration
+            if (isset($aData['TAS_DEF_MESSAGE_TYPE']) && isset($aData['TAS_DEF_MESSAGE_TEMPLATE'])) {
+                G::LoadClass('configuration');
+
+                $oConf = new Configurations();
+                $oConf->aConfig = array(
+                    'TAS_DEF_MESSAGE_TYPE'     => $aData['TAS_DEF_MESSAGE_TYPE'],
+                    'TAS_DEF_MESSAGE_TEMPLATE' => $aData['TAS_DEF_MESSAGE_TEMPLATE']
+                );
+
+                $oConf->saveConfig('TAS_EXTRA_PROPERTIES', $aData['TAS_UID'], '', '');
+
+                unset($aData['TAS_DEF_MESSAGE_TYPE']);
+                unset($aData['TAS_DEF_MESSAGE_TEMPLATE']);
+            }
+
+            $result = $oTask->update($aData);
+
+            $response["status"]  = "OK";
+
+            if ($result == 3) {
+                $response["status"]  = "CRONCL";
+            }
+
+            echo G::json_encode($response);
+            break;
+    }
+} catch (Exception $oException) {
+    die($oException->getMessage());
 }
-catch (Exception $oException) {
-  die($oException->getMessage());
-}
-?>
+
