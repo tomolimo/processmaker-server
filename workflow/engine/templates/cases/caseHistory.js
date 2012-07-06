5df21ae1e552380fb2a6d23132b958c6211df84d
Julio Cesar Laura <juliocesar@colosa.com>
2012-07-06 16:33:07 -0400

    BUG 0000 Add validation in case history with value null
    - The function "startDateRender" don't validate null values

--- workflow/engine/templates/cases/caseHistory.js
+++ workflow/engine/templates/cases/caseHistory.js
@@ -1,35 +1,35 @@
 /*
  * @author: Douglas Medrano
- * May 03, 2011 
+ * May 03, 2011
  */
 
-    function onResizeIframe(idIframe){  
-      window.parent.tabIframeWidthFix2(idIframe);  
-    }  
-    
+    function onResizeIframe(idIframe){
+      window.parent.tabIframeWidthFix2(idIframe);
+    }
+
     function ajaxPostRequest(url, callback_function, id){
       var d = new Date();
       var time = d.getTime();
       url= url + '&nocachetime='+time;
-      var return_xml=false;    
+      var return_xml=false;
       var http_request = false;
-      
+
         if (window.XMLHttpRequest){ // Mozilla, Safari,...
           http_request = new XMLHttpRequest();
             if (http_request.overrideMimeType){
-              http_request.overrideMimeType('text/xml'); 
+              http_request.overrideMimeType('text/xml');
             }
         }
         else if (window.ActiveXObject){// IE
           try{
             http_request = new ActiveXObject("Msxml2.XMLHTTP");
-          } 
+          }
           catch (e){
               try{
                 http_request = new ActiveXObject("Microsoft.XMLHTTP");
               }
               catch (e){
-               
+
               }
           }
         }
@@ -37,17 +37,17 @@
           alert('This browser is not supported.');
           return false;
         }
-        
+
         http_request.onreadystatechange = function(){
             if (http_request.readyState == 4){
                 if (http_request.status == 200){
                     if (return_xml){
                       eval(callback_function + '(http_request.responseXML)');
                     }
-                    else{		               	
-                      eval(callback_function + '(http_request.responseText, \''+id+'\')');			
+                    else{
+                      eval(callback_function + '(http_request.responseText, \''+id+'\')');
                     }
-                } 
+                }
                 else{
                   alert('Error found on request:(Code: ' + http_request.status + ')');
                 }
@@ -55,8 +55,8 @@
         }
       http_request.open('GET', url, true);
       http_request.send(null);
-    }   
- 
+    }
+
   var processesGrid;
   var store;
 
@@ -68,7 +68,7 @@
           table.style.display = '';
         }
     }
-    
+
     new Ext.KeyMap(
       document,
       {
@@ -91,31 +91,31 @@
     Ext.onReady(function(){
       Ext.state.Manager.setProvider(new Ext.state.CookieProvider());
       Ext.QuickTips.init();
-      
+
       historyGridList();
-    
-    });  
-  
-    //!historyGridList|  
+
+    });
+
+    //!historyGridList|
     //!!historyGridList|changeLog
     function historyGridListChangeLogPanelBody_RSP(resp,id){
-      var historyGridListChangeLogPanel_id_ = Ext.getCmp('historyGridListChangeLogPanel_id');    
-      historyGridListChangeLogPanel_id_.show();      
+      var historyGridListChangeLogPanel_id_ = Ext.getCmp('historyGridListChangeLogPanel_id');
+      historyGridListChangeLogPanel_id_.show();
       var historyGridListChangeLogPanelBody_= document.getElementById('historyGridListChangeLogPanelBody_JXP');
-      historyGridListChangeLogPanelBody_.innerHTML= resp;  
+      historyGridListChangeLogPanelBody_.innerHTML= resp;
     }
-  
+
     function historyGridListChangeLogPanelBody_JXP(){
       //!historyGridListChangeLogGlobal
       historyGridListChangeLogGlobal.idHistory = historyGridListChangeLogGlobal.idHistory;
-      
+
       //dataSystem
       var idHistory = historyGridListChangeLogGlobal.idHistory;
-      
+
       var url = "caseHistory_Ajax.php?actionAjax=historyGridListChangeLogPanelBody_JXP&idHistory="+idHistory;
       ajaxPostRequest(url,'historyGridListChangeLogPanelBody_RSP');
     }
-  
+
     function historyGridListChangeLogPanel(){
         var w = new Ext.Window({
           //draggable: Ext.util.Draggable,
@@ -136,23 +136,23 @@
             },
             {
               name:'ajaxAction',
-              xtype:'hidden',              
+              xtype:'hidden',
               value:'uploadFileNewProcess'
             }
           ]
-        });  
-      historyGridListChangeLogPanelBody_JXP();    
-    }  
-  
+        });
+      historyGridListChangeLogPanelBody_JXP();
+    }
+
   var historyGridListChangeLogGlobal = {};
   historyGridListChangeLogGlobal.idHistory ='';
-  
+
     function historyGridListChangeLog(){
-      
-      historyGridListChangeLogGlobal.idHistory = historyGridListChangeLogGlobal.idHistory;             
+
+      historyGridListChangeLogGlobal.idHistory = historyGridListChangeLogGlobal.idHistory;
       var rowSelected = processesGrid.getSelectionModel().getSelected();
-        if( rowSelected ){        
-          var idHistory = rowSelected.data.ID_HISTORY;          
+        if( rowSelected ){
+          var idHistory = rowSelected.data.ID_HISTORY;
           historyGridListChangeLogGlobal.idHistory = idHistory;
           historyGridListChangeLogPanel();
         }
@@ -167,10 +167,10 @@
               buttons: Ext.MessageBox.OK
             });
         }
-      
+
     }
   //!!historyGridList|changeLog
-  
+
   function historyGridList(){
       store = new Ext.data.GroupingStore({
         proxy : new Ext.data.HttpProxy
@@ -178,8 +178,8 @@
           {
             url: 'caseHistory_Ajax.php?actionAjax=historyGridList_JXP'
           }
-        ),       
-    
+        ),
+
         reader : new Ext.data.JsonReader
         (
           {
@@ -203,24 +203,24 @@
               {name : 'APP_DISABLE_ACTION_DATE', type:'float'}
             ]
           }
-        ) 
+        )
       });
-    
+
       var expander = new Ext.ux.grid.RowExpander({
         tpl : new Ext.Template(
           '<p><b>'+TRANSLATIONS.ID_PRO_DESCRIPTION+':</b> {PRO_DESCRIPTION}</p><br>'
         )
-      });   
-  
-  
+      });
+
+
       startDateRender = function(v){
         var dateString = "-";
-          if(v!="-"){
-            dateString = _DF(v,"m/d/Y H:i:s");      
+          if(v != "-" && v != null){
+            dateString = _DF(v,"m/d/Y H:i:s");
           }
-        return dateString;    
+        return dateString;
       }
-    
+
       actionRenderingTranslation = function(v){
         var actionTranslate = "";
         if(v=="PAUSE"){
@@ -237,11 +237,11 @@
         }
         else if(v==""||v==null){
           actionTranslate = _("ID_DERIVATED");
-        }  
+        }
         return actionTranslate;
       };
-    
-    
+
+
       processesGrid = new Ext.grid.GridPanel({
         region: 'center',
         layout: 'fit',
@@ -257,15 +257,15 @@
         frame:false,
         //plugins: expander,
         cls : 'grid_with_checkbox',
-        columnLines: true,    
+        columnLines: true,
         viewConfig: {
           forceFit:true
-        },    
+        },
         cm: new Ext.grid.ColumnModel({
           defaults: {
               width: 200,
               sortable: true
-          },    
+          },
           columns: [
             //expander,
             {id:'ID_HISTORY', dataIndex: 'ID_HISTORY', hidden:true, hideable:false},
@@ -277,7 +277,7 @@
               return String.format("<font color='{0}'>{1}</font>", color, v);
             }},*/
             {header: _("ID_TASK_TRANSFER"), dataIndex: 'DEL_DELEGATE_DATE', width: 60, renderer:startDateRender},
-            {header: _("ID_START_DATE"), dataIndex: 'DEL_INIT_DATE', width: 60, renderer: startDateRender}, 
+            {header: _("ID_START_DATE"), dataIndex: 'DEL_INIT_DATE', width: 60, renderer: startDateRender},
             {header: _("ID_END_DATE"), dataIndex: 'DEL_FINISH_DATE', width: 60, renderer:startDateRender},
             {header: _("ID_ACTION"), dataIndex: 'APP_TYPE', width: 50, renderer: actionRenderingTranslation},
             {header: _("ID_ENABLE_ACTION"), dataIndex: 'APP_ENABLE_ACTION_DATE', width: 70, renderer:startDateRender},
@@ -295,11 +295,11 @@
             icon: '/images/ext/gray/shapes/hourglass.png',
             handler: function(){
               var rowSelected = processesGrid.getSelectionModel().getSelected();
-              
-              if( rowSelected ){                
+
+              if( rowSelected ){
                 window.parent.historyGridListChangeLogGlobal.idHistory = rowSelected.data.ID_HISTORY;
                 window.parent.historyGridListChangeLogGlobal.tasTitle = rowSelected.data.TAS_TITLE;
-                
+
                 var idHistory = window.parent.historyGridListChangeLogGlobal.idHistory;
                 window.parent.Actions.tabFrame('changeLogTab'+idHistory);
               }
@@ -312,10 +312,10 @@
                   animEl: 'elId',
                   icon: Ext.MessageBox.INFO,
                   buttons: Ext.MessageBox.OK
-                });                
+                });
               }
-                
-            
+
+
           },
             disabled:false
           },
@@ -335,16 +335,16 @@
           rowdblclick: emptyReturn,
           render: function(){
             this.loadMask = new Ext.LoadMask(this.body, {msg:'Loading...'});
-            processesGrid.getSelectionModel().on('rowselect', function(){        
+            processesGrid.getSelectionModel().on('rowselect', function(){
               var rowSelected = processesGrid.getSelectionModel().getSelected();
-              
+
             });
           }
         }
       });
-  
+
     processesGrid.store.load({params: {"function":"languagesList"}});
-    
+
       processesGrid.store.on(
         'load',
         function()
@@ -356,16 +356,16 @@
           single: true
         }
       );
-      
+
     processesGrid.addListener('rowcontextmenu', emptyReturn,this);
     processesGrid.on('rowcontextmenu', function (grid, rowIndex, evt) {
       var sm = grid.getSelectionModel();
       sm.selectRow(rowIndex, sm.isSelected(rowIndex));
-      
+
       var rowSelected = Ext.getCmp('processesGrid').getSelectionModel().getSelected();
       var activator = Ext.getCmp('activator2');
       var debug = Ext.getCmp('debug');
-      
+
       if( rowSelected.data.PRO_STATUS == 'ACTIVE' ){
         activator.setIconClass('icon-deactivate');
         activator.setText(TRANSLATIONS.ID_DEACTIVATE);
@@ -373,7 +373,7 @@
         activator.setIconClass('icon-activate');
         activator.setText(TRANSLATIONS.ID_ACTIVATE);
       }
-  
+
       if( rowSelected.data.PRO_DEBUG == 1){
         debug.setIconClass('icon-debug-disabled');
         debug.setText(_('ID_DISABLE_DEBUG'));
@@ -382,22 +382,22 @@
         debug.setText(_('ID_ENABLE_DEBUG'));
       }
     }, this);
-    
+
     processesGrid.on('contextmenu', function (evt) {
       evt.preventDefault();
     }, this);
-    
-    
-  
-  
-    
+
+
+
+
+
     function emptyReturn(){
-      return; 
+      return;
     }
-    
-        
-  
-  
+
+
+
+
     var viewport = new Ext.Viewport({
       layout: 'border',
       autoScroll: true,
@@ -407,6 +407,6 @@
     });
   }
   //!historyGridList|
-  
-  
+
+
 
