51b40de6a5777c87b379f006d8dd62194a4db90d
Marco Antonio Nina <marcoantonionina@colosa.com>
2012-07-11 18:22:10 -0400

    BUG 0000 Short tags
    - Modify short tags.

--- workflow/engine/templates/cases/showDebugFrameBreaker.php
+++ workflow/engine/templates/cases/showDebugFrameBreaker.php
@@ -1,4 +1,9 @@
-<?php if(isset($_POST['NextStep'])){?>
-    <div class="ui-widget-header ui-corner-all" width="100%" align="center">Processmaker - Debugger (Break Point)&nbsp;&nbsp;&nbsp;&nbsp; <input type="button" value="Continue" class="module_app_button___gray" onclick="javascript:location.href='<?=$_POST['NextStep']?>'">
+<?php if (isset($_POST['NextStep'])) {?>
+    <div class="ui-widget-header ui-corner-all" width="100%" align="center">
+    Processmaker - Debugger (Break Point)&nbsp;&nbsp;&nbsp;&nbsp;
+    <input type="button" value="Continue" class="module_app_button___gray"
+    onclick="javascript:location.href='<?php echo $_POST['NextStep']; ?>'">
     </div>
-<?php }?>
+    <?php
+      }
+
