7a23d26106f27e94ff573b3d743e986950a26c41
jennylee <jennylee@colosa.com>
2012-09-28 14:35:39 -0400

    BUG 7306 Case Tracker download documents doesnt work, shows 404 Not Found page.
    The file workflow/engine/methods/tracker/tracker_ShowOutputDocument.php was incomplete, there were not some validations like versioning, and there were not taking into account the extension of the file to download. I add those validations, taking like example the file /workflow/engine/methods/cases/cases_ShowOutputDocument.php

--- workflow/engine/methods/tracker/tracker_ShowOutputDocument.php
+++ workflow/engine/methods/tracker/tracker_ShowOutputDocument.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * cases_ShowOutputDocument.php
- *  
+ *
  * ProcessMaker Open Source Edition
  * Copyright (C) 2004 - 2008 Colosa Inc.23
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
@@ -14,44 +14,76 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * For more information, contact Colosa Inc, 2566 Le Jeune Rd., 
+ *
+ * For more information, contact Colosa Inc, 2566 Le Jeune Rd.,
  * Coral Gables, FL, 33134, USA, or email info@colosa.com.
- * 
+ *
  */
 /*
  * Created on 13-02-2008
  *
  * @author David Callizaya <davidsantos@colosa.com>
  */
-if (!isset($_SESSION['PROCESS']))
-  {
-	  G::header('location: login');
-  }
-  
-  require_once ( "classes/model/AppDocumentPeer.php" );
+require_once ( "classes/model/AppDocumentPeer.php" );
 
-  $oAppDocument = new AppDocument();
-  $oAppDocument->Fields = $oAppDocument->load($_GET['a']);
+$oAppDocument = new AppDocument();
+$oAppDocument->Fields = $oAppDocument->load($_GET['a'],(isset($_GET['v']) )? $_GET['v'] : NULL );
 
-  $sAppDocUid = $oAppDocument->getAppDocUid();
-  $info = pathinfo( $oAppDocument->getAppDocFilename() );
-  if (!isset($_GET['ext'])) {
+$sAppDocUid = $oAppDocument->getAppDocUid();
+$info = pathinfo( $oAppDocument->getAppDocFilename() );
+if (!isset($_GET['ext'])) {
     $ext = $info['extension'];
-  }
-  else {
-  	if ($_GET['ext'] != '') {
-  		$ext = $_GET['ext'];
-  	}
-  	else {
-  		$ext = $info['extension'];
-  	}
-  }
+}
+else {
+    if ($_GET['ext'] != '') {
+        $ext = $_GET['ext'];
+    }
+    else {
+        $ext = $info['extension'];
+    }
+}
+$ver= (isset($_GET['v']) && $_GET['v']!='') ? '_'.$_GET['v'] : '';
 
-  $realPath = PATH_DOCUMENT . $_SESSION['APPLICATION'] . '/outdocs/' . $info['basename'] . '.' . $ext ;
-  G::streamFile ( $realPath, true );
+if(!$ver)   //This code is in the case the outputdocument won't be versioned
+  $ver='_1';
 
+$realPath = PATH_DOCUMENT . $oAppDocument->Fields['APP_UID'] . '/outdocs/' . $sAppDocUid .$ver. '.' . $ext ;
+$realPath1 = PATH_DOCUMENT . $oAppDocument->Fields['APP_UID'] . '/outdocs/' . $info['basename'] .$ver. '.' . $ext ;
+$realPath2 = PATH_DOCUMENT . $oAppDocument->Fields['APP_UID'] . '/outdocs/' . $info['basename']. '.' . $ext ;
+$sw_file_exists=false;
+if(file_exists($realPath)){
+    $sw_file_exists=true;
+}elseif(file_exists($realPath1)){
+    $sw_file_exists=true;
+    $realPath=$realPath1;
+}elseif(file_exists($realPath2)){
+    $sw_file_exists=true;
+    $realPath=$realPath2;
+}
+if(!$sw_file_exists){
+    $error_message="'".$info['basename'] .$ver. '.' . $ext."' ".G::LoadTranslation('ID_ERROR_STREAMING_FILE');
+    if((isset($_POST['request']))&&($_POST['request']==true)){
+        $res ['success'] = 'failure';
+        $res ['message'] = $error_message;
+        print G::json_encode ( $res );
+    }else{
+        G::SendMessageText($error_message, "ERROR");
+        $backUrlObj=explode("sys".SYS_SYS,$_SERVER['HTTP_REFERER']);
+        G::header("location: "."/sys".SYS_SYS.$backUrlObj[1]);
+        die;
+    }
+
+}else{
+    if((isset($_POST['request']))&&($_POST['request']==true)){
+        $res ['success'] = 'success';
+        $res ['message'] = $info['basename'] .$ver. '.' . $ext;
+        print G::json_encode ( $res );
+    }else{
+        G::streamFile ( $realPath, true ,$info['basename'] .$ver. '.' . $ext );
+    }
+}
+//G::streamFile ( $realPath, true);
 ?>
