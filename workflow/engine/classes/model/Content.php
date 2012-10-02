e9f1a08dee04849bd405fa265bfaaee9fcc26071
Julio Cesar Laura <juliocesar@colosa.com>
2012-10-02 10:52:02 -0400

    BUG 9720 Proceso de actualizacion en ProcessMaker es lento IMPROVEMENT

--- workflow/engine/classes/model/Content.php
+++ workflow/engine/classes/model/Content.php
@@ -2,10 +2,10 @@
 /**
  * Content.php
  * @package    workflow.engine.classes.model
- *  
+ *
  * ProcessMaker Open Source Edition
  * Copyright (C) 2004 - 2011 Colosa Inc.
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
@@ -15,13 +15,13 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * For more information, contact Colosa Inc, 2566 Le Jeune Rd., 
+ *
+ * For more information, contact Colosa Inc, 2566 Le Jeune Rd.,
  * Coral Gables, FL, 33134, USA, or email info@colosa.com.
- * 
+ *
  */
 
 require_once 'classes/model/om/BaseContent.php';
@@ -29,7 +29,7 @@ require_once 'classes/model/om/BaseContent.php';
 /**
  * Skeleton subclass for representing a row from the 'CONTENT' table.
  *
- * 
+ *
  *
  * You should add additional methods to this class to meet the
  * application requirements.  This class will only be generated as
@@ -38,7 +38,7 @@ require_once 'classes/model/om/BaseContent.php';
  * @package    workflow.engine.classes.model
  */
 class Content extends BaseContent {
-  
+
         public $langs;
         public $rowsProcessed;
         public $rowsInserted;
@@ -46,7 +46,7 @@ class Content extends BaseContent {
         public $rowsClustered;
         public $langsAsoc;
   /*
-  * Load the content row specified by the parameters: 
+  * Load the content row specified by the parameters:
   * @param string $sUID
   * @return variant
   */
@@ -65,46 +65,46 @@ class Content extends BaseContent {
     return $ConValue;
   }
   /*
-  * Find a valid Lang for current Content. The most recent 
+  * Find a valid Lang for current Content. The most recent
   * @param string $ConCategory
   * @param string  $ConParent
-  * @param string $ConId  
+  * @param string $ConId
   * @return string
-  * 
+  *
   */
   function getDefaultContentLang($ConCategory, $ConParent, $ConId, $destConLang) {
     $Criteria = new Criteria ( 'workflow' );
     $Criteria->clearSelectColumns ()->clearOrderByColumns ();
-    
+
     $Criteria->addSelectColumn ( ContentPeer::CON_CATEGORY );
     $Criteria->addSelectColumn ( ContentPeer::CON_PARENT );
     $Criteria->addSelectColumn ( ContentPeer::CON_ID );
     $Criteria->addSelectColumn ( ContentPeer::CON_LANG );
     $Criteria->addSelectColumn ( ContentPeer::CON_VALUE );
-    
+
     $Criteria->add ( ContentPeer::CON_CATEGORY, $ConCategory, CRITERIA::EQUAL );
     $Criteria->add ( ContentPeer::CON_PARENT, $ConParent, CRITERIA::EQUAL );
     $Criteria->add ( ContentPeer::CON_ID, $ConId, CRITERIA::EQUAL );
-    
+
     $Criteria->add ( ContentPeer::CON_LANG, $destConLang, CRITERIA::NOT_EQUAL );
-    
+
     $rs = ContentPeer::doSelectRS ( $Criteria );
     $rs->setFetchmode ( ResultSet::FETCHMODE_ASSOC );
     $rs->next ();
-    
+
     if (is_array ( $row = $rs->getRow () )) {
       $defaultLang = $row ['CON_LANG'];
-    
+
     } else {
       $defaultLang = "";
     }
     return ($defaultLang);
   }
   /*
-  * Load the content row and the Save automatically the row for the destination language 
+  * Load the content row and the Save automatically the row for the destination language
   * @param string $ConCategory
   * @param string  $ConParent
-  * @param string $ConId 
+  * @param string $ConId
   * @param string $destConLang
   * @return string
   * if the row doesn't exist, it will be created automatically, even the default 'en' language
@@ -112,18 +112,18 @@ class Content extends BaseContent {
   function autoLoadSave($ConCategory, $ConParent, $ConId, $destConLang) {
     //search in 'en' language, the default language
     $content = ContentPeer::retrieveByPK ( $ConCategory, $ConParent, $ConId, 'en' );
-    
+
     if ((is_null ( $content )) || ($content->getConValue () == "")) {
       $differentLang = Content::getDefaultContentLang ( $ConCategory, $ConParent, $ConId, $destConLang );
       $content = ContentPeer::retrieveByPK ( $ConCategory, $ConParent, $ConId, $differentLang );
     }
-    
+
     //to do: review if the $destConLang is a valid language/
     if (is_null ( $content ))
       $ConValue = ''; //we dont find any value for this field and language in CONTENT table
     else
       $ConValue = $content->getConValue ();
-    
+
     try {
       $con = ContentPeer::retrieveByPK ( $ConCategory, $ConParent, $ConId, $destConLang );
       if (is_null ( $con )) {
@@ -140,30 +140,30 @@ class Content extends BaseContent {
     } catch ( Exception $e ) {
       throw ($e);
     }
-    
+
     return $ConValue;
   }
-  
+
   /*
-  * Insert a content row  
+  * Insert a content row
   * @param string $ConCategory
   * @param string $ConParent
   * @param string $ConId
   * @param string $ConLang
-  * @param string $ConValue 
+  * @param string $ConValue
   * @return variant
   */
   function addContent($ConCategory, $ConParent, $ConId, $ConLang, $ConValue) {
     try {
       if ($ConLang != 'en') {
         $baseLangContent = ContentPeer::retrieveByPk($ConCategory, $ConParent, $ConId, 'en');
-        if ($baseLangContent === null) {      
+        if ($baseLangContent === null) {
           Content::addContent($ConCategory, $ConParent, $ConId, 'en', $ConValue);
         }
       }
-      
+
       $con = ContentPeer::retrieveByPK ( $ConCategory, $ConParent, $ConId, $ConLang );
-      
+
       if (is_null ( $con )) {
         $con = new Content ( );
       } else {
@@ -187,14 +187,14 @@ class Content extends BaseContent {
       throw ($e);
     }
   }
-  
+
   /*
-  * Insert a content row  
+  * Insert a content row
   * @param string $ConCategory
   * @param string $ConParent
   * @param string $ConId
   * @param string $ConLang
-  * @param string $ConValue 
+  * @param string $ConValue
   * @return variant
   */
   function insertContent($ConCategory, $ConParent, $ConId, $ConLang, $ConValue) {
@@ -216,14 +216,14 @@ class Content extends BaseContent {
       throw ($e);
     }
   }
-  
+
   /*
-  * remove a content row  
+  * remove a content row
   * @param string $ConCategory
   * @param string $ConParent
   * @param string $ConId
   * @param string $ConLang
-  * @param string $ConValue 
+  * @param string $ConValue
   * @return variant
   */
   function removeContent($ConCategory, $ConParent, $ConId) {
@@ -243,7 +243,7 @@ class Content extends BaseContent {
     } catch ( Exception $e ) {
       throw ($e);
     }
-  
+
   }
 
   /*
@@ -253,10 +253,10 @@ class Content extends BaseContent {
   * @param  string  $ConParent
   * @param  string  $ConId
   * @param  string  $ConLang
-  * @param  string  $ConValue 
+  * @param  string  $ConValue
   * @return boolean true or false
   */
-  function Exists ($ConCategory, $ConParent, $ConId, $ConLang) 
+  function Exists ($ConCategory, $ConParent, $ConId, $ConLang)
   {
     try {
       $oPro = ContentPeer::retrieveByPk($ConCategory, $ConParent, $ConId, $ConLang);
@@ -276,7 +276,7 @@ class Content extends BaseContent {
     *
     * @param  array  $langs
     */
-    function regenerateContent($langId)
+    function regenerateContent($langs)
     {
         //Search the language
         $key = array_search('en',$langs);
@@ -290,13 +290,13 @@ class Content extends BaseContent {
         foreach ($langs as $key=>$value) {
             $this->langsAsoc[$value] = $value;
         }
-        
+
         $this->langs = $langs;
         $this->rowsProcessed = 0;
         $this->rowsInserted  = 0;
         $this->rowsUnchanged = 0;
         $this->rowsClustered = 0;
-        
+
         $con = Propel::getConnection('workflow');
         $sql = " SELECT DISTINCT CON_LANG
                 FROM CONTENT ";
@@ -391,7 +391,7 @@ class Content extends BaseContent {
       $c->addSelectColumn(ContentPeer::CON_LANG);
 
       $c->add ( ContentPeer::CON_LANG, $lanId );
-      
+
       $result = ContentPeer::doSelectRS ( $c );
       $result->setFetchmode(ResultSet::FETCHMODE_ASSOC);
       $result->next ();
@@ -402,7 +402,7 @@ class Content extends BaseContent {
 
         if( $content !== null )
           $content->delete();
-        
+
         $result->next ();
         $row = $result->getRow ();
       }
@@ -412,7 +412,7 @@ class Content extends BaseContent {
     }
   }
   //Added by Enrique at Feb 9th,2011
-  //Gets all Role Names by Role 
+  //Gets all Role Names by Role
   function getAllContentsByRole($sys_lang=SYS_LANG){
   	if (!isset($sys_lang)) $sys_lang = 'en';
   	$oCriteria = new Criteria('workflow');
@@ -427,7 +427,7 @@ class Content extends BaseContent {
   	$aRoles = Array();
   	while ($oDataset->next()){
   		$xRow = $oDataset->getRow();
-  		$aRoles[$xRow['CON_ID']] = $xRow['ROL_NAME']; 
+  		$aRoles[$xRow['CON_ID']] = $xRow['ROL_NAME'];
   	}
   	return $aRoles;
   }
