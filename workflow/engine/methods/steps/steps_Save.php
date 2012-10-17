35225d9e7b8639cc7712925f8c158772488ea399
jennylee <jennylee@colosa.com>
2012-10-17 17:36:05 -0400

    CODE STYLE, workflow/engine/methods/steps/
    FILES:
    conditions_Edit.php
    conditions_Save.php
    steps_Ajax.php
    steps_Delete.php
    steps_Down.php
    steps_New.php
    steps_Save.php
    steps_SupervisorAjax.php
    steps_Up.php

--- workflow/engine/methods/steps/steps_Save.php
+++ workflow/engine/methods/steps/steps_Save.php
@@ -12,43 +12,39 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  * For more information, contact Colosa Inc, 2566 Le Jeune Rd.,
  * Coral Gables, FL, 33134, USA, or email info@colosa.com.
- *
  */
 try {
-  global $RBAC;
-  switch ($RBAC->userCanAccess('PM_FACTORY')) {
-  	case -2:
-  	  G::SendTemporalMessage('ID_USER_HAVENT_RIGHTS_SYSTEM', 'error', 'labels');
-  	  G::header('location: ../login/login');
-  	  die;
-  	break;
-  	case -1:
-  	  G::SendTemporalMessage('ID_USER_HAVENT_RIGHTS_PAGE', 'error', 'labels');
-  	  G::header('location: ../login/login');
-  	  die;
-  	break;
-  }
-  require_once 'classes/model/Step.php';
-  $oStep    = new Step();
-  $sStepUID = $oStep->create(array('PRO_UID' => $_POST['sProcess'], 'TAS_UID' => $_POST['sTask']));
-  $oStep->update(array('STEP_UID'      => $sStepUID,
-                       'STEP_TYPE_OBJ' => $_POST['sType'],
-                       'STEP_UID_OBJ'  => $_POST['sUID'],
-                       'STEP_POSITION' => ($oStep->getNextPosition($_POST['sTask']) - 1),
-                       'STEP_MODE'     => (isset($_POST['sMode'])) ? $_POST['sMode'] : 'EDIT'));
-  G::LoadClass('processMap');
-  $oProcessMap = new ProcessMap();
-  $oProcessMap->getStepsCriteria($_POST['sTask']);
+    global $RBAC;
+    switch ($RBAC->userCanAccess( 'PM_FACTORY' )) {
+        case - 2:
+            G::SendTemporalMessage( 'ID_USER_HAVENT_RIGHTS_SYSTEM', 'error', 'labels' );
+            G::header( 'location: ../login/login' );
+            die();
+            break;
+        case - 1:
+            G::SendTemporalMessage( 'ID_USER_HAVENT_RIGHTS_PAGE', 'error', 'labels' );
+            G::header( 'location: ../login/login' );
+            die();
+            break;
+    }
+    require_once 'classes/model/Step.php';
+    $oStep = new Step();
+    $sStepUID = $oStep->create( array ('PRO_UID' => $_POST['sProcess'],'TAS_UID' => $_POST['sTask']
+    ) );
+    $oStep->update( array ('STEP_UID' => $sStepUID,'STEP_TYPE_OBJ' => $_POST['sType'],'STEP_UID_OBJ' => $_POST['sUID'],'STEP_POSITION' => ($oStep->getNextPosition( $_POST['sTask'] ) - 1),'STEP_MODE' => (isset( $_POST['sMode'] )) ? $_POST['sMode'] : 'EDIT'
+    ) );
+    G::LoadClass( 'processMap' );
+    $oProcessMap = new ProcessMap();
+    $oProcessMap->getStepsCriteria( $_POST['sTask'] );
+} catch (Exception $oException) {
+    die( $oException->getMessage() );
 }
-catch (Exception $oException) {
-	die($oException->getMessage());
-}
-?>
+
--- workflow/engine/methods/steps/steps_SupervisorAjax.php
+++ workflow/engine/methods/steps/steps_SupervisorAjax.php
@@ -12,59 +12,57 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  * For more information, contact Colosa Inc, 2566 Le Jeune Rd.,
  * Coral Gables, FL, 33134, USA, or email info@colosa.com.
- *
  */
 try {
-	global $RBAC;
-  switch ($RBAC->userCanAccess('PM_FACTORY'))
-  {
-  	case -2:
-  	  G::SendTemporalMessage('ID_USER_HAVENT_RIGHTS_SYSTEM', 'error', 'labels');
-  	  G::header('location: ../login/login');
-  	  die;
-  	break;
-  	case -1:
-  	  G::SendTemporalMessage('ID_USER_HAVENT_RIGHTS_PAGE', 'error', 'labels');
-  	  G::header('location: ../login/login');
-  	  die;
-  	break;
-  }
+    global $RBAC;
+    switch ($RBAC->userCanAccess( 'PM_FACTORY' )) {
+        case - 2:
+            G::SendTemporalMessage( 'ID_USER_HAVENT_RIGHTS_SYSTEM', 'error', 'labels' );
+            G::header( 'location: ../login/login' );
+            die();
+            break;
+        case - 1:
+            G::SendTemporalMessage( 'ID_USER_HAVENT_RIGHTS_PAGE', 'error', 'labels' );
+            G::header( 'location: ../login/login' );
+            die();
+            break;
+    }
 
-  //srequire_once 'classes/model/StepTrigger.php';
+    //srequire_once 'classes/model/StepTrigger.php';
 
-  G::LoadClass('processMap');
-  $oProcessMap = new ProcessMap();
 
-  switch ($_POST['action']) {
-  	case 'availableSupervisorDynaforms':
-  	  $oProcessMap->availableSupervisorDynaforms($_POST['PRO_UID']);
-    break;
-    case 'assignSupervisorDynaform':
-      $oProcessMap->assignSupervisorStep($_POST['PRO_UID'], 'DYNAFORM', $_POST['DYN_UID']);
-    break;
-    case 'removeSupervisorDynaform':
-      $oProcessMap->removeSupervisorStep($_POST['STEP_UID'], $_POST['PRO_UID'], 'DYNAFORM', $_POST['DYN_UID'], $_POST['STEP_POSITION']);
-    break;
-    case 'availableSupervisorInputs':
-  	  $oProcessMap->availableSupervisorInputs($_POST['PRO_UID']);
-    break;
-    case 'assignSupervisorInput':
-      $oProcessMap->assignSupervisorStep($_POST['PRO_UID'], 'INPUT_DOCUMENT', $_POST['INP_DOC_UID']);
-    break;
-    case 'removeSupervisorInput':
-      $oProcessMap->removeSupervisorStep($_POST['STEP_UID'], $_POST['PRO_UID'], 'INPUT_DOCUMENT', $_POST['INP_DOC_UID'], $_POST['STEP_POSITION']);
-    break;
-  }
+    G::LoadClass( 'processMap' );
+    $oProcessMap = new ProcessMap();
+
+    switch ($_POST['action']) {
+        case 'availableSupervisorDynaforms':
+            $oProcessMap->availableSupervisorDynaforms( $_POST['PRO_UID'] );
+            break;
+        case 'assignSupervisorDynaform':
+            $oProcessMap->assignSupervisorStep( $_POST['PRO_UID'], 'DYNAFORM', $_POST['DYN_UID'] );
+            break;
+        case 'removeSupervisorDynaform':
+            $oProcessMap->removeSupervisorStep( $_POST['STEP_UID'], $_POST['PRO_UID'], 'DYNAFORM', $_POST['DYN_UID'], $_POST['STEP_POSITION'] );
+            break;
+        case 'availableSupervisorInputs':
+            $oProcessMap->availableSupervisorInputs( $_POST['PRO_UID'] );
+            break;
+        case 'assignSupervisorInput':
+            $oProcessMap->assignSupervisorStep( $_POST['PRO_UID'], 'INPUT_DOCUMENT', $_POST['INP_DOC_UID'] );
+            break;
+        case 'removeSupervisorInput':
+            $oProcessMap->removeSupervisorStep( $_POST['STEP_UID'], $_POST['PRO_UID'], 'INPUT_DOCUMENT', $_POST['INP_DOC_UID'], $_POST['STEP_POSITION'] );
+            break;
+    }
+} catch (Exception $oException) {
+    die( $oException->getMessage() );
 }
-catch (Exception $oException) {
-	die($oException->getMessage());
-}
-?>
+
