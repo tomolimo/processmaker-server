f09f9d37f46fa36ee42d19b4e0c4f1ca5487db7d
norahmollo <norah@colosa.com>
2012-10-17 15:23:02 +0000

    ODE STYLE Formating workflow/engine/methods/outpudocs/
    Change format files in workflow/engine/methods/outpudocs/

--- workflow/engine/methods/outputdocs/outputdocs_Edit.php
+++ workflow/engine/methods/outputdocs/outputdocs_Edit.php
@@ -12,94 +12,92 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  * For more information, contact Colosa Inc, 2566 Le Jeune Rd.,
  * Coral Gables, FL, 33134, USA, or email info@colosa.com.
- *
  */
 try {
-	global $RBAC;
-  switch ($RBAC->userCanAccess('PM_FACTORY')) {
-  	case -2:
-  	  G::SendTemporalMessage('ID_USER_HAVENT_RIGHTS_SYSTEM', 'error', 'labels');
-  	  G::header('location: ../login/login');
-  	  die;
-  	break;
-  	case -1:
-  	  G::SendTemporalMessage('ID_USER_HAVENT_RIGHTS_PAGE', 'error', 'labels');
-  	  G::header('location: ../login/login');
-  	  die;
-  	break;
-  }
-
-  require_once 'classes/model/OutputDocument.php';
-  $ooutputDocument = new OutputDocument();
-  if (isset($_GET['OUT_DOC_UID'])) {
-    $aFields = $ooutputDocument->load($_GET['OUT_DOC_UID']);
-  }
-  else {
-  	$aFields            = array();
-  	$aFields['PRO_UID'] = $_GET['PRO_UID'];
-  }
-
-  require_once 'classes/model/OutputDocument.php';
-  $ooutputDocument = new OutputDocument();
-  if (isset($_GET['OUT_DOC_UID'])) {
-    $aFields = $ooutputDocument->load($_GET['OUT_DOC_UID']);
-  }
-  else {
-  	$aFields            = array();
-  	$aFields['PRO_UID'] = $_GET['PRO_UID'];
-  }
-
-  $type = isset ( $aFields['OUT_DOC_TYPE']) ? $aFields['OUT_DOC_TYPE'] : 'HTML';
-
-  G::LoadClass('xmlfield_InputPM');
-  $G_PUBLISH = new Publisher();
-
-  switch ( $type ) {
-  	case 'HTML' :
-      //$G_PUBLISH->AddContent('xmlform', 'xmlform', 'outputdocs/outputdocs_Edit', '', $aFields , '../outputdocs/outputdocs_Save');
-      $oHeadPublisher =& headPublisher::getSingleton(); 
-      $oHeadPublisher->assign('OUT_DOC_UID', $_GET['OUT_DOC_UID']);
-      $translations = G::getTranslations(Array(
-        'ID_FILE', 'ID_OUT_PUT_DOC_UPLOAD_TITLE', 'ID_UPLOADING_FILE', 'ID_UPLOAD', 'ID_CANCEL', 
-        'ID_SAVE', 'ID_LOAD_FROM_FILE', 'ID_SELECT_TEMPLATE_FILE', 'ID_ALERT_MESSAGE', 'ID_INVALID_FILE'
-      ));
-//      $oHeadPublisher->assign('TRANSLATIONS', $translations);
-      $oHeadPublisher->addExtJsScript('outputdocs/htmlEditor', false );    //adding a javascript file .js
-      G::RenderPage('publish', 'extJs');
-      die;
-    break;
+    global $RBAC;
+    switch ($RBAC->userCanAccess( 'PM_FACTORY' )) {
+        case - 2:
+            G::SendTemporalMessage( 'ID_USER_HAVENT_RIGHTS_SYSTEM', 'error', 'labels' );
+            G::header( 'location: ../login/login' );
+            die();
+            break;
+        case - 1:
+            G::SendTemporalMessage( 'ID_USER_HAVENT_RIGHTS_PAGE', 'error', 'labels' );
+            G::header( 'location: ../login/login' );
+            die();
+            break;
+    }
     
-    case 'JRXML' :
-    case 'ACROFORM' :
-      $type = $aFields['OUT_DOC_TYPE'];
-      if ( $type == 'JRXML') $extension = 'jrxml';
-      if ( $type == 'ACROFORM') $extension = 'pdf';
-
-      // The ereg_replace function has been DEPRECATED as of PHP 5.3.0.
-      // $downFileName = ereg_replace('[^A-Za-z0-9_]', '_', $aFields['OUT_DOC_TITLE'] ) . '.' . $extension;
-      $downFileName = preg_replace('/[^A-Za-z0-9_]/i', '_', $aFields['OUT_DOC_TITLE'] ) . '.' . $extension;
-      $filename = PATH_DYNAFORM . $aFields['PRO_UID'] . PATH_SEP . $aFields['OUT_DOC_UID'] . '.' . $extension ;
-      if ( file_exists ( $filename) )
-        $aFields['FILENAME'] = $downFileName;
-      else
-        $aFields['FILENAME'] = '';
-
-      $aFields['FILELINK'] = '../outputdocs/downloadFile?' . $aFields['OUT_DOC_UID'];
-      $G_PUBLISH->AddContent('xmlform', 'xmlform', 'outputdocs/outputdocsUploadFile', '', $aFields , '../outputdocs/uploadFile');
-      $G_PUBLISH->AddContent('view', 'outputdocs/editJrxml' );
-    break;
-  }
-  G::RenderPage('publish', 'raw');
-
+    require_once 'classes/model/OutputDocument.php';
+    $ooutputDocument = new OutputDocument();
+    if (isset( $_GET['OUT_DOC_UID'] )) {
+        $aFields = $ooutputDocument->load( $_GET['OUT_DOC_UID'] );
+    } else {
+        $aFields = array ();
+        $aFields['PRO_UID'] = $_GET['PRO_UID'];
+    }
+    
+    require_once 'classes/model/OutputDocument.php';
+    $ooutputDocument = new OutputDocument();
+    if (isset( $_GET['OUT_DOC_UID'] )) {
+        $aFields = $ooutputDocument->load( $_GET['OUT_DOC_UID'] );
+    } else {
+        $aFields = array ();
+        $aFields['PRO_UID'] = $_GET['PRO_UID'];
+    }
+    
+    $type = isset( $aFields['OUT_DOC_TYPE'] ) ? $aFields['OUT_DOC_TYPE'] : 'HTML';
+    
+    G::LoadClass( 'xmlfield_InputPM' );
+    $G_PUBLISH = new Publisher();
+    
+    switch ($type) {
+        case 'HTML':
+            //$G_PUBLISH->AddContent('xmlform', 'xmlform', 'outputdocs/outputdocs_Edit', '', $aFields , '../outputdocs/outputdocs_Save');
+            $oHeadPublisher = & headPublisher::getSingleton();
+            $oHeadPublisher->assign( 'OUT_DOC_UID', $_GET['OUT_DOC_UID'] );
+            $translations = G::getTranslations( Array ('ID_FILE','ID_OUT_PUT_DOC_UPLOAD_TITLE','ID_UPLOADING_FILE','ID_UPLOAD','ID_CANCEL','ID_SAVE','ID_LOAD_FROM_FILE','ID_SELECT_TEMPLATE_FILE','ID_ALERT_MESSAGE','ID_INVALID_FILE') );
+            //      $oHeadPublisher->assign('TRANSLATIONS', $translations);
+            $oHeadPublisher->addExtJsScript( 'outputdocs/htmlEditor', false ); //adding a javascript file .js
+            G::RenderPage( 'publish', 'extJs' );
+            die();
+            break;
+        case 'JRXML':
+            break;
+        case 'ACROFORM':
+            $type = $aFields['OUT_DOC_TYPE'];
+            if ($type == 'JRXML') {
+                $extension = 'jrxml';
+            }
+            if ($type == 'ACROFORM') {
+                $extension = 'pdf';
+            }
+                
+            // The ereg_replace function has been DEPRECATED as of PHP 5.3.0.
+            // $downFileName = ereg_replace('[^A-Za-z0-9_]', '_', $aFields['OUT_DOC_TITLE'] ) . '.' . $extension;
+            $downFileName = preg_replace( '/[^A-Za-z0-9_]/i', '_', $aFields['OUT_DOC_TITLE'] ) . '.' . $extension;
+            $filename = PATH_DYNAFORM . $aFields['PRO_UID'] . PATH_SEP . $aFields['OUT_DOC_UID'] . '.' . $extension;
+            if (file_exists( $filename )) {
+                $aFields['FILENAME'] = $downFileName;
+            } else {
+                $aFields['FILENAME'] = '';
+            }
+            
+            $aFields['FILELINK'] = '../outputdocs/downloadFile?' . $aFields['OUT_DOC_UID'];
+            $G_PUBLISH->AddContent( 'xmlform', 'xmlform', 'outputdocs/outputdocsUploadFile', '', $aFields, '../outputdocs/uploadFile' );
+            $G_PUBLISH->AddContent( 'view', 'outputdocs/editJrxml' );
+            break;
+    }
+    G::RenderPage( 'publish', 'raw' );
+} catch (Exception $oException) {
+    die( $oException->getMessage() );
 }
-catch (Exception $oException) {
-	die($oException->getMessage());
-}
+
--- workflow/engine/methods/outputdocs/outputdocs_Properties.php
+++ workflow/engine/methods/outputdocs/outputdocs_Properties.php
@@ -13,79 +13,73 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  * For more information, contact Colosa Inc, 2566 Le Jeune Rd.,
  * Coral Gables, FL, 33134, USA, or email info@colosa.com.
- *
  */
 try {
-  global $RBAC;
-  switch ($RBAC->userCanAccess('PM_FACTORY')) {
-    case -2:
-      G::SendTemporalMessage('ID_USER_HAVENT_RIGHTS_SYSTEM', 'error', 'labels');
-      G::header('location: ../login/login');
-      die;
-      break;
-    case -1:
-      G::SendTemporalMessage('ID_USER_HAVENT_RIGHTS_PAGE', 'error', 'labels');
-      G::header('location: ../login/login');
-      die;
-      break;
-  }
-
-  require_once 'classes/model/OutputDocument.php';
-  $ooutputDocument = new OutputDocument();
-  if (isset($_GET['OUT_DOC_UID'])) {
-    $aFields = $ooutputDocument->load($_GET['OUT_DOC_UID']);
-  } else {
-    $aFields = array();
-    $aFields['PRO_UID'] = $_GET['PRO_UID'];
-  }
-
-  $type = isset($aFields['OUT_DOC_TYPE']) ? $aFields['OUT_DOC_TYPE'] : 'HTML';
-
-  $aFields['OUT_DOC_TAGS'] = isset($aFields['OUT_DOC_TAGS']) ? $aFields['OUT_DOC_TAGS'] : 'OUTPUT';
-  $aFields['OUT_DOC_VERSIONING'] = strval($aFields['OUT_DOC_VERSIONING']);
-  $aFields['OUT_DOC_LANDSCAPE'] = strval($aFields['OUT_DOC_LANDSCAPE']);
-  if(isset($aFields['OUT_DOC_PDF_SECURITY_ENABLED'])){
-    $aFields['OUT_DOC_PDF_SECURITY_ENABLED'] = strval($aFields['OUT_DOC_PDF_SECURITY_ENABLED']);
-  }
-
-
-  if (isset($aFields['OUT_DOC_PDF_SECURITY_OPEN_PASSWORD']) && $aFields['OUT_DOC_PDF_SECURITY_OPEN_PASSWORD'] != "") {
-    $aFields['OUT_DOC_PDF_SECURITY_OPEN_PASSWORD'] = G::decrypt($aFields['OUT_DOC_PDF_SECURITY_OPEN_PASSWORD'], $_GET['OUT_DOC_UID']);
-    $aFields['OUT_DOC_PDF_SECURITY_OWNER_PASSWORD'] = G::decrypt($aFields['OUT_DOC_PDF_SECURITY_OWNER_PASSWORD'], $_GET['OUT_DOC_UID']);
-  }
-
-  G::LoadClass('xmlfield_InputPM');
-  $G_PUBLISH = new Publisher();
-  switch ($type) {
-    case 'HTML' :
-      $G_PUBLISH->AddContent('xmlform', 'xmlform', 'outputdocs/outputdocs_Properties', '', $aFields, '../outputdocs/outputdocs_Save');
-      break;
-    case 'JRXML' :
-      $G_PUBLISH->AddContent('xmlform', 'xmlform', 'outputdocs/outputdocs_Properties', '', $aFields, '../outputdocs/outputdocs_Save');
-      /*
-        //  	     $G_PUBLISH->AddContent('xmlform', 'xmlform', 'outputdocs/outputdocsDynaformList', '', $aFields , '../outputdocs/outputdocs_Save');
-        require_once 'classes/model/Process.php';
-        G::LoadClass( 'processMap');
-        $sProcessUID = $aFields['PRO_UID'];
-        $oProcess = new Process();
-        $oProcessMap = new ProcessMap();
-        $aFields  = $oProcess->load($sProcessUID);
-        $G_PUBLISH->AddContent('propeltable', 'paged-table', 'dynaforms/dynaforms_ShortList', $oProcessMap->getDynaformsCriteria($sProcessUID), $aFields);
-       */
-      break;
-    case 'ACROFORM' :
-      $G_PUBLISH->AddContent('xmlform', 'xmlform', 'outputdocs/outputdocs_Properties', '', $aFields, '../outputdocs/outputdocs_Save');
-      break;
-  }
-  G::RenderPage('publish', 'raw');
+    global $RBAC;
+    switch ($RBAC->userCanAccess( 'PM_FACTORY' )) {
+        case - 2:
+            G::SendTemporalMessage( 'ID_USER_HAVENT_RIGHTS_SYSTEM', 'error', 'labels' );
+            G::header( 'location: ../login/login' );
+            die();
+            break;
+        case - 1:
+            G::SendTemporalMessage( 'ID_USER_HAVENT_RIGHTS_PAGE', 'error', 'labels' );
+            G::header( 'location: ../login/login' );
+            die();
+            break;
+    }
+    require_once 'classes/model/OutputDocument.php';
+    $ooutputDocument = new OutputDocument();
+    if (isset( $_GET['OUT_DOC_UID'] )) {
+        $aFields = $ooutputDocument->load( $_GET['OUT_DOC_UID'] );
+    } else {
+        $aFields = array ();
+        $aFields['PRO_UID'] = $_GET['PRO_UID'];
+    }
+    $type = isset( $aFields['OUT_DOC_TYPE'] ) ? $aFields['OUT_DOC_TYPE'] : 'HTML';
+    $aFields['OUT_DOC_TAGS'] = isset( $aFields['OUT_DOC_TAGS'] ) ? $aFields['OUT_DOC_TAGS'] : 'OUTPUT';
+    $aFields['OUT_DOC_VERSIONING'] = strval( $aFields['OUT_DOC_VERSIONING'] );
+    $aFields['OUT_DOC_LANDSCAPE'] = strval( $aFields['OUT_DOC_LANDSCAPE'] );
+    if (isset( $aFields['OUT_DOC_PDF_SECURITY_ENABLED'] )) {
+        $aFields['OUT_DOC_PDF_SECURITY_ENABLED'] = strval( $aFields['OUT_DOC_PDF_SECURITY_ENABLED'] );
+    }
+    if (isset( $aFields['OUT_DOC_PDF_SECURITY_OPEN_PASSWORD'] ) && $aFields['OUT_DOC_PDF_SECURITY_OPEN_PASSWORD'] != "") {
+        $aFields['OUT_DOC_PDF_SECURITY_OPEN_PASSWORD'] = G::decrypt( $aFields['OUT_DOC_PDF_SECURITY_OPEN_PASSWORD'], $_GET['OUT_DOC_UID'] );
+        $aFields['OUT_DOC_PDF_SECURITY_OWNER_PASSWORD'] = G::decrypt( $aFields['OUT_DOC_PDF_SECURITY_OWNER_PASSWORD'], $_GET['OUT_DOC_UID'] );
+    }
+    G::LoadClass( 'xmlfield_InputPM' );
+    $G_PUBLISH = new Publisher();
+    switch ($type) {
+        case 'HTML':
+            $G_PUBLISH->AddContent( 'xmlform', 'xmlform', 'outputdocs/outputdocs_Properties', '', $aFields, '../outputdocs/outputdocs_Save' );
+            break;
+        case 'JRXML':
+            $G_PUBLISH->AddContent( 'xmlform', 'xmlform', 'outputdocs/outputdocs_Properties', '', $aFields, '../outputdocs/outputdocs_Save' );
+            /*
+            //  	     $G_PUBLISH->AddContent('xmlform', 'xmlform', 'outputdocs/outputdocsDynaformList', '', $aFields , '../outputdocs/outputdocs_Save');
+            require_once 'classes/model/Process.php';
+            G::LoadClass( 'processMap');
+            $sProcessUID = $aFields['PRO_UID'];
+            $oProcess = new Process();
+            $oProcessMap = new ProcessMap();
+            $aFields  = $oProcess->load($sProcessUID);
+            $G_PUBLISH->AddContent('propeltable', 'paged-table', 'dynaforms/dynaforms_ShortList', $oProcessMap->getDynaformsCriteria($sProcessUID), $aFields);
+            */
+            break;
+        case 'ACROFORM':
+            $G_PUBLISH->AddContent( 'xmlform', 'xmlform', 'outputdocs/outputdocs_Properties', '', $aFields, '../outputdocs/outputdocs_Save' );
+            break;
+    }
+    G::RenderPage( 'publish', 'raw' );
 } catch (Exception $oException) {
-  die($oException->getMessage());
+    die( $oException->getMessage() );
 }
+
