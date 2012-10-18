c1de21c87f6a44b40642fa0dd1037cdfaa4c7d60
norahmollo <norah@colosa.com>
2012-10-18 15:08:40 +0000

    CODE STYLE Formating workflow/engine/methods/cases
    Change format files in workflow/engine/methods/cases

--- workflow/engine/methods/cases/caseNotesAjax.php
+++ workflow/engine/methods/cases/caseNotesAjax.php
@@ -1,72 +1,76 @@
 <?php
-if (!isset($_REQUEST ['action'])) {
-  $res ['success'] = 'failure';
-  $res ['message'] = 'You may request an action';
-  print G::json_encode($res);
-  die ();
+if (! isset( $_REQUEST['action'] )) {
+    $res['success'] = 'failure';
+    $res['message'] = 'You may request an action';
+    print G::json_encode( $res );
+    die();
 }
-if (!function_exists($_REQUEST ['action'])) {
-  $res ['success'] = 'failure';
-  $res ['message'] = 'The requested action does not exist';
-  header("Content-Type: application/json");
-  print G::json_encode($res);
-  die ();
-}
-
-$functionName = $_REQUEST ['action'];
-$functionParams = isset($_REQUEST ['params']) ? $_REQUEST ['params'] : array();
-
-$functionName($functionParams);
-
-function getExtJSParams() {
-  $validParams = array('callback' => '', 'dir' => 'DESC', 'sort' => '', 'start' => 0, 'limit' => 25, 'filter' => '', 'search' => '', 'action' => '', 'xaction' => '', 'data' => '', 'status' => '', 'query' => '', 'fields' => "");
-  $result = array();
-  foreach ($validParams as $paramName => $paramDefault) {
-    $result[$paramName] = isset($_REQUEST[$paramName]) ? $_REQUEST[$paramName] : isset($_REQUEST[$paramName]) ? $_REQUEST[$paramName] : $paramDefault;
-  }
-  return $result;
+if (! function_exists( $_REQUEST['action'] )) {
+    $res['success'] = 'failure';
+    $res['message'] = 'The requested action does not exist';
+    header( "Content-Type: application/json" );
+    print G::json_encode( $res );
+    die();
 }
 
-function sendJsonResultGeneric($response, $callback) {
-  header("Content-Type: application/json");
-  $finalResponse = G::json_encode($response);
-  if ($callback != '') {
-    print $callback . "($finalResponse);";
-  } else {
-    print $finalResponse;
-  }
+$functionName = $_REQUEST['action'];
+$functionParams = isset( $_REQUEST['params'] ) ? $_REQUEST['params'] : array ();
+
+$functionName( $functionParams );
+
+function getExtJSParams ()
+{
+    $validParams = array ('callback' => '','dir' => 'DESC','sort' => '','start' => 0,'limit' => 25,'filter' => '','search' => '','action' => '','xaction' => '','data' => '','status' => '','query' => '','fields' => ""
+    );
+    $result = array ();
+    foreach ($validParams as $paramName => $paramDefault) {
+        $result[$paramName] = isset( $_REQUEST[$paramName] ) ? $_REQUEST[$paramName] : isset( $_REQUEST[$paramName] ) ? $_REQUEST[$paramName] : $paramDefault;
+    }
+    return $result;
 }
 
-function getNotesList() {
-  extract(getExtJSParams());
-  require_once ( "classes/model/AppNotes.php" );
-  if ((isset($_REQUEST['appUid'])) && (trim($_REQUEST['appUid']) != "")) {
-    $appUid = $_REQUEST['appUid'];
-  } else {
-    $appUid = $_SESSION['APPLICATION'];
-  }
-  $usrUid = (isset($_SESSION['USER_LOGGED'])) ? $_SESSION['USER_LOGGED'] : "";
-  $appNotes = new AppNotes();
-  $response = $appNotes->getNotesList($appUid, '', $start, $limit);
-  sendJsonResultGeneric($response['array'], $callback);
+function sendJsonResultGeneric ($response, $callback)
+{
+    header( "Content-Type: application/json" );
+    $finalResponse = G::json_encode( $response );
+    if ($callback != '') {
+        print $callback . "($finalResponse);";
+    } else {
+        print $finalResponse;
+    }
 }
 
-function postNote() {
-  extract(getExtJSParams());
-  if ((isset($_REQUEST['appUid'])) && (trim($_REQUEST['appUid']) != "")) {
-    $appUid = $_REQUEST['appUid'];
-  } else {
-    $appUid = $_SESSION['APPLICATION'];
-  }
-  $usrUid = (isset($_SESSION['USER_LOGGED'])) ? $_SESSION['USER_LOGGED'] : "";
-  require_once ( "classes/model/AppNotes.php" );
-
-  $noteContent=addslashes($_POST['noteText']);
-
-  $appNotes=new AppNotes();
-  $response=$appNotes->postNewNote($appUid, $usrUid, $noteContent);
-  
-  sendJsonResultGeneric($response, $callback);
+function getNotesList ()
+{
+    extract( getExtJSParams() );
+    require_once ("classes/model/AppNotes.php");
+    if ((isset( $_REQUEST['appUid'] )) && (trim( $_REQUEST['appUid'] ) != "")) {
+        $appUid = $_REQUEST['appUid'];
+    } else {
+        $appUid = $_SESSION['APPLICATION'];
+    }
+    $usrUid = (isset( $_SESSION['USER_LOGGED'] )) ? $_SESSION['USER_LOGGED'] : "";
+    $appNotes = new AppNotes();
+    $response = $appNotes->getNotesList( $appUid, '', $start, $limit );
+    sendJsonResultGeneric( $response['array'], $callback );
 }
 
-?>
+function postNote ()
+{
+    extract( getExtJSParams() );
+    if ((isset( $_REQUEST['appUid'] )) && (trim( $_REQUEST['appUid'] ) != "")) {
+        $appUid = $_REQUEST['appUid'];
+    } else {
+        $appUid = $_SESSION['APPLICATION'];
+    }
+    $usrUid = (isset( $_SESSION['USER_LOGGED'] )) ? $_SESSION['USER_LOGGED'] : "";
+    require_once ("classes/model/AppNotes.php");
+
+    $noteContent = addslashes( $_POST['noteText'] );
+
+    $appNotes = new AppNotes();
+    $response = $appNotes->postNewNote( $appUid, $usrUid, $noteContent );
+
+    sendJsonResultGeneric( $response, $callback );
+}
+
