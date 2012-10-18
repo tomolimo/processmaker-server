2cb0b136d1163985c3c93eaadfb714c8273c6c30
norahmollo <norah@colosa.com>
2012-10-18 15:42:36 +0000

    CODE STYLE Formating workflow/engine/methods/cases
    Change format files in workflow/engine/methods/cases

--- workflow/engine/methods/cases/cases_SchedulerGetTasks.php
+++ workflow/engine/methods/cases/cases_SchedulerGetTasks.php
@@ -1,25 +1,23 @@
-<select style="width: 300px;" name="form[TAS_UID]" id="form[TAS_UID]" class="module_app_input___gray" required="1">
-
+<select style="width: 300px;" name="form[TAS_UID]" id="form[TAS_UID]"
+	class="module_app_input___gray" required="1">
 <?php
 //$oUserId = '2963666854afbb1cea372c4011254883';
 //$oProcessId = '9977730834afd2a0deecaf3040551794';
 $oUserId = $_POST['USR_UID'];
 $oProcessId = $_POST['PRO_UID'];
-
-G::LoadClass('case');
+G::LoadClass( 'case' );
 $oCase = new Cases();
 $startTasks = $oCase->getStartCases( $oUserId );
-
-foreach($startTasks as $task){
-   if((isset($task['pro_uid']))&&($task['pro_uid'] == $oProcessId)){
-       $taskValue = explode('(',$task['value']);
-       $tasksLastIndex = count($taskValue)-1;
-       $taskValue = explode(')',$taskValue[$tasksLastIndex]);
-
-       echo "<option value=\"".$task['uid']."\">".$taskValue[0]."</option>";
-   }
+foreach ($startTasks as $task) {
+    if ((isset( $task['pro_uid'] )) && ($task['pro_uid'] == $oProcessId)) {
+        $taskValue = explode( '(', $task['value'] );
+        $tasksLastIndex = count( $taskValue ) - 1;
+        $taskValue = explode( ')', $taskValue[$tasksLastIndex] );
+        echo "<option value=\"" . $task['uid'] . "\">" . $taskValue[0] . "</option>";
+    }
 }
 //print_r($startTasks);
 //  echo "<option value=\"".$value."\">".$label."</option>";
 ?>
 </select>
+
--- workflow/engine/methods/cases/cases_Scheduler_Edit.php
+++ workflow/engine/methods/cases/cases_Scheduler_Edit.php
@@ -12,20 +12,19 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  * For more information, contact Colosa Inc, 2566 Le Jeune Rd.,
  * Coral Gables, FL, 33134, USA, or email info@colosa.com.
- *
  */
 try {
-	global $RBAC;
-  
-/*  
+    global $RBAC;
+
+/*
   switch ($RBAC->userCanAccess('PM_FACTORY'))
   {
   	case -2:
@@ -39,100 +38,98 @@ try {
   	  die;
   	break;
   }
-*/  
+*/
 
-	require_once 'classes/model/CaseScheduler.php';
-	require_once 'classes/model/Process.php';
-	require_once 'classes/model/Task.php';
+    require_once 'classes/model/CaseScheduler.php';
+    require_once 'classes/model/Process.php';
+    require_once 'classes/model/Task.php';
 
-//	$G_MAIN_MENU           = 'processmaker';
-//	$G_ID_MENU_SELECTED    = 'CASES';
-	$G_PUBLISH             = new Publisher;
-  
-	G::LoadClass('case');
-  
-	/* Prepare page before to show */
-  
+    //	$G_MAIN_MENU           = 'processmaker';
+    //	$G_ID_MENU_SELECTED    = 'CASES';
+    $G_PUBLISH = new Publisher();
 
-	$oCaseScheduler = new CaseScheduler();
-	$aFields = $oCaseScheduler->load($_GET['SCH_UID']);
- 	
- 
-	$aFields['UID_SCHEDULER'] ="scheduler" ;
+    G::LoadClass( 'case' );
 
-  // load according the scheduler option selected daily/weekly/monthly/one time
-	$nOpt = $aFields['SCH_OPTION'];
-	switch($nOpt){
-		case 1 :  $aStartDay =  explode('|', $aFields['SCH_DAYS_PERFORM_TASK']);
-  				if($aStartDay[0] != 3) {
-  					$aFields['SCH_DAYS_PERFORM_TASK'] = $aStartDay[0];
-  				} else {	
-  					$aFields['SCH_DAYS_PERFORM_TASK'] = $aStartDay[0];
-  					$aFields['SCH_DAYS_PERFORM_TASK_OPT_3'] = $aStartDay[1]; 
-  			  	}
-  			  	
-  				break;
-		case 2 : 
-				$aFields['SCH_WEEK_DAYS_2'] = $aFields['SCH_WEEK_DAYS'];
-  				break;
-		case 3 : // $nStartDay = $aFields['SCH_START_DAY'];
-  				$aStartDay =  explode('|', $aFields['SCH_START_DAY']);
-				if($aStartDay[0] == 1){
-					$aFields['SCH_START_DAY_OPT_1'] = $aStartDay[1]; 
-				} else {
-					$aFields['SCH_START_DAY_OPT_2_WEEKS'] = $aStartDay[1]; 
-					$aFields['SCH_START_DAY_OPT_2_DAYS_WEEK'] = $aStartDay[2]; 
-				}
-				$aFields['SCH_START_DAY'] = $aStartDay[0];
-				$aFields['SCH_MONTHS_2'] = $aFields['SCH_MONTHS'];
-				$aFields['SCH_MONTHS_3'] = $aFields['SCH_MONTHS'];
-	  			break;
-		case 4 :
-				 
-	  			break;
-	}
-  
-  	$aFields['SCH_START_TIME'] = date('H:i' , strtotime($aFields['SCH_START_TIME']));
-  	$aFields['PREV_SCH_START_TIME'] = $aFields['SCH_START_TIME'];
+    /* Prepare page before to show */
 
-  	$aFields['SCH_START_DATE'] = date('Y-m-d', strtotime($aFields['SCH_START_DATE']));
-  	$aFields['PREV_SCH_START_DATE'] = $aFields['SCH_START_DATE'];
+    $oCaseScheduler = new CaseScheduler();
+    $aFields = $oCaseScheduler->load( $_GET['SCH_UID'] );
 
-	if(!empty($aFields['SCH_END_DATE'])){
-            $aFields['SCH_END_DATE'] = date('Y-m-d', strtotime($aFields['SCH_END_DATE']));
-            $aFields['PREV_SCH_END_DATE'] = date('Y-m-d', strtotime($aFields['SCH_END_DATE']));
-        }
-	if($aFields['SCH_REPEAT_STOP_IF_RUNNING']==0 || $aFields['SCH_REPEAT_STOP_IF_RUNNING']==null){
-            $aFields['SCH_REPEAT_STOP_IF_RUNNING']=null;
-        }else {
-            $aFields['SCH_REPEAT_STOP_IF_RUNNING']='On';
-        }
+    $aFields['UID_SCHEDULER'] = "scheduler";
 
-        $aFields['SCH_USER_NAME'] = $aFields['SCH_DEL_USER_NAME'];
-        $aFields['SCH_USER_PASSWORD'] = 'DefaultPM';
-        $aFields['SCH_USER_UID'] = $aFields['SCH_DEL_USER_UID'];
-        $aFields['SCH_START_DATE'] = date ("Y-m-d",strtotime($aFields['SCH_START_DATE']));
+    // load according the scheduler option selected daily/weekly/monthly/one time
+    $nOpt = $aFields['SCH_OPTION'];
+    switch ($nOpt) {
+        case 1:
+            $aStartDay = explode( '|', $aFields['SCH_DAYS_PERFORM_TASK'] );
+            if ($aStartDay[0] != 3) {
+                $aFields['SCH_DAYS_PERFORM_TASK'] = $aStartDay[0];
+            } else {
+                $aFields['SCH_DAYS_PERFORM_TASK'] = $aStartDay[0];
+                $aFields['SCH_DAYS_PERFORM_TASK_OPT_3'] = $aStartDay[1];
+            }
 
-        // validating if any of the advanced fields is non empty
-//        var_dump($aFields['SCH_END_DATE']);
-//        var_dump($aFields['SCH_REPEAT_EVERY']);
-//        die();
-        if ($aFields['SCH_END_DATE']!=NULL||trim($aFields['SCH_REPEAT_EVERY'])!=''){
-            $aFields['SCH_ADVANCED'] = 'true';
-        } else {
-            $aFields['SCH_ADVANCED'] = 'false';
-        }
+            break;
+        case 2:
+            $aFields['SCH_WEEK_DAYS_2'] = $aFields['SCH_WEEK_DAYS'];
+            break;
+        case 3: // $nStartDay = $aFields['SCH_START_DAY'];
+            $aStartDay = explode( '|', $aFields['SCH_START_DAY'] );
+            if ($aStartDay[0] == 1) {
+                $aFields['SCH_START_DAY_OPT_1'] = $aStartDay[1];
+            } else {
+                $aFields['SCH_START_DAY_OPT_2_WEEKS'] = $aStartDay[1];
+                $aFields['SCH_START_DAY_OPT_2_DAYS_WEEK'] = $aStartDay[2];
+            }
+            $aFields['SCH_START_DAY'] = $aStartDay[0];
+            $aFields['SCH_MONTHS_2'] = $aFields['SCH_MONTHS'];
+            $aFields['SCH_MONTHS_3'] = $aFields['SCH_MONTHS'];
+            break;
+        case 4:
 
-        $aFields['PRO_UID_TMP'] = isset($_GET['PRO_UID'])?$_GET['PRO_UID']:$_SESSION['PROCESS'];
-        $aFields['PHP_START_DATE'] = date('Y-m-d');
-        $aFields['PHP_END_DATE']   = date('Y-m-d', mktime(0, 0, 0, date('m'), date('d'), date('Y') + 5));
+            break;
+    }
 
-	$G_PUBLISH->AddContent('xmlform', 'xmlform', 'cases/cases_Scheduler_Edit.xml', '', $aFields, 'cases_Scheduler_Update');
+    $aFields['SCH_START_TIME'] = date( 'H:i', strtotime( $aFields['SCH_START_TIME'] ) );
+    $aFields['PREV_SCH_START_TIME'] = $aFields['SCH_START_TIME'];
 
-	G::RenderPage('publishBlank','blank');
-  
+    $aFields['SCH_START_DATE'] = date( 'Y-m-d', strtotime( $aFields['SCH_START_DATE'] ) );
+    $aFields['PREV_SCH_START_DATE'] = $aFields['SCH_START_DATE'];
+
+    if (! empty( $aFields['SCH_END_DATE'] )) {
+        $aFields['SCH_END_DATE'] = date( 'Y-m-d', strtotime( $aFields['SCH_END_DATE'] ) );
+        $aFields['PREV_SCH_END_DATE'] = date( 'Y-m-d', strtotime( $aFields['SCH_END_DATE'] ) );
+    }
+    if ($aFields['SCH_REPEAT_STOP_IF_RUNNING'] == 0 || $aFields['SCH_REPEAT_STOP_IF_RUNNING'] == null) {
+        $aFields['SCH_REPEAT_STOP_IF_RUNNING'] = null;
+    } else {
+        $aFields['SCH_REPEAT_STOP_IF_RUNNING'] = 'On';
+    }
+
+    $aFields['SCH_USER_NAME'] = $aFields['SCH_DEL_USER_NAME'];
+    $aFields['SCH_USER_PASSWORD'] = 'DefaultPM';
+    $aFields['SCH_USER_UID'] = $aFields['SCH_DEL_USER_UID'];
+    $aFields['SCH_START_DATE'] = date( "Y-m-d", strtotime( $aFields['SCH_START_DATE'] ) );
+
+    // validating if any of the advanced fields is non empty
+    //        var_dump($aFields['SCH_END_DATE']);
+    //        var_dump($aFields['SCH_REPEAT_EVERY']);
+    //        die();
+    if ($aFields['SCH_END_DATE'] != NULL || trim( $aFields['SCH_REPEAT_EVERY'] ) != '') {
+        $aFields['SCH_ADVANCED'] = 'true';
+    } else {
+        $aFields['SCH_ADVANCED'] = 'false';
+    }
+
+    $aFields['PRO_UID_TMP'] = isset( $_GET['PRO_UID'] ) ? $_GET['PRO_UID'] : $_SESSION['PROCESS'];
+    $aFields['PHP_START_DATE'] = date( 'Y-m-d' );
+    $aFields['PHP_END_DATE'] = date( 'Y-m-d', mktime( 0, 0, 0, date( 'm' ), date( 'd' ), date( 'Y' ) + 5 ) );
+
+    $G_PUBLISH->AddContent( 'xmlform', 'xmlform', 'cases/cases_Scheduler_Edit.xml', '', $aFields, 'cases_Scheduler_Update' );
+
+    G::RenderPage( 'publishBlank', 'blank' );
+
 } catch (Exception $oException) {
-	die($oException->getMessage());
+    die( $oException->getMessage() );
 }
 
-?>
