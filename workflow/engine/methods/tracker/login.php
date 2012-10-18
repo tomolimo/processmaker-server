7c9238d89500a5cf9316631dc728a99e3cb52832
norahmollo <norah@colosa.com>
2012-10-18 17:17:30 +0000

    CODE STYLE Formating worflow/engine/methods/tracker/
    Change format files in worflow/engine/methods/tracker/

--- workflow/engine/methods/tracker/login.php
+++ workflow/engine/methods/tracker/login.php
@@ -1,5 +1,5 @@
 <?php
- /**
+/**
  * login.php
  *
  * ProcessMaker Open Source Edition
@@ -12,34 +12,28 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  * For more information, contact Colosa Inc, 2566 Le Jeune Rd.,
  * Coral Gables, FL, 33134, USA, or email info@colosa.com.
- *
  */
- 
-   /*
-   * Login for authentication of Case Tracker
-   *
-   * @author Everth S. Berrios Morales <everth@colosa.com>
-   *   
-   */
- 
 
-$G_PUBLISH = new Publisher;
+/*
+* Login for authentication of Case Tracker
+*
+* @author Everth S. Berrios Morales <everth@colosa.com>
+*
+*/
+
+$G_PUBLISH = new Publisher();
 //echo G::generateUniqueNumber();
-$G_PUBLISH->AddContent('xmlform', 'xmlform', 'tracker/login', '', '', SYS_URI.'tracker/authentication.php');
+$G_PUBLISH->AddContent( 'xmlform', 'xmlform', 'tracker/login', '', '', SYS_URI . 'tracker/authentication.php' );
 
 G::RenderPage( "publish" );
 session_destroy();
 session_start();
 
- 
- 
- 
- 
