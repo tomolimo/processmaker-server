69430f97787f34127ab2d08f19966ba6be49e5d6
Ralph Asendeteufrer <ralph@colosa.com>
2012-10-18 14:22:10 -0400

    COD STYLE changes  files modified:   install/autoinstallPlugins.php                    install/autoinstallProcesses.php                    install/heartbeatStatus.php                    install/install.php                    install/installServer.php                    install/newSite.php                    install/r.php

--- workflow/engine/methods/install/autoinstallProcesses.php
+++ workflow/engine/methods/install/autoinstallProcesses.php
@@ -12,47 +12,40 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  * For more information, contact Colosa Inc, 2566 Le Jeune Rd.,
  * Coral Gables, FL, 33134, USA, or email info@colosa.com.
- *
  */
-  G::LoadClass('Installer');
-	$inst = new Installer();
-	
-	G::LoadClass('processes');
-  $oProcess = new Processes();
-  
-  //Get Available autoinstall process
-	$availableProcess = $inst->getDirectoryFiles(PATH_OUTTRUNK."autoinstall","pm");
-	
-	
-	
-	$path=PATH_OUTTRUNK."autoinstall".PATH_SEP;
-	$message="";
-	foreach($availableProcess as $processfile){
-	  
-	  $oData = $oProcess->getProcessData ( $path . $processfile  );
-	  $Fields['PRO_FILENAME']  = $processfile;
+G::LoadClass( 'Installer' );
+$inst = new Installer();
+
+G::LoadClass( 'processes' );
+$oProcess = new Processes();
+
+//Get Available autoinstall process
+$availableProcess = $inst->getDirectoryFiles( PATH_OUTTRUNK . "autoinstall", "pm" );
+
+$path = PATH_OUTTRUNK . "autoinstall" . PATH_SEP;
+$message = "";
+foreach ($availableProcess as $processfile) {
+
+    $oData = $oProcess->getProcessData( $path . $processfile );
+    $Fields['PRO_FILENAME'] = $processfile;
     $Fields['IMPORT_OPTION'] = 2;
     $sProUid = $oData->process['PRO_UID'];
-    if ( $oProcess->processExists ( $sProUid ) ) { 
-      $message.="$processfile - Not imported (process exist)<br>";
-      
-    }else{
-      $oProcess->createProcessFromData ($oData, $path . $processfile );
-      $message.="$processfile - OK<br>";
+    if ($oProcess->processExists( $sProUid )) {
+        $message .= "$processfile - Not imported (process exist)<br>";
+
+    } else {
+        $oProcess->createProcessFromData( $oData, $path . $processfile );
+        $message .= "$processfile - OK<br>";
     }
-	}
-	
-  echo $message;
+}
 
+echo $message;
 
-  
-
-?>
