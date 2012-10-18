22c2db5a4ccc3f319a89e62288915600d955deb4
jennylee <jennylee@colosa.com>
2012-10-18 14:32:01 -0400

    CODE STYLE, workflow/engine/methods/triggers/
    FILES:
    triggersProperties.php
    triggersTree.php
    triggers_Ajax.php
    triggers_CreateWizard.php
    triggers_Delete.php
    triggers_Edit.php
    triggers_EditCustom.php
    triggers_EditWizard.php
    triggers_Save.php
    triggers_WizardSave.php
    triggers_WizardUpdate.php

--- workflow/engine/methods/triggers/triggersProperties.php
+++ workflow/engine/methods/triggers/triggersProperties.php
@@ -12,33 +12,31 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  * For more information, contact Colosa Inc, 2566 Le Jeune Rd.,
  * Coral Gables, FL, 33134, USA, or email info@colosa.com.
- *
  */
-if (($RBAC_Response=$RBAC->userCanAccess("PM_FACTORY"))!=1) return $RBAC_Response;
-require_once('classes/model/Triggers.php');
-
-if (isset($_GET['TRI_UID']))
-{
-	$oTrigger = new Triggers();
-	$aFields = $oTrigger->load($_GET['TRI_UID']);
+if (($RBAC_Response = $RBAC->userCanAccess( "PM_FACTORY" )) != 1) {
+    return $RBAC_Response;
 }
-else
-{
-	$aFields['PRO_UID']  = $_GET['PRO_UID'];
-	//$aFields['PRO_UID']  = (isset($_SESSION['PROCESS']) ? $_SESSION['PROCESS'] : '');
-	$aFields['TRI_TYPE'] = 'SCRIPT';
+require_once ('classes/model/Triggers.php');
+
+if (isset( $_GET['TRI_UID'] )) {
+    $oTrigger = new Triggers();
+    $aFields = $oTrigger->load( $_GET['TRI_UID'] );
+} else {
+    $aFields['PRO_UID'] = $_GET['PRO_UID'];
+    //$aFields['PRO_UID']  = (isset($_SESSION['PROCESS']) ? $_SESSION['PROCESS'] : '');
+    $aFields['TRI_TYPE'] = 'SCRIPT';
 }
 
-G::LoadClass('xmlfield_InputPM');
+G::LoadClass( 'xmlfield_InputPM' );
 $G_PUBLISH = new Publisher();
-$G_PUBLISH->AddContent('xmlform', 'xmlform', 'triggers/triggersProperties', '', $aFields, '../triggers/triggers_Save');
-G::RenderPage('publish', 'raw');
-?>
+$G_PUBLISH->AddContent( 'xmlform', 'xmlform', 'triggers/triggersProperties', '', $aFields, '../triggers/triggers_Save' );
+G::RenderPage( 'publish', 'raw' );
+
