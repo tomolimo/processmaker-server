f1b60265d367a70927839af5394262b03592cd7c
Ralph Asendeteufrer <ralph@colosa.com>
2012-10-18 13:33:43 -0400

    CODE STYLE files modified:   inputdocs/inputdocs_Delete.php                   inputdocs/inputdocs_Edit.php                   inputdocs/inputdocs_List.php                   inputdocs/inputdocs_Save.php

--- workflow/engine/methods/inputdocs/inputdocs_Delete.php
+++ workflow/engine/methods/inputdocs/inputdocs_Delete.php
@@ -12,100 +12,98 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  * For more information, contact Colosa Inc, 2566 Le Jeune Rd.,
  * Coral Gables, FL, 33134, USA, or email info@colosa.com.
- *
  */
 try {
-	global $RBAC;
-  switch ($RBAC->userCanAccess('PM_FACTORY')) {
-  	case -2:
-  	  G::SendTemporalMessage('ID_USER_HAVENT_RIGHTS_SYSTEM', 'error', 'labels');
-  	  G::header('location: ../login/login');
-  	  die;
-  	break;
-  	case -1:
-  	  G::SendTemporalMessage('ID_USER_HAVENT_RIGHTS_PAGE', 'error', 'labels');
-  	  G::header('location: ../login/login');
-  	  die;
-  	break;
-  }
-  
-  require_once 'classes/model/StepSupervisor.php';
-  require_once 'classes/model/ObjectPermission.php';
-  require_once 'classes/model/InputDocument.php';
-  require_once 'classes/model/Step.php';
-  require_once 'classes/model/ObjectPermission.php';
-  G::LoadClass( 'processMap' );
-  
-  if(isset($_POST['function']))
-    $sfunction =$_POST['function'];
-  else
-    $sfunction =$_POST['functions'];
-  
-  switch($sfunction){
-     
-            case 'getRelationInfDoc':
+    global $RBAC;
+    switch ($RBAC->userCanAccess( 'PM_FACTORY' )) {
+        case - 2:
+            G::SendTemporalMessage( 'ID_USER_HAVENT_RIGHTS_SYSTEM', 'error', 'labels' );
+            G::header( 'location: ../login/login' );
+            die();
+            break;
+        case - 1:
+            G::SendTemporalMessage( 'ID_USER_HAVENT_RIGHTS_PAGE', 'error', 'labels' );
+            G::header( 'location: ../login/login' );
+            die();
+            break;
+    }
+
+    require_once 'classes/model/StepSupervisor.php';
+    require_once 'classes/model/ObjectPermission.php';
+    require_once 'classes/model/InputDocument.php';
+    require_once 'classes/model/Step.php';
+    require_once 'classes/model/ObjectPermission.php';
+    G::LoadClass( 'processMap' );
+
+    if (isset( $_POST['function'] )) {
+        $sfunction = $_POST['function'];
+    } else {
+        $sfunction = $_POST['functions'];
+    }
+
+    switch ($sfunction) {
+        case 'getRelationInfDoc':
             try {
                 $oStepSupervisor = new StepSupervisor();
-                $fields2=$oStepSupervisor->loadInfo($_POST['INP_DOC_UID']);
-                $result->passed=true;
-                if(is_array($fields2)){
-                    $result->passed=false;
+                $fields2 = $oStepSupervisor->loadInfo( $_POST['INP_DOC_UID'] );
+                $result->passed = true;
+                if (is_array( $fields2 )) {
+                    $result->passed = false;
                 }
                 $result->success = true;
-                $result->msg = $result->passed ? '' : G::LoadTranslation('ID_INPUTDOCUMENT_TASK_RELATION_EXISTS');
-           }
-           catch (Exception $e) {
-              $result->success = false;
-              $result->passed  = false;
-              $result->msg = $e->getMessage();
-           }
-        print G::json_encode($result);
-  	break;
-  	case 'deleteInputDocument':
-        try {
-        $oStepSupervisor = new StepSupervisor();
-        $fields2=$oStepSupervisor->loadInfo($_POST['INP_DOC_UID']);
-        $oStepSupervisor->remove($fields2['STEP_UID']);
+                $result->msg = $result->passed ? '' : G::LoadTranslation( 'ID_INPUTDOCUMENT_TASK_RELATION_EXISTS' );
+            } catch (Exception $e) {
+                $result->success = false;
+                $result->passed = false;
+                $result->msg = $e->getMessage();
+            }
+            print G::json_encode( $result );
+            break;
+        case 'deleteInputDocument':
+            try {
+                $oStepSupervisor = new StepSupervisor();
+                $fields2 = $oStepSupervisor->loadInfo( $_POST['INP_DOC_UID'] );
+                $oStepSupervisor->remove( $fields2['STEP_UID'] );
 
-        $oPermission = new ObjectPermission();
-        $fields3=$oPermission->loadInfo($_POST['INP_DOC_UID']);
-        if(is_array($fields3))
-         $oPermission->remove($fields3['OP_UID']);
+                $oPermission = new ObjectPermission();
+                $fields3 = $oPermission->loadInfo( $_POST['INP_DOC_UID'] );
+                if (is_array( $fields3 )) {
+                    $oPermission->remove( $fields3['OP_UID'] );
+                }
 
-        $oInputDocument = new InputDocument();
-        $fields = $oInputDocument->load($_POST['INP_DOC_UID']);
+                $oInputDocument = new InputDocument();
+                $fields = $oInputDocument->load( $_POST['INP_DOC_UID'] );
 
-        $oInputDocument->remove($_POST['INP_DOC_UID']);
+                $oInputDocument->remove( $_POST['INP_DOC_UID'] );
 
-        $oStep = new Step();
-        $oStep->removeStep('INPUT_DOCUMENT', $_POST['INP_DOC_UID']);
+                $oStep = new Step();
+                $oStep->removeStep( 'INPUT_DOCUMENT', $_POST['INP_DOC_UID'] );
 
-        $oOP = new ObjectPermission();
-        $oOP->removeByObject('INPUT', $_POST['INP_DOC_UID']);
+                $oOP = new ObjectPermission();
+                $oOP->removeByObject( 'INPUT', $_POST['INP_DOC_UID'] );
 
-        //refresh dbarray with the last change in inputDocument
-        $oMap = new processMap();
-        $oCriteria = $oMap->getInputDocumentsCriteria($fields['PRO_UID']);
+                //refresh dbarray with the last change in inputDocument
+                $oMap = new processMap();
+                $oCriteria = $oMap->getInputDocumentsCriteria( $fields['PRO_UID'] );
 
-        $result->success = true;
-        $result->msg = G::LoadTranslation('ID_INPUTDOCUMENT_REMOVED');
-      } catch (Exception $e) {
-        $result->success = false;
-        $result->msg = $e->getMessage();
-      }
-      print G::json_encode($result);
-     break;
+                $result->success = true;
+                $result->msg = G::LoadTranslation( 'ID_INPUTDOCUMENT_REMOVED' );
+            } catch (Exception $e) {
+                $result->success = false;
+                $result->msg = $e->getMessage();
+            }
+            print G::json_encode( $result );
+            break;
+    }
+} catch (Exception $oException) {
+    die( $oException->getMessage() );
 }
-}
-catch (Exception $oException) {
-	die($oException->getMessage());
-}
-?>
+
