9e9f4be8e6696fdefb9f6f54d92b69126cf4f87e
norahmollo <norah@colosa.com>
2012-10-19 18:50:57 +0000

    CODE STYLE Format
    Change format

--- workflow/engine/classes/model/ReportTable.php
+++ workflow/engine/classes/model/ReportTable.php
@@ -1,7 +1,8 @@
 <?php
 /**
  * ReportTable.php
- * @package    workflow.engine.classes.model
+ *
+ * @package workflow.engine.classes.model
  *
  * ProcessMaker Open Source Edition
  * Copyright (C) 2004 - 2011 Colosa Inc.
@@ -13,11 +14,11 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  * For more information, contact Colosa Inc, 2566 Le Jeune Rd.,
  * Coral Gables, FL, 33134, USA, or email info@colosa.com.
@@ -27,205 +28,200 @@
 require_once 'classes/model/Content.php';
 require_once 'classes/model/om/BaseReportTable.php';
 
-
 /**
  * Skeleton subclass for representing a row from the 'REPORT_TABLE' table.
  *
  *
  *
  * You should add additional methods to this class to meet the
- * application requirements.  This class will only be generated as
+ * application requirements. This class will only be generated as
  * long as it does not already exist in the output directory.
  *
- * @package    workflow.engine.classes.model
+ * @package workflow.engine.classes.model
  */
-class ReportTable extends BaseReportTable {
-  /**
-   * This value goes in the content table
-   * @var        string
-   */
-  protected $rep_tab_title = '';
+class ReportTable extends BaseReportTable
+{
+    /**
+     * This value goes in the content table
+     *
+     * @var string
+     */
+    protected $rep_tab_title = '';
 
-  /**
-   * Get the rep_tab_title column value.
-   * @return     string
-   */
-  public function getRepTabTitle() {
-    if ( $this->getRepTabUid() == "" ) {
-      throw ( new Exception( "Error in getRepTabTitle, the getRepTabUid() can't be blank") );
-    }
-    $lang = defined ( 'SYS_LANG' ) ? SYS_LANG : 'en';
-    $this->rep_tab_title = Content::load ( 'REP_TAB_TITLE', '', $this->getRepTabUid(), $lang );
-    return $this->rep_tab_title;
-  }
-
-  /**
-   * Set the rep_tab_title column value.
-   *
-   * @param      string $v new value
-   * @return     void
-   */
-  public function setRepTabTitle($v)
-  {
-    if ( $this->getRepTabUid() == "" ) {
-      throw ( new Exception( "Error in setRepTabTitle, the setRepTabUid() can't be blank") );
-    }
-    $v=isset($v)?((string)$v):'';
-    $lang = defined ( 'SYS_LANG') ? SYS_LANG : 'en';
-    if ($this->rep_tab_title !== $v || $v==="") {
-      $this->rep_tab_title = $v;
-      $res = Content::addContent( 'REP_TAB_TITLE', '', $this->getRepTabUid(), $lang, $this->rep_tab_title );
-      return $res;
-    }
-    return 0;
-  }
-
-  public function load($RepTabUid)
-  {
-    try {
-      $oRow = ReportTablePeer::retrieveByPK( $RepTabUid );
-      if (!is_null($oRow))
-      {
-        $aFields = $oRow->toArray(BasePeer::TYPE_FIELDNAME);
-        $this->fromArray($aFields, BasePeer::TYPE_FIELDNAME);
-        $this->setNew(false);
-        $this->setRepTabTitle($aFields['REP_TAB_TITLE']=$this->getRepTabTitle());
-        return $aFields;
-      }
-      else {
-        //throw( new Exception( "The row '$RepTabUid' in table ReportTable doesn't exist!" ));
-        return array();
-      }
-    }
-    catch (Exception $oError) {
-      throw($oError);
-    }
-  }
-  public function create($aData)
-  {
-    $con = Propel::getConnection(ReportTablePeer::DATABASE_NAME);
-    try
+    /**
+     * Get the rep_tab_title column value.
+     *
+     * @return string
+     */
+    public function getRepTabTitle ()
     {
-      $con->begin();
-      if ( isset ( $aData['REP_TAB_UID'] ) && $aData['REP_TAB_UID']== '' )
-        unset ( $aData['REP_TAB_UID'] );
-      if ( !isset ( $aData['REP_TAB_UID'] ) )
-        $this->setRepTabUid(G::generateUniqueID());
-      else
-        $this->setRepTabUid($aData['REP_TAB_UID'] );
+        if ($this->getRepTabUid() == "") {
+            throw (new Exception( "Error in getRepTabTitle, the getRepTabUid() can't be blank" ));
+        }
+        $lang = defined( 'SYS_LANG' ) ? SYS_LANG : 'en';
+        $this->rep_tab_title = Content::load( 'REP_TAB_TITLE', '', $this->getRepTabUid(), $lang );
+        return $this->rep_tab_title;
+    }
 
-      $this->setProUid($aData['PRO_UID']);
-      $this->setRepTabName($aData['REP_TAB_NAME']);
-      $this->setRepTabType($aData['REP_TAB_TYPE']);
-      if ( !isset ( $aData['REP_TAB_GRID'] ) )
-        $this->setRepTabGrid("");
-      else
-        $this->setRepTabGrid( $aData['REP_TAB_GRID'] );
-      if ( !isset ( $aData['REP_TAB_CONNECTION'] ) )
-        $this->setRepTabConnection("report");
-      else
-        $this->setRepTabConnection( $aData['REP_TAB_CONNECTION'] );
-      $this->setRepTabCreateDate(date('Y-m-d H:i:s'));
-      $this->setRepTabStatus('ACTIVE');
+    /**
+     * Set the rep_tab_title column value.
+     *
+     * @param string $v new value
+     * @return void
+     */
+    public function setRepTabTitle ($v)
+    {
+        if ($this->getRepTabUid() == "") {
+            throw (new Exception( "Error in setRepTabTitle, the setRepTabUid() can't be blank" ));
+        }
+        $v = isset( $v ) ? ((string) $v) : '';
+        $lang = defined( 'SYS_LANG' ) ? SYS_LANG : 'en';
+        if ($this->rep_tab_title !== $v || $v === "") {
+            $this->rep_tab_title = $v;
+            $res = Content::addContent( 'REP_TAB_TITLE', '', $this->getRepTabUid(), $lang, $this->rep_tab_title );
+            return $res;
+        }
+        return 0;
+    }
 
-      if($this->validate())
-      {
-        if ( !isset ( $aData['REP_TAB_TITLE'] ) )
-          $this->setRepTabTitle("");
-        else
-          $this->setRepTabTitle( $aData['REP_TAB_TITLE'] );
+    public function load ($RepTabUid)
+    {
+        try {
+            $oRow = ReportTablePeer::retrieveByPK( $RepTabUid );
+            if (! is_null( $oRow )) {
+                $aFields = $oRow->toArray( BasePeer::TYPE_FIELDNAME );
+                $this->fromArray( $aFields, BasePeer::TYPE_FIELDNAME );
+                $this->setNew( false );
+                $this->setRepTabTitle( $aFields['REP_TAB_TITLE'] = $this->getRepTabTitle() );
+                return $aFields;
+            } else {
+                //throw( new Exception( "The row '$RepTabUid' in table ReportTable doesn't exist!" ));
+                return array ();
+            }
+        } catch (Exception $oError) {
+            throw ($oError);
+        }
+    }
 
-        $result=$this->save();
-        $con->commit();
-        return $result;
-      }
-      else
-      {
-        $con->rollback();
-        throw(new Exception("Failed Validation in class ".get_class($this)."."));
-      }
-    }
-    catch(Exception $e)
+    public function create ($aData)
     {
-      $con->rollback();
-      throw($e);
-    }
-  }
-  public function update($fields)
-  {
-    $con = Propel::getConnection(ReportTablePeer::DATABASE_NAME);
-    try
-    {
-      $con->begin();
-      $this->load($fields['REP_TAB_UID']);
-      $this->fromArray($fields,BasePeer::TYPE_FIELDNAME);
-      
-      $sDataBase = 'database_' . strtolower(DB_ADAPTER);
-      if(G::LoadSystemExist($sDataBase)){
-        G::LoadSystem($sDataBase);
-        $oDataBase = new database();
-        $oValidate = $oDataBase->getValidate($this->validate());
-      } else {
-        $oValidate = $this->validate();
-      }
-      // if($this->validate())
-      if($oValidate)
-      {
-        $contentResult=0;
-        if (array_key_exists("REP_TAB_TITLE", $fields)) $contentResult+=$this->setRepTabTitle($fields["REP_TAB_TITLE"]);
-        $result=$this->save();
-        $result=($result==0)?($contentResult>0?1:0):$result;
-        $con->commit();
-        return $result;
-      }
-      else
-      {
-        $con->rollback();
-        $validationE=new Exception("Failed Validation in class ".get_class($this).".");
-        $validationE->aValidationFailures = $this->getValidationFailures();
-        throw($validationE);
-      }
-    }
-    catch(Exception $e)
-    {
-      $con->rollback();
-      throw($e);
-    }
-  }
-  public function remove($RepTabUid)
-  {
-    $con = Propel::getConnection(ReportTablePeer::DATABASE_NAME);
-    try
-    {
-      $con->begin();
-      $oRepTab = ReportTablePeer::retrieveByPK( $RepTabUid );
-      if (!is_null($oRepTab)) {
-        Content::removeContent( 'REP_TAB_TITLE', '', $this->getRepTabUid());
-        $result = $oRepTab->delete();
-        $con->commit();
-      }
-      return $result;
-    }
-    catch(Exception $e)
-    {
-      $con->rollback();
-      throw($e);
-    }
-  }
+        $con = Propel::getConnection( ReportTablePeer::DATABASE_NAME );
+        try {
+            $con->begin();
+            if (isset( $aData['REP_TAB_UID'] ) && $aData['REP_TAB_UID'] == '') {
+                unset( $aData['REP_TAB_UID'] );
+            }
+            if (! isset( $aData['REP_TAB_UID'] )) {
+                $this->setRepTabUid( G::generateUniqueID() );
+            } else {
+                $this->setRepTabUid( $aData['REP_TAB_UID'] );
+            }
 
-  function reportTableExists ( $RepTabUid ) {
-    $con = Propel::getConnection(ReportTablePeer::DATABASE_NAME);
-    try {
-      $oRepTabUid = ReportTablePeer::retrieveByPk( $RepTabUid );
-      if (is_object($oRepTabUid) && get_class ($oRepTabUid) == 'ReportTable' ) {
-        return true;
-      }
-      else {
-        return false;
-      }
+            $this->setProUid( $aData['PRO_UID'] );
+            $this->setRepTabName( $aData['REP_TAB_NAME'] );
+            $this->setRepTabType( $aData['REP_TAB_TYPE'] );
+            if (! isset( $aData['REP_TAB_GRID'] )) {
+                $this->setRepTabGrid( "" );
+            } else {
+                $this->setRepTabGrid( $aData['REP_TAB_GRID'] );
+            }
+            if (! isset( $aData['REP_TAB_CONNECTION'] )) {
+                $this->setRepTabConnection( "report" );
+            } else {
+                $this->setRepTabConnection( $aData['REP_TAB_CONNECTION'] );
+            }
+            $this->setRepTabCreateDate( date( 'Y-m-d H:i:s' ) );
+            $this->setRepTabStatus( 'ACTIVE' );
+
+            if ($this->validate()) {
+                if (! isset( $aData['REP_TAB_TITLE'] )) {
+                    $this->setRepTabTitle( "" );
+                } else {
+                    $this->setRepTabTitle( $aData['REP_TAB_TITLE'] );
+                }
+                $result = $this->save();
+                $con->commit();
+                return $result;
+            } else {
+                $con->rollback();
+                throw (new Exception( "Failed Validation in class " . get_class( $this ) . "." ));
+            }
+        } catch (Exception $e) {
+            $con->rollback();
+            throw ($e);
+        }
     }
-    catch (Exception $oError) {
-      throw($oError);
+
+    public function update ($fields)
+    {
+        $con = Propel::getConnection( ReportTablePeer::DATABASE_NAME );
+        try {
+            $con->begin();
+            $this->load( $fields['REP_TAB_UID'] );
+            $this->fromArray( $fields, BasePeer::TYPE_FIELDNAME );
+
+            $sDataBase = 'database_' . strtolower( DB_ADAPTER );
+            if (G::LoadSystemExist( $sDataBase )) {
+                G::LoadSystem( $sDataBase );
+                $oDataBase = new database();
+                $oValidate = $oDataBase->getValidate( $this->validate() );
+            } else {
+                $oValidate = $this->validate();
+            }
+            // if($this->validate())
+            if ($oValidate) {
+                $contentResult = 0;
+                if (array_key_exists( "REP_TAB_TITLE", $fields )) {
+                    $contentResult += $this->setRepTabTitle( $fields["REP_TAB_TITLE"] );
+                }
+                $result = $this->save();
+                $result = ($result == 0) ? ($contentResult > 0 ? 1 : 0) : $result;
+                $con->commit();
+                return $result;
+            } else {
+                $con->rollback();
+                $validationE = new Exception( "Failed Validation in class " . get_class( $this ) . "." );
+                $validationE->aValidationFailures = $this->getValidationFailures();
+                throw ($validationE);
+            }
+        } catch (Exception $e) {
+            $con->rollback();
+            throw ($e);
+        }
     }
-  }
-} // ReportTable
+
+    public function remove ($RepTabUid)
+    {
+        $con = Propel::getConnection( ReportTablePeer::DATABASE_NAME );
+        try {
+            $con->begin();
+            $oRepTab = ReportTablePeer::retrieveByPK( $RepTabUid );
+            if (! is_null( $oRepTab )) {
+                Content::removeContent( 'REP_TAB_TITLE', '', $this->getRepTabUid() );
+                $result = $oRepTab->delete();
+                $con->commit();
+            }
+            return $result;
+        } catch (Exception $e) {
+            $con->rollback();
+            throw ($e);
+        }
+    }
+
+    public function reportTableExists ($RepTabUid)
+    {
+        $con = Propel::getConnection( ReportTablePeer::DATABASE_NAME );
+        try {
+            $oRepTabUid = ReportTablePeer::retrieveByPk( $RepTabUid );
+            if (is_object( $oRepTabUid ) && get_class( $oRepTabUid ) == 'ReportTable') {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception $oError) {
+            throw ($oError);
+        }
+    }
+}
+// ReportTable
+
--- workflow/engine/classes/model/Route.php
+++ workflow/engine/classes/model/Route.php
@@ -1,7 +1,8 @@
 <?php
 /**
  * Route.php
- * @package    workflow.engine.classes.model
+ *
+ * @package workflow.engine.classes.model
  *
  * ProcessMaker Open Source Edition
  * Copyright (C) 2004 - 2011 Colosa Inc.
@@ -13,11 +14,11 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  * For more information, contact Colosa Inc, 2566 Le Jeune Rd.,
  * Coral Gables, FL, 33134, USA, or email info@colosa.com.
@@ -33,184 +34,184 @@ require_once 'classes/model/Content.php';
  *
  *
  * You should add additional methods to this class to meet the
- * application requirements.  This class will only be generated as
+ * application requirements. This class will only be generated as
  * long as it does not already exist in the output directory.
  *
- * @package    workflow.engine.classes.model
+ * @package workflow.engine.classes.model
  */
-class Route extends BaseRoute {
+class Route extends BaseRoute
+{
 
-  /*
+    /*
   * Load the application document registry
   * @param string $sRouUid
   * @return variant
   */
-  public function load($sRouUid)
-  {
-    try {
-      $oRoute = RoutePeer::retrieveByPK($sRouUid);
-      if (!is_null($oRoute))
-      {
-        $aFields = $oRoute->toArray(BasePeer::TYPE_FIELDNAME);
-        $this->fromArray($aFields, BasePeer::TYPE_FIELDNAME);
-        return $aFields;
-      }
-      else {
-        throw(new Exception('This row doesn\'t exist!'));
-      }
-    }
-    catch (Exception $oError) {
-      throw($oError);
-    }
-  }
-
-  /**
-   * Create the application document registry
-   * @param array $aData
-   * @return string
-  **/
-  public function create($aData)
-  {
-    $oConnection = Propel::getConnection(RoutePeer::DATABASE_NAME);
-    try {
-      $sRouteUID        = G::generateUniqueID();
-      $aData['ROU_UID'] = $sRouteUID;
-      $oRoute           = new Route();
-
-      // validating default values
-      $aData['ROU_TO_LAST_USER'] = $this->validateValue(isset($aData['ROU_TO_LAST_USER']) ? $aData['ROU_TO_LAST_USER'] : '', array('TRUE', 'FALSE'), 'FALSE');
-      $aData['ROU_OPTIONAL']     = $this->validateValue(isset($aData['ROU_OPTIONAL']) ? $aData['ROU_OPTIONAL'] : '', array('TRUE', 'FALSE'), 'FALSE');
-      $aData['ROU_SEND_EMAIL']   = $this->validateValue(isset($aData['ROU_SEND_EMAIL']) ? $aData['ROU_SEND_EMAIL']: '', array('TRUE', 'FALSE'), 'TRUE');
-
-      $oRoute->fromArray($aData, BasePeer::TYPE_FIELDNAME);
-
-      if ($oRoute->validate()) {
-        $oConnection->begin();
-        $iResult = $oRoute->save();
-        $oConnection->commit();
-        return $sRouteUID;
-      }
-      else {
-        $sMessage = '';
-        $aValidationFailures = $oRoute->getValidationFailures();
-        foreach($aValidationFailures as $oValidationFailure) {
-          $sMessage .= $oValidationFailure->getMessage() . '<br />';
+    public function load ($sRouUid)
+    {
+        try {
+            $oRoute = RoutePeer::retrieveByPK( $sRouUid );
+            if (! is_null( $oRoute )) {
+                $aFields = $oRoute->toArray( BasePeer::TYPE_FIELDNAME );
+                $this->fromArray( $aFields, BasePeer::TYPE_FIELDNAME );
+                return $aFields;
+            } else {
+                throw (new Exception( 'This row doesn\'t exist!' ));
+            }
+        } catch (Exception $oError) {
+            throw ($oError);
         }
-        throw(new Exception('The registry cannot be created!<br />'.$sMessage));
-      }
     }
-    catch (Exception $oError) {
-      $oConnection->rollback();
-      throw($oError);
-    }
-  }
 
-  /**
-   * Update the application document registry
-   * @param array $aData
-   * @return string
-  **/
-  public function update($aData)
-  {
-    $oConnection = Propel::getConnection(RoutePeer::DATABASE_NAME);
-    try {
-      $oRoute = RoutePeer::retrieveByPK($aData['ROU_UID']);
-      if (!is_null($oRoute))
-      {
-        // validating default values
-        if (isset($aData['ROU_TO_LAST_USER'])) {
-          $aData['ROU_TO_LAST_USER'] = $this->validateValue($aData['ROU_TO_LAST_USER'], array('TRUE', 'FALSE'), 'FALSE');
+    /**
+     * Create the application document registry
+     *
+     * @param array $aData
+     * @return string
+     *
+     */
+    public function create ($aData)
+    {
+        $oConnection = Propel::getConnection( RoutePeer::DATABASE_NAME );
+        try {
+            $sRouteUID = G::generateUniqueID();
+            $aData['ROU_UID'] = $sRouteUID;
+            $oRoute = new Route();
+
+            // validating default values
+            $aData['ROU_TO_LAST_USER'] = $this->validateValue( isset( $aData['ROU_TO_LAST_USER'] ) ? $aData['ROU_TO_LAST_USER'] : '', array ('TRUE','FALSE'
+            ), 'FALSE' );
+            $aData['ROU_OPTIONAL'] = $this->validateValue( isset( $aData['ROU_OPTIONAL'] ) ? $aData['ROU_OPTIONAL'] : '', array ('TRUE','FALSE'
+            ), 'FALSE' );
+            $aData['ROU_SEND_EMAIL'] = $this->validateValue( isset( $aData['ROU_SEND_EMAIL'] ) ? $aData['ROU_SEND_EMAIL'] : '', array ('TRUE','FALSE'
+            ), 'TRUE' );
+
+            $oRoute->fromArray( $aData, BasePeer::TYPE_FIELDNAME );
+
+            if ($oRoute->validate()) {
+                $oConnection->begin();
+                $iResult = $oRoute->save();
+                $oConnection->commit();
+                return $sRouteUID;
+            } else {
+                $sMessage = '';
+                $aValidationFailures = $oRoute->getValidationFailures();
+                foreach ($aValidationFailures as $oValidationFailure) {
+                    $sMessage .= $oValidationFailure->getMessage() . '<br />';
+                }
+                throw (new Exception( 'The registry cannot be created!<br />' . $sMessage ));
+            }
+        } catch (Exception $oError) {
+            $oConnection->rollback();
+            throw ($oError);
         }
-        if (isset($aData['ROU_OPTIONAL'])) {
-          $aData['ROU_OPTIONAL']     = $this->validateValue($aData['ROU_OPTIONAL'], array('TRUE', 'FALSE'), 'FALSE');
+    }
+    /**
+     * Update the application document registry
+     *
+     * @param array $aData
+     * @return string
+     *
+     */
+    public function update ($aData)
+    {
+        $oConnection = Propel::getConnection( RoutePeer::DATABASE_NAME );
+        try {
+            $oRoute = RoutePeer::retrieveByPK( $aData['ROU_UID'] );
+            if (! is_null( $oRoute )) {
+                // validating default values
+                if (isset( $aData['ROU_TO_LAST_USER'] )) {
+                    $aData['ROU_TO_LAST_USER'] = $this->validateValue( $aData['ROU_TO_LAST_USER'], array ('TRUE','FALSE'
+                    ), 'FALSE' );
+                }
+                if (isset( $aData['ROU_OPTIONAL'] )) {
+                    $aData['ROU_OPTIONAL'] = $this->validateValue( $aData['ROU_OPTIONAL'], array ('TRUE','FALSE'
+                    ), 'FALSE' );
+                }
+                if (isset( $aData['ROU_SEND_EMAIL'] )) {
+                    $aData['ROU_SEND_EMAIL'] = $this->validateValue( $aData['ROU_SEND_EMAIL'], array ('TRUE','FALSE'
+                    ), 'TRUE' );
+                }
+
+                $oRoute->fromArray( $aData, BasePeer::TYPE_FIELDNAME );
+                if ($oRoute->validate()) {
+                    $oConnection->begin();
+                    $iResult = $oRoute->save();
+                    $oConnection->commit();
+                    return $iResult;
+                } else {
+                    $sMessage = '';
+                    $aValidationFailures = $oRoute->getValidationFailures();
+                    foreach ($aValidationFailures as $oValidationFailure) {
+                        $sMessage .= $oValidationFailure->getMessage() . '<br />';
+                    }
+                    throw (new Exception( 'The ROUTE tables cannot be updated!<br />' . $sMessage ));
+                }
+            } else {
+                throw (new Exception( "The row " . $aData['ROU_UID'] . " doesn't exist!" ));
+            }
+        } catch (Exception $oError) {
+            $oConnection->rollback();
+            throw ($oError);
         }
-        if (isset($aData['ROU_SEND_EMAIL'])) {
-          $aData['ROU_SEND_EMAIL']   = $this->validateValue($aData['ROU_SEND_EMAIL'], array('TRUE', 'FALSE'), 'TRUE');
+    }
+
+    /**
+     * Remove the application document registry
+     *
+     * @param array $aData
+     * @return string
+     *
+     */
+    public function remove ($sRouUid)
+    {
+        $oConnection = Propel::getConnection( RoutePeer::DATABASE_NAME );
+        try {
+            $oRoute = RoutePeer::retrieveByPK( $sRouUid );
+            if (! is_null( $oRoute )) {
+                $oConnection->begin();
+                $iResult = $oRoute->delete();
+                $oConnection->commit();
+                return $iResult;
+            } else {
+                throw (new Exception( 'This row doesn\'t exist!' ));
+            }
+        } catch (Exception $oError) {
+            $oConnection->rollback();
+            throw ($oError);
         }
+    }
 
-        $oRoute->fromArray($aData, BasePeer::TYPE_FIELDNAME);
-        if ($oRoute->validate()) {
-          $oConnection->begin();
-          $iResult = $oRoute->save();
-          $oConnection->commit();
-          return $iResult;
+    public function routeExists ($sRouUid)
+    {
+        $con = Propel::getConnection( RoutePeer::DATABASE_NAME );
+        try {
+            $oRouUid = RoutePeer::retrieveByPk( $sRouUid );
+            if (is_object( $oRouUid ) && get_class( $oRouUid ) == 'Route') {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception $oError) {
+            throw ($oError);
         }
-        else {
-          $sMessage = '';
-          $aValidationFailures = $oRoute->getValidationFailures();
-          foreach($aValidationFailures as $oValidationFailure) {
-            $sMessage .= $oValidationFailure->getMessage() . '<br />';
-          }
-          throw(new Exception('The ROUTE tables cannot be updated!<br />'.$sMessage));
+    }
+
+    /**
+     * Validate value for a variable that only accepts some determinated values
+     *
+     * @param $value string - value to test
+     * @param $validValues array - list of valid values
+     * @param $default string default value, if the tested value is not valid the default value is returned
+     * @return the tested and accepted value
+     */
+    public function validateValue ($value, $validValues, $default)
+    {
+        if (! in_array( $value, $validValues )) {
+            $value = $default;
         }
-      }
-      else {
-        throw(new Exception( "The row " . $aData['ROU_UID'] . " doesn't exist!" ));
-      }
+        return $value;
     }
-    catch (Exception $oError) {
-      $oConnection->rollback();
-      throw($oError);
-    }
-  }
+}
+// Route
 
-  /**
-   * Remove the application document registry
-   * @param array $aData
-   * @return string
-  **/
-  public function remove($sRouUid)
-  {
-    $oConnection = Propel::getConnection(RoutePeer::DATABASE_NAME);
-    try {
-      $oRoute = RoutePeer::retrieveByPK($sRouUid);
-      if (!is_null($oRoute))
-      {
-        $oConnection->begin();
-        $iResult = $oRoute->delete();
-        $oConnection->commit();
-        return $iResult;
-      }
-      else {
-        throw(new Exception('This row doesn\'t exist!'));
-      }
-    }
-    catch (Exception $oError) {
-      $oConnection->rollback();
-      throw($oError);
-    }
-  }
-
-  function routeExists ( $sRouUid ) {
-    $con = Propel::getConnection(RoutePeer::DATABASE_NAME);
-    try {
-      $oRouUid = RoutePeer::retrieveByPk( $sRouUid );
-      if (is_object($oRouUid) && get_class ($oRouUid) == 'Route' ) {
-        return true;
-      }
-      else {
-        return false;
-      }
-    }
-    catch (Exception $oError) {
-      throw($oError);
-    }
-  }
-
-  /**
-   * Validate value for a variable that only accepts some determinated values
-   *
-   * @param $value string - value to test
-   * @param $validValues array - list of valid values
-   * @param $default string default value, if the tested value is not valid the default value is returned
-   * @return the tested and accepted value
-   */
-  function validateValue($value, $validValues, $default)
-  {
-    if (!in_array($value, $validValues)) {
-      $value = $default;
-    }
-
-    return $value;
-  }
-} // Route
