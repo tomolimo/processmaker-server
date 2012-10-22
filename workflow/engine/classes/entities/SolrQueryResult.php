dcf7b029798ca8d947331d8ad6b8fc45c37c45fe
jennylee <jennylee@colosa.com>
2012-10-22 16:35:54 -0400

    CODE STYLE
    FILES:
    gulliver/system/class.tree.php
    workflow/engine/classes/class.memcached.php
    workflow/engine/classes/class.net.php
    workflow/engine/classes/entities/AppSolrQueue.php
    workflow/engine/classes/entities/FacetInterfaceRequest.php
    workflow/engine/classes/entities/FacetInterfaceResult.php
    workflow/engine/classes/entities/FacetItem.php
    workflow/engine/classes/entities/FacetResult.php
    workflow/engine/classes/entities/SelectedFacetGroupItem.php
    workflow/engine/classes/entities/SolrQueryResult.php

--- workflow/engine/classes/entities/SolrQueryResult.php
+++ workflow/engine/classes/entities/SolrQueryResult.php
@@ -3,38 +3,36 @@ require_once ('Base.php');
 
 class Entity_SolrQueryResult extends Entity_Base
 {
-  public $sEcho = '';
-  public $iTotalRecords = 0;
-  public $iTotalDisplayRecords = 10;
-  public $aaData = array (); // array of arrays of records to
-                             // display
-  
-  private function __construct()
-  {
-  }
-  
-  static function createEmpty()
-  {
-    $obj = new Entity_SolrQueryResult ();
-    return $obj;
-  }
-  
-  static function createForRequest($data)
-  {
-    $obj = new Entity_SolrQueryResult ();
-    
-    $obj->initializeObject ($data);
-    
-    $requiredFields = array (
-        'sEcho',
-        'iTotalRecords',
-        'iTotalDisplayRecords',
-        'aaData' 
-    );
-    
-    $obj->validateRequiredFields ($requiredFields);
-    
-    return $obj;
-  }
+    public $sEcho = '';
+    public $iTotalRecords = 0;
+    public $iTotalDisplayRecords = 10;
+    public $aaData = array (); // array of arrays of records to
 
+    // display
+
+
+    private function __construct ()
+    {
+    }
+
+    static function createEmpty ()
+    {
+        $obj = new Entity_SolrQueryResult();
+        return $obj;
+    }
+
+    static function createForRequest ($data)
+    {
+        $obj = new Entity_SolrQueryResult();
+
+        $obj->initializeObject( $data );
+
+        $requiredFields = array ('sEcho','iTotalRecords','iTotalDisplayRecords','aaData'
+        );
+
+        $obj->validateRequiredFields( $requiredFields );
+
+        return $obj;
+    }
 }
+
