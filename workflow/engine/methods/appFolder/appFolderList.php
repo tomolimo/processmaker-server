cfbbf82f776d839007ec9a2de41cffd15b4ae133
jennylee <jennylee@colosa.com>
2012-10-22 17:23:01 -0400

    CODE STYLE, changes
    FILES:
    gulliver/system/class.database_mssql.php
    gulliver/system/class.database_mysql.php
    workflow/engine/classes/class.cli.php
    workflow/engine/classes/class.pluginRegistry.php
    workflow/engine/classes/entities/FacetGroup.php
    workflow/engine/classes/entities/SolrUpdateDocument.php
    workflow/engine/classes/triggers/class.pmTalendFunctions.php
    workflow/engine/methods/appFolder/appFolderList.php
    workflow/engine/methods/processes/processes_Ajax.php

--- workflow/engine/methods/appFolder/appFolderList.php
+++ workflow/engine/methods/appFolder/appFolderList.php
@@ -1,57 +1,54 @@
 <?php
 
-  try {
+try {
 
-  $G_MAIN_MENU = 'processmaker';
-  $G_SUB_MENU = '';
-  $G_ID_MENU_SELECTED = 'FOLDERS';
-  $G_ID_SUB_MENU_SELECTED = '';
+    $G_MAIN_MENU = 'processmaker';
+    $G_SUB_MENU = '';
+    $G_ID_MENU_SELECTED = 'FOLDERS';
+    $G_ID_SUB_MENU_SELECTED = '';
 
-  $G_PUBLISH = new Publisher;
+    $G_PUBLISH = new Publisher();
 
+    if ((isset( $_POST['form']['FOLDER_UID'] )) && (isset( $_POST['form']['MOVE_FOLDER_PATH'] ))) {
+        require_once ("classes/model/AppDocument.php");
+        $oAppDocument = new AppDocument();
 
-  if((isset($_POST['form']['FOLDER_UID']))&&(isset($_POST['form']['MOVE_FOLDER_PATH']))){
-    require_once ( "classes/model/AppDocument.php" );
-    $oAppDocument = new AppDocument();
-
-    //Move files to another FOLDER_UID'
-    $folderUid=$_POST['form']['FOLDER_UID'];
-    $filesArrayAux=explode(";",$_POST['form']['MOVE_FOLDER_PATH']);
-    $filesArray=array();
-    foreach($filesArrayAux as $value){
-        if($value!=""){
-            $valueAux=explode("|",$value);
-            $filesArray[$valueAux[1]]=$valueAux[0];
+        //Move files to another FOLDER_UID'
+        $folderUid = $_POST['form']['FOLDER_UID'];
+        $filesArrayAux = explode( ";", $_POST['form']['MOVE_FOLDER_PATH'] );
+        $filesArray = array ();
+        foreach ($filesArrayAux as $value) {
+            if ($value != "") {
+                $valueAux = explode( "|", $value );
+                $filesArray[$valueAux[1]] = $valueAux[0];
+            }
+        }
+        foreach ($filesArray as $keyDoc => $sw) {
+            if ($sw == "true") {
+                $keyDocArray = explode( "_", $keyDoc );
+                $aFields = array ('APP_DOC_UID' => $keyDocArray[0],'DOC_VERSION' => $keyDocArray[1],'FOLDER_UID' => $folderUid
+                );
+                $oAppDocument->update( $aFields );
+            }
         }
     }
-    foreach($filesArray as $keyDoc => $sw){
-        if($sw=="true"){
-            $keyDocArray=explode("_",$keyDoc);
-            $aFields = array('APP_DOC_UID'         => $keyDocArray[0],
-                             'DOC_VERSION'         => $keyDocArray[1],
-                             'FOLDER_UID'          => $folderUid);
-            $oAppDocument->update($aFields);
-        }
-    }
-  }
 
-//$rootFolder='5320083284b210ceb511e43070218744';
-$rootFolder='0';
-//$rootFolder='4977070264b54bf093aef68069996372';
+    //$rootFolder='5320083284b210ceb511e43070218744';
+    $rootFolder = '0';
+    //$rootFolder='4977070264b54bf093aef68069996372';
 
 
-  $G_PUBLISH->AddContent('view', 'appFolder/appFolderTree' );
-  $G_PUBLISH->AddContent('smarty', 'appFolder/appFolderFileList', '', '', array());
-  G::RenderPage( "publish-treeview" , 'blank');
+    $G_PUBLISH->AddContent( 'view', 'appFolder/appFolderTree' );
+    $G_PUBLISH->AddContent( 'smarty', 'appFolder/appFolderFileList', '', '', array () );
+    G::RenderPage( "publish-treeview", 'blank' );
 
-  }
-  catch ( Exception $e ) {
-    $G_PUBLISH = new Publisher;
+} catch (Exception $e) {
+    $G_PUBLISH = new Publisher();
     $aMessage['MESSAGE'] = $e->getMessage();
-    $G_PUBLISH->AddContent('xmlform', 'xmlform', 'login/showMessage', '', $aMessage );
+    $G_PUBLISH->AddContent( 'xmlform', 'xmlform', 'login/showMessage', '', $aMessage );
     G::RenderPage( 'publish', 'blank' );
-  }
-  ?>
+}
+?>
 <script>
 
 
