32fd17e9aec191ceb70b656987df55dcc2450b3e
Gustavo Cruz <gustavo.cruz@colosa.com>
2013-01-07 17:13:03 -0400

    removing some test files

--- workflow/engine/methods/outputdocs/testTinyFrame.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
-<iframe src="http://gustavo.pmos.net/sysworkflow/en/classic/outputdocs/testTiny" width="100%" height="100%"/>
