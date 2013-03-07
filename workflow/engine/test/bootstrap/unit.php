66b63dbf6e5897950023de404d70f1cd36f45f66
ralph <ralph@colosa.com>
2013-03-07 14:38:47 -0400

    adding the autoclass loader

--- workflow/engine/test/bootstrap/unit.php
+++ workflow/engine/test/bootstrap/unit.php
@@ -62,10 +62,48 @@
   //$_test_dir = realpath(dirname(__FILE__).'/..');
   //require_once( 'lime/lime.php');
 
+  require_once (PATH_GULLIVER . "class.bootstrap.php");
+echo "boot\n";
+  spl_autoload_register(array('Bootstrap', 'autoloadClass'));
+echo "autoloadclass\n";
+  Bootstrap::registerClass('G', PATH_GULLIVER . "class.g.php");
+  Bootstrap::registerClass('System',        PATH_HOME . "engine/classes/class.system.php");
+// Call more Classes
+  Bootstrap::registerClass('headPublisher', PATH_GULLIVER . "class.headPublisher.php");
+  Bootstrap::registerClass('publisher', PATH_GULLIVER . "class.publisher.php");
+  Bootstrap::registerClass('xmlform', PATH_GULLIVER . "class.xmlform.php");
+  Bootstrap::registerClass('XmlForm_Field', PATH_GULLIVER . "class.xmlform.php");
+  Bootstrap::registerClass('xmlformExtension', PATH_GULLIVER . "class.xmlformExtension.php");
+  Bootstrap::registerClass('form',         PATH_GULLIVER . "class.form.php");
+  Bootstrap::registerClass('menu',         PATH_GULLIVER . "class.menu.php");
+  Bootstrap::registerClass('Xml_Document', PATH_GULLIVER . "class.xmlDocument.php");
+  Bootstrap::registerClass('DBSession',    PATH_GULLIVER . "class.dbsession.php");
+  Bootstrap::registerClass('DBConnection', PATH_GULLIVER . "class.dbconnection.php");
+  Bootstrap::registerClass('DBRecordset',  PATH_GULLIVER . "class.dbrecordset.php");
+  Bootstrap::registerClass('DBTable',      PATH_GULLIVER . "class.dbtable.php");
+  Bootstrap::registerClass('xmlMenu',      PATH_GULLIVER . "class.xmlMenu.php");
+  Bootstrap::registerClass('XmlForm_Field_FastSearch', PATH_GULLIVER . "class.xmlformExtension.php");
+  Bootstrap::registerClass('XmlForm_Field_XmlMenu', PATH_GULLIVER . "class.xmlMenu.php");
+  Bootstrap::registerClass('XmlForm_Field_HTML',  PATH_GULLIVER . "class.dvEditor.php");
+  Bootstrap::registerClass('XmlForm_Field_WYSIWYG_EDITOR',  PATH_GULLIVER . "class.wysiwygEditor.php");
+  Bootstrap::registerClass('Controller',          PATH_GULLIVER . "class.controller.php");
+  Bootstrap::registerClass('HttpProxyController', PATH_GULLIVER . "class.httpProxyController.php");
+  Bootstrap::registerClass('templatePower',            PATH_GULLIVER . "class.templatePower.php");
+  Bootstrap::registerClass('XmlForm_Field_SimpleText', PATH_GULLIVER . "class.xmlformExtension.php");
+  Bootstrap::registerClass('Propel',          PATH_THIRDPARTY . "propel/Propel.php");
+  Bootstrap::registerClass('Creole',          PATH_THIRDPARTY . "creole/Creole.php");
+  Bootstrap::registerClass('Criteria',        PATH_THIRDPARTY . "propel/util/Criteria.php");
+  Bootstrap::registerClass('Groups',       PATH_HOME . "engine/classes/class.groups.php");
+  Bootstrap::registerClass('Tasks',        PATH_HOME . "engine/classes/class.tasks.php");
+  Bootstrap::registerClass('Calendar',     PATH_HOME . "engine/classes/class.calendar.php");
+  Bootstrap::registerClass('processMap',   PATH_HOME . "engine/classes/class.processMap.php");
+
+  Bootstrap::registerSystemClasses();
+echo "classes registed \n";
   require_once( PATH_THIRDPARTY . 'pake' . PATH_SEP . 'pakeFunction.php');
   require_once( PATH_THIRDPARTY . 'pake' . PATH_SEP . 'pakeGetopt.class.php');
   require_once( PATH_CORE . 'config' . PATH_SEP . 'environments.php');
 
-  if ( !defined ( 'G_ENVIRONMENT') ) 
+  if ( !defined ( 'G_ENVIRONMENT') )
     define ( 'G_ENVIRONMENT', G_TEST_ENV );
 
