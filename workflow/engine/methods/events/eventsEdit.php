5e5df0dcd331fb6684d7c52a38243fc00b5ba34c
Brayan Osmar Pereyra Suxo <brayan@colosa.com>
2013-03-14 11:33:59 -0400

    test

--- workflow/engine/methods/events/eventsEdit.php
+++ workflow/engine/methods/events/eventsEdit.php
@@ -36,8 +36,6 @@ if (isset( $_GET['EVN_UID'] )) {
     require_once 'classes/model/Event.php';
     $oEvent = new Event();
     $aFields = $oEvent->load( $_GET['EVN_UID'] );
-    //G::pr($aFields);
-    //die;
 } else {
     $aFields = array ('PRO_UID' => $_GET['PRO_UID'] );
 }
@@ -60,7 +58,8 @@ foreach ($aAux1 as $aAux2) {
 
 $oProcessMap = new processMap( new DBConnection() );
 $aTriggersList = $oProcessMap->getTriggers( $_SESSION['PROCESS'] );
-$aTriggersFileds[0] = Array ('TRI_UID' => 'char','TRI_TITLE' => 'char');
+$aTriggersFileds = array ();
+$aTriggersFileds[] = array ('TRI_UID' => 'char','TRI_TITLE' => 'char');
 
 foreach ($aTriggersList as $i => $v) {
     unset( $aTriggersList[$i]['PRO_UID'] );
