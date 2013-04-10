ed00e2ed3106b7e88e02e9d298388892b8b7bc3f
Julio Cesar Laura <juliocesar@colosa.com>
2013-04-10 15:13:20 -0400

    IMPROVEMENT in file ajaxListener.php, change "elseif" to "else if" in javascript code

--- workflow/engine/methods/cases/ajaxListener.php
+++ workflow/engine/methods/cases/ajaxListener.php
@@ -640,7 +640,7 @@ class Ajax
                     http_request.overrideMimeType('text/xml');
               }
             }
-            elseif (window.ActiveXObject) {
+            else if (window.ActiveXObject) {
                 // IE
                 try {
                     http_request = new ActiveXObject("Msxml2.XMLHTTP");
