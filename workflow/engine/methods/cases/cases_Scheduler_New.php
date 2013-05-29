06d5953c3899c5b71b46b56e92cf30a4ad129e51
Marco Antonio Nina <marcoantonionina@colosa.com>
2013-05-29 11:54:59 -0400

    BUG-11275 Error en la creacion de Case Scheduler SOLVED
    - No validation.
    - Was add validation for name case scheduler.

--- workflow/engine/methods/cases/cases_Scheduler_New.php
+++ workflow/engine/methods/cases/cases_Scheduler_New.php
@@ -88,7 +88,7 @@ try {
 
     $aFields['SCH_LIST'] = '';
     foreach ($_SESSION['_DBArray']['cases_scheduler'] as $key => $item) {
-        $aFields['SCH_LIST'] .=  $item['SCH_NAME'] . '^';
+        $aFields['SCH_LIST'] .=  htmlspecialchars($item['SCH_NAME'], ENT_QUOTES) . '^';
     }
 
     $G_PUBLISH->AddContent( 'xmlform', 'xmlform', 'cases/cases_Scheduler_New.xml', '', $aFields, 'cases_Scheduler_Save' );
