336854d2f38af2d8919500bc40fb86ab7c293ec9
Luis Fernando Saisa Lopez <luis.saisa@colosa.com>
2013-06-13 17:21:41 +0000

    BUG 11435 "El campo formula permite un limite reducido para..." SOLVED
    - El campo formula permite un limite reducido para introducir caracteres
    - Problema resuelto, en los DynaForm en su campo "Formula", se aumento su maxlength a 500
    * Available from version ProcessMaker-2.5.2-testing.1

--- workflow/engine/methods/cases/cases_Step.php
+++ workflow/engine/methods/cases/cases_Step.php
@@ -220,7 +220,7 @@ try {
 
             $oHeadPublisher = & headPublisher::getSingleton();
 
-            if(!isset($_SESSION["PM_RUN_OUTSIDE_MAIN_APP"])) {
+            if (!isset($_SESSION["PM_RUN_OUTSIDE_MAIN_APP"])) {
                 $oHeadPublisher->addScriptCode( "
                                                     if (typeof parent != 'undefined') {
                                                         if (parent.setNode) {
@@ -1000,7 +1000,7 @@ try {
 $oHeadPublisher = & headPublisher::getSingleton();
 $oHeadPublisher->addScriptFile( "/jscore/cases/core/cases_Step.js" );
 
-if(!isset($_SESSION["PM_RUN_OUTSIDE_MAIN_APP"])) {
+if (!isset($_SESSION["PM_RUN_OUTSIDE_MAIN_APP"])) {
     $oHeadPublisher->addScriptCode( "
                                         if (typeof parent != 'undefined') {
                                             if (parent.showCaseNavigatorPanel) {
