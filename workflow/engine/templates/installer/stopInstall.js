651ecfeafd44b97364d2dac4eb4f2cb8bf8462c5
Brayan Osmar Pereyra Suxo <brayan@colosa.com>
2013-08-30 10:32:17 -0400

    Adicion de formulario para evitar installer para Linux con el PARTNER_FLAG

---
+++ workflow/engine/templates/installer/stopInstall.js
@@ -0,0 +1,16 @@
+Ext.onReady(function() {
+
+    var formLicenseLog = new Ext.FormPanel({
+        labelWidth : 60,
+        frame : true,
+        autoScroll: true,
+        monitorValid : true,
+        renderTo : 'bodyNoInsatalled',
+        title : _('ID_TITLE_NO_INSTALL'),
+
+        items:[
+            {html: _('ID_NO_INSTALL')}
+        ]
+    });
+});
+
