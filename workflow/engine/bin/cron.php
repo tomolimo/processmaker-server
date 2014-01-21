f2b8183ce67c654a7435a02b58193c7e51292eb6
Julio Cesar Laura <juliocesar@colosa.com>
2014-01-21 13:02:08 -0400

    BUG 13938 Line error_reporting(E_ALL) in cron.php and cron_single.php is generating unnecesary errors SOLVED
    - Unnecesary lines to set the configuration for "display_errors" and "error_reporting"
    - Delete unnecesary lines and now this configurations is setted with the processmaker configuration (env.ini)

--- workflow/engine/bin/cron.php
+++ workflow/engine/bin/cron.php
@@ -3,10 +3,6 @@
  * cron.php
  * @package workflow-engine-bin
  */
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-ini_set('memory_limit', '300M'); // nore: this may need to be higher for many projects
-$mem_limit = (int) ini_get('memory_limit');
 
 if ( !defined('PATH_SEP') ) {
   define('PATH_SEP', ( substr(PHP_OS, 0, 3) == 'WIN' ) ? '\\' : '/');
