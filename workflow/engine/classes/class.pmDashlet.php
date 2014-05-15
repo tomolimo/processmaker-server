3fff15f99e1d148dc5ff2629f22db93048685833
Marco Antonio Nina <marcoantonionina@colosa.com>
2014-05-15 14:06:24 -0400

    BUG-14923 Field in Dashboards SOLVED
    - Add span in file class.pmDashlet.php

--- workflow/engine/classes/class.pmDashlet.php
+++ workflow/engine/classes/class.pmDashlet.php
@@ -320,9 +320,7 @@ class PMDashlet extends DashletInstance implements DashletInterface
                 }
             }
             foreach ($dashletsInstances as $key => $field) {
-                if ($field['DAS_CLASS'] == 'dashletCustomDrillDown') {
-                    $dashletsInstances[$key]['DAS_TITLE'] = $field['DAS_TITLE'] . '</span><span style="float:right; font: bold;" id="'. $field['DAS_INS_UID'] .'">';
-                }
+                $dashletsInstances[$key]['DAS_TITLE'] = $field['DAS_TITLE'] . '</span><span style="float:right; font: bold;" id="'. $field['DAS_INS_UID'] .'">';
             }
             // Check for role assigments
             // ToDo: Next release
