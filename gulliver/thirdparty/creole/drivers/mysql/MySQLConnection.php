1a6da4cb6277e7cf01e8bc352d4eb8c2a123f0f0
Marco Antonio Nina <marcoantonionina@colosa.com>
2014-06-16 11:47:23 -0400

    BUG-14984 validations
    - A validation was added

--- gulliver/thirdparty/creole/drivers/mysql/MySQLConnection.php
+++ gulliver/thirdparty/creole/drivers/mysql/MySQLConnection.php
@@ -199,7 +199,11 @@ class MySQLConnection extends ConnectionCommon implements Connection {
         }//echo $sql . '<br /><br />';
         $result = @mysql_query($sql, $this->dblink);
         if (!$result) {
-            throw new SQLException('Could not execute query', mysql_error($this->dblink), $sql);
+            if (isset($_SESSION['PROCESSMAKER_ENV']['debug_sql']) && $_SESSION['PROCESSMAKER_ENV']['debug_sql'] == 1) {
+                throw new SQLException('Could not execute query', mysql_error($this->dblink), $sql);
+            } else {
+                throw new SQLException('It is not possible to execute the query. Please contact your system administrator');
+            }
         }
         return new MySQLResultSet($this, $result, $fetchmode);
     }
