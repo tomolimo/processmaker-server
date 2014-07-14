12f5ecb9611e2879c54c68e6f55dc9cdf6e1c773
jennylee <jennylee@colosa.com>
2014-07-14 15:13:23 -0400

    BUG_13887 Permite eliminacion del lenguaje actual.
    El lengujae actual podia ser borrado.
    El lenguaje por defecto 'en' tambien podia ser borrado.
    
    Se agrego validaciones para que el lenguaje por defecto de PM (en) no sea eliminado jamas y tambien para que el lenguaje que se esta usando actualmente tampoco sea eliminado.

--- workflow/engine/methods/setup/language_Ajax.php
+++ workflow/engine/methods/setup/language_Ajax.php
@@ -152,17 +152,28 @@ try {
             $oContent = new Content();
             $aRow = $oDataset->getRow();
 
-            //THERE IS NO ANY CASE STARTED FROM THES LANGUAGE
-            if ($aRow[0] == 0) { //so we can delete this language
-                try {
-                    Content::removeLanguageContent( $locale );
-                    $trn->removeTranslationEnvironment( $locale );
-                    echo G::LoadTranslation( 'ID_LANGUAGE_DELETED_SUCCESSFULLY' );
-                } catch (Exception $e) {
-                    echo $e->getMessage();
-                }
+            $actualLanguage = $_ENV['REQUEST_URI'];
+            $actualLanguage = explode("/", $actualLanguage);
+
+            if($locale != "en"){ //Default Lengage 'en'
+            	if($locale != $actualLanguage[2]){ //Current lenguage
+            		//THERE IS NO ANY CASE STARTED FROM THES LANGUAGE
+            		if ($aRow[0] == 0) { //so we can delete this language
+            			try {
+            				Content::removeLanguageContent( $locale );
+            				$trn->removeTranslationEnvironment( $locale );
+            				echo G::LoadTranslation( 'ID_LANGUAGE_DELETED_SUCCESSFULLY' );
+            			} catch (Exception $e) {
+            				echo $e->getMessage();
+            			}
+            		} else {
+            			echo str_replace( '{0}', $aRow[0], G::LoadTranslation( 'ID_LANGUAGE_CANT_DELETE' ) );
+            		}
+            	} else {
+            		echo str_replace( '{0}', $aRow[0], G::LoadTranslation( 'ID_LANGUAGE_CANT_DELETE_CURRENTLY' ) );
+            	}
             } else {
-                echo str_replace( '{0}', $aRow[0], G::LoadTranslation( 'ID_LANGUAGE_CANT_DELETE' ) );
+            	echo str_replace( '{0}', $aRow[0], G::LoadTranslation( 'ID_LANGUAGE_CANT_DELETE_DEFAULT' ) );
             }
             break;
     }
