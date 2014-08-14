b499b6a614d2762f88c701d97513cd1effe4601f
jennylee <jennylee@colosa.com>
2014-08-14 10:31:47 -0400

    BUG-15198 Problemas con querys en fields.
    Se aumenta una validacion para el caso en que el campo este en modo 'edit' y tenga ya datos almacenados, en este caso no se ejecutara el query y se mostrara el valor ya almacenado.

--- gulliver/system/class.xmlform.php
+++ gulliver/system/class.xmlform.php
@@ -320,6 +320,10 @@ class XmlForm_Field
             return 1;
         }
 
+        if(isset($this->mode) && $this->mode == "edit" && ($this->owner->values[$this->name] !== "")){
+        	return 1;
+        }
+
         if(isset($this->mode) && $this->mode == "view" && ($this->type == "text" || $this->type == "currency" || $this->type == "percentage" || $this->type == "textarea" || $this->type == "hidden" || $this->type == "suggest")){
         	return 1;
         }
