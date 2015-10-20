a0f066df5be00136ec7bccd4526031da526c39fc
tomolimo <olivier.moron@araymond.com>
2015-10-20 11:08:07 +0200

    SOAP sessions timelife are harcoded to 15'
    Changed this timelife to match session.cookie_lifetime

--- workflow/engine/classes/class.wsBase.php
+++ workflow/engine/classes/class.wsBase.php
@@ -138,13 +138,15 @@ class wsBase
             $sessionId = G::generateUniqueID();
             $wsResponse = new wsResponse( '0', $sessionId );
 
+			$timelife = explode( ':', gmdate("H:i:s", ini_get('session.cookie_lifetime')));
+			
             $session = new Session();
             $session->setSesUid( $sessionId );
             $session->setSesStatus( 'ACTIVE' );
             $session->setUsrUid( $uid );
             $session->setSesRemoteIp( $_SERVER['REMOTE_ADDR'] );
             $session->setSesInitDate( date( 'Y-m-d H:i:s' ) );
-            $session->setSesDueDate( date( 'Y-m-d H:i:s', mktime( date( 'H' ), date( 'i' ) + 15, date( 's' ), date( 'm' ), date( 'd' ), date( 'Y' ) ) ) );
+            $session->setSesDueDate( date( 'Y-m-d H:i:s', mktime( date( 'H' ) + $timelife[0], date( 'i' ) + $timelife[1], date( 's' ) + $timelife[2], date( 'm' ), date( 'd' ), date( 'Y' ) ) ) );
             $session->setSesEndDate( '' );
             $session->Save();
 
