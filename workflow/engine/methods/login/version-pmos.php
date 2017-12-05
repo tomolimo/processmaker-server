f5a51caf317926e8f567e74a11bbba240bd26f85
tomolimo <tomolimo@gmail.com>
2017-12-05 10:25:07 +0100

    Added RE-1.7 version number

--- workflow/engine/methods/login/version-pmos.php
+++ workflow/engine/methods/login/version-pmos.php
@@ -1,3 +1,4 @@
 <?php
-  define("PM_VERSION", "3.0.1.8-community");
-  define("PM_BUILD_VERSION", "3.0.1.8-community");
+  define("PM_VERSION", "3.0.1.8-community-RE-1.7");
+  define("PM_BUILD_VERSION", "3.0.1.8-community-RE-1.7");
+  
